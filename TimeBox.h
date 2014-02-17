cae63a3b95482fd8996c20da9282d6955e107dcd
cbossut <bossut.clement@gmail.com>
2014-02-17 18:40:35 +0100

    Add TimeBox complete API

---
+++ TimeBox.h
@@ -0,0 +1,80 @@
+/*!
+ * \file TimeBox.h
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#ifndef TIMEBOX_H_
+#define TIMEBOX_H_
+
+#include <string>
+
+#include "IObservable.h"
+
+namespace OSSIA {
+
+class EndEvent;
+class Scenario;
+class StartEvent;
+class State;
+class TimeNode;
+class TimeProcess;
+class TimeValue;
+
+class TimeBox : public IObservable {
+
+  // Constructors, destructor, assignment
+  TimeBox();
+  TimeBox(const TimeBox&);
+  ~TimeBox();
+  TimeBox & operator= (const TimeBox&);
+
+  // Lecture
+  void play(bool log = false, std::string name = "") const;
+
+  // Navigation
+  TimeNode & getPreviousNode() const;
+  TimeNode & getNextNode();
+  StartEvent & getStartEvent() const;
+  EndEvent & getEndEvent() const;
+  Scenario & getParentScenario() const;
+
+  // Iterators
+  class const_iterator; // bidirectional
+  const_iterator begin() const;
+  const_iterator end() const;
+  const_iterator find(const TimeProcess&) const;
+
+  // Managing TimeProcesses
+  void addTimeProcess(const TimeProcess&);
+  bool removeTimeProcess(const TimeProcess&);
+
+  // Accessors
+  TimeValue getLength() const;
+  void setLength(TimeValue);
+  TimeValue getMinimumLength() const;
+  void setMinimumLength(TimeValue);
+  TimeValue getMaximumLength() const;
+  void setMaximumLength(TimeValue);
+  bool isRigid();
+  State & getStartState() const;
+  void setStartState(const State&); // set a copy
+  void setStartState(const State*); // set a reference
+  State & getEndState() const;
+  void setEndState(const State&); // set a copy
+  void setEndState(const State*); // set a reference
+
+  // pimpl idiom
+private:
+  class Impl;
+  Impl * pimpl;
+
+};
+
+}
+
+#endif /* TIMEBOX_H_ */
