019e38c246d36685a51e904f3d52e48950797767
cbossut <bossut.clement@gmail.com>
2014-03-03 15:26:16 +0100

    Add Scenario.h

---
+++ Scenario.h
@@ -0,0 +1,49 @@
+/*!
+ * \file Scenario.h
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#ifndef SCENARIO_H_
+#define SCENARIO_H_
+
+#include <set>
+
+#include "TimeProcess.h"
+
+namespace OSSIA {
+
+class TimeBox;
+class TimeNode;
+
+class Scenario : public TimeProcess {
+
+public:
+
+  // Constructors, destructor, assignment
+  Scenario();
+  Scenario(const Scenario&);
+  ~Scenario();
+  Scenario & operator= (const Scenario&);
+
+  // Lecture
+  virtual void play() const override;
+
+  // Navigation
+  std::set<TimeBox*> getTimeBoxes() const;
+  std::set<TimeNode*> getTimeNodes() const;
+
+  // pimpl idiom
+private:
+  class Impl;
+  Impl * pimpl;
+
+};
+
+}
+
+#endif /* SCENARIO_H_ */
