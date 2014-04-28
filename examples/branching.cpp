9fc45052b46a8c481f3b58c991db144346221fae
cbossut <bossut.clement@gmail.com>
2014-04-28 18:07:28 +0200

    Examples update

--- examples/branching.cpp
+++ examples/branching.cpp
@@ -8,6 +8,9 @@
  * http://www.cecill.info
  */
 
+#include "Event.h"
+#include "Expression.h"
+#include "InteractiveBranching.h"
 #include "Scenario.h"
 #include "State.h"
 #include "TimeBox.h"
@@ -20,7 +23,6 @@ void main() {
 
   State stStart;
   State stEnd;
-  State stChoice;
   State stA;
   State stB1;
   State stB2;
@@ -34,7 +36,27 @@ void main() {
   // Scenario
 
   Scenario branching;
+
+  // Setting states
+
   branching.setStartState(stStart);
   branching.setEndState(stEnd);
+  A.getEndEvent().setState(stA);
+  B1.getEndEvent().setState(stB1);
+  B2.getEndEvent().setState(stB2);
+
+  // Add boxes to Scenario
+
+  branching.addTimeBox(A, branching.getStartEvent());
+
+  // Branching
+
+  InteractiveBranching cond;
+  cond.setEvent(A.getEndEvent());
+  cond.addBranch(B1.getStartEvent(), "choice == 1");
+  cond.addBranch(B2.getStartEvent(), "choice == 2");
+
+  branching.addTimeBox(B1, B1.getStartEvent()); // ??
+  branching.addTimeBox(B2, B2.getStartEvent()); // ??
 
 }
