2958f4ecee86a4cc3ef97abe8eb862ba95697d67
cbossut <bossut.clement@gmail.com>
2014-04-28 18:07:14 +0200

    Expression draft

---
+++ Expression.h
@@ -0,0 +1,30 @@
+/*!
+ * \file Expression.h
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#ifndef EXPRESSION_H_
+#define EXPRESSION_H_
+
+namespace OSSIA {
+
+class Expression {
+
+public:
+
+  // Constructors, destructor, assignment
+  Expression();
+  Expression(const char*);
+
+};
+
+}
+
+
+
+#endif /* EXPRESSION_H_ */
