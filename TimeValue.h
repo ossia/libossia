2461da2491a128100809dbcb8a364641ecf4f4e1
cbossut <bossut.clement@gmail.com>
2014-04-29 17:38:34 +0200

    TimeValue draft

---
+++ TimeValue.h
@@ -0,0 +1,32 @@
+/*!
+ * \file TimeValue.h
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#ifndef TIMEVALUE_H_
+#define TIMEVALUE_H_
+
+namespace OSSIA {
+
+class TimeValue {
+
+  // Constructors, destructor, assignment
+  TimeValue();
+  TimeValue(const int);
+  TimeValue & operator= (const int);
+
+  // Cast
+  operator int();
+
+};
+
+}
+
+
+
+#endif /* TIMEVALUE_H_ */
