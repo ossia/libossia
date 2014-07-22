56154554106d758cf618454879f3aaa18e32358b
cbossut <bossut.clement@gmail.com>
2014-07-22 15:53:46 +0200

    Implementation example

---
+++ ImplScore/Scenario.cpp
@@ -0,0 +1,17 @@
+/*!
+ * \file Scenario.cpp
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#include "Scenario.h"
+
+class OSSIA::Scenario::Impl {
+
+
+
+};
