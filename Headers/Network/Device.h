9f1479c872ef6644271513cc0a3ae57628c74b5f
cbossut <bossut.clement@gmail.com>
2014-09-09 10:49:52 +0200

    Network Headers WIP

---
+++ Headers/Network/Device.h
@@ -0,0 +1,18 @@
+/*!
+ * \file Device.h
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#ifndef DEVICE_H_
+#define DEVICE_H_
+
+
+
+
+
+#endif /* DEVICE_H_ */
---
+++ Headers/Network/Protocol.h
@@ -0,0 +1,28 @@
+/*!
+ * \file Protocol.h
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#ifndef PROTOCOL_H_
+#define PROTOCOL_H_
+
+namespace OSSIA {
+
+class Protocol {
+
+public:
+
+  virtual ~Protocol() = 0;
+
+};
+
+}
+
+
+
+#endif /* PROTOCOL_H_ */
