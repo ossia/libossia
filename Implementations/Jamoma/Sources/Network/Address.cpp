788126b329da4f1ad1752edc7cd916a5f3d0dbc7
theod <theo.delahogue@gmea.net>
2014-11-03 12:29:43 +0100

    More work on Address and Device implementations

--- Implementations/Jamoma/Sources/Network/Address.cpp
+++ Implementations/Jamoma/Sources/Network/Address.cpp
@@ -10,12 +10,16 @@
 
 #include "Network/Address.h"
 
+#include "TTModular.h"
+
 namespace OSSIA {
 
 template <typename T>
 class Address<T>::Impl {
     
   public:
+  
+  TTAddress mAddress;
   
   Impl()
   {
@@ -74,37 +78,38 @@ Address<T> Address<T>::addAddress(std::string, U min, U max) const
 template <typename T>
 bool Address<T>::isRoot() const
 {
-  ;
+  return pimpl->mAddress == kTTAdrsRoot;
 }
   
 template <typename T>
 Address<T>& Address<T>::getParent() const
 {
-  ;
+  // théo : one way to do this but there is no such a constructor
+  //return Address(pimpl->mAddress.getParent().c_str());
 }
   
 template <typename T>
 Device<T>& Address<T>::getDevice() const
 {
-  ;
+  ; // théo : how to retreive the device ? should we have it as a member of the class ?
 }
 
 template <typename T>
-void addChild(const Address<T>&)
+void Address<T>::addChild(const Address<T>&)
  {
-   ;
+   ; // théo : here do we need the device too ?
  }
   
 template <typename T>
-bool removeChild(const Address<T>&)
+bool Address<T>::removeChild(const Address<T>&)
 {
-  ;
+  ; // théo : here do we need the device too ?
 }
 
 template <typename T>
-std::string getAddress()
+std::string Address<T>::getAddress()
 {
-  ;
+  return pimpl->mAddress.c_str();
 }
 
 }
