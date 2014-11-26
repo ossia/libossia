9fcc71f537abdde9f3ae775f9e782f2c6e204f05
theod <theo.delahogue@gmea.net>
2014-11-26 17:24:15 +0100

    Working on State_Impl

--- Implementations/Jamoma/Sources/Editor/State_Impl.cpp
+++ Implementations/Jamoma/Sources/Editor/State_Impl.cpp
@@ -12,84 +12,115 @@
 
 namespace OSSIA
 {
-    State* State::create()
+  State* State::create()
+  {
+    // todo : we shouldn't init each time we create an object ...
+    TTFoundationInit("/usr/local/jamoma/");
+    TTModularInit("/usr/local/jamoma/");
+    TTScoreInit("/usr/local/jamoma/");
+    
+    return new State_Impl();
+  }
+  
+  State_Impl::State_Impl()
+  {}
+  
+  State_Impl::~State_Impl()
+  {}
+  
+  void State_Impl::launch() const
+  {
+    for (mLines.begin(); mLines.end(); mLines.next())
     {
-        // todo : we shouldn't init each time we create an object ...
-        TTFoundationInit("/usr/local/jamoma/");
-        TTModularInit("/usr/local/jamoma/");
-        TTScoreInit("/usr/local/jamoma/");
+      TTDictionary line = mLines.current()[0];
+      
+      // retreive the object under the address
+      TTAddress address;
+      TTValue v;
+      line.lookup(kTTSym_address, v);
+      address = v[0];
+      
+      TTNodeDirectoryPtr directory = accessApplicationDirectoryFrom(address);
+      if (directory == NULL)
+        continue;
+      
+      TTNodePtr node;
+      TTErr err = directory->getTTNode(address, &node);
+      
+      if (!err)
+      {
+        TTObject object = node->getObject();
         
-        return new State_Impl();
-    }
-    
-    State_Impl::State_Impl() :
-    mScript("Script"),
-    mRunning(NO)
-    {}
-    
-    State_Impl::~State_Impl()
-    {}
-    
-    void State_Impl::launch() const
-    {
-        if (!mRunning)
+        if (object.valid())
         {
-            mRunning = YES;
-            
-            mScript.send(kTTSym_Run);
-            
-            mRunning = NO;
+          // for data object
+          if (object.name() == kTTSym_Data)
+          {
+            // send the line using the command message
+            if (address.getAttribute() == kTTSym_value)
+            {
+              object.send(kTTSym_Command, line); // TODO : TTData::Command should handles TTDictionary
+              continue;
+            }
+          }
+          
+          // any other case : set attribute
+          line.getValue(v);
+          object.set(address.getAttribute(), v);
         }
+      }
     }
+  }
+  
+  class State::const_iterator
+  {};
+  
+  State::const_iterator State_Impl::begin() const
+  {}
+  
+  State::const_iterator State_Impl::end() const
+  {}
+  
+  State::const_iterator State_Impl::find(const StateElement&) const
+  {}
+  
+  // Managing StateElements
+  void State_Impl::addStateElement(const StateElement& stateElement)
+  {
+    if (stateElement.getType() != StateElement::StateElementType::MESSAGE_TYPE)
+      return;
     
-    class State::const_iterator
-    {};
+    Message       message = stateElement;
+    TTAddress     address = message.getAddress();
+    TTValue       value = message.getValue();
+    TTDictionary  line;
     
-    State::const_iterator State_Impl::begin() const
-    {}
+    line.setSchema(kTTSym_command);
+    line.setValue(value);
     
-    State::const_iterator State_Impl::end() const
-    {}
+    // default attribute is value attribute
+    if (address.getAttribute() == kTTSymEmpty)
+      address.appendAttribute(kTTSym_value);
     
-    State::const_iterator State_Impl::find(const StateElement&) const
-    {}
+    line.append(kTTSym_address, address);
     
-    // Managing StateElements
-    void State_Impl::addStateElement(const StateElement& stateElement)
-    {
-        if (stateElement.getType() != StateElement::StateElementType::MESSAGE_TYPE)
-            return;
-        
-        Message     message = stateElement;
-        TTAddress   address = message.getAddress();
-        TTValuePtr  value = message.getValue();
-        
-        // get the lines of the state
-        TTValue v;
-        mScript.get("flattenedLines", v);
-        TTListPtr flattenedLines = TTListPtr(TTPtr(v[0]));
-        
-        // find the line at address
-        TTErr err = flattenedLines->find(&TTScriptFindAddress, (TTPtr)&address, v);
-        
-        // the line doesn't exist : append it to the state
-        if (err)
-        {
-            TTValue command = *value;
-            command.prepend(address);
-            
-            mScript.send("AppendCommand", command);
-        }
-        // the line exists : replace the value
-        else
-        {
-            TTDictionaryBasePtr aLine = TTDictionaryBasePtr((TTPtr)v[0]);
-            aLine->setValue(*value);
-        }
-    }
+    mLines.append(line);
+  }
+  
+  bool State_Impl::removeStateElement(const StateElement&)
+  {
+    return false;
+  }
+  
+  void State_ImplFindAddress(const TTValue& lineValue, TTPtr addressPtrToMatch, TTBoolean& found)
+  {
+	TTDictionary  line = lineValue[0];
+	TTAddress     address;
+    TTValue       v;
+
+	line.lookup(kTTSym_address, v);
+    address = v[0];
     
-    bool State_Impl::removeStateElement(const StateElement&)
-    {
-        return false;
-    }
+	found = address == *((TTAddress*)addressPtrToMatch);
+  }
 }
