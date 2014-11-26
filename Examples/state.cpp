bc05861295dfc57e9dd6f935844400c41d943d17
theod <theo.delahogue@gmea.net>
2014-11-26 12:16:10 +0100

    Adding state example

---
+++ Examples/state.cpp
@@ -0,0 +1,22 @@
+/*!
+ * \file state.cpp
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#include "Editor/State.h"
+
+using namespace OSSIA;
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    ;
+}
