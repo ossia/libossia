0ece7b055ebac5b9cfc2cc8c97fa3a0eafd751f3
cbossut <bossut.clement@gmail.com>
2014-12-01 16:41:59 +0100

    Turn State into a std container

--- Headers/Editor/State.h
+++ Headers/Editor/State.h
@@ -29,15 +29,33 @@ public:
   // Lecture
   virtual void launch() const override = 0;
 
-  // Iterators
-  class const_iterator; // bidirectional
+  // Std container
+  class iterator;
+  class const_iterator;
+  class reverse_iterator;
+  class const_reverse_iterator;
+  class size_type;
+  virtual iterator begin() = 0;
+  virtual iterator end() = 0;
+  virtual reverse_iterator rbegin() = 0;
+  virtual reverse_iterator rend() = 0;
   virtual const_iterator begin() const = 0;
   virtual const_iterator end() const = 0;
-  virtual const_iterator find(const StateElement&) const = 0;
-
-  // Managing StateElements
-  virtual void addStateElement(const StateElement&) = 0;
-  virtual bool removeStateElement(const StateElement&) = 0;
+  virtual const_reverse_iterator rbegin() const = 0;
+  virtual const_reverse_iterator rend() const = 0;
+  virtual const_iterator cbegin() const = 0;
+  virtual const_iterator cend() const = 0;
+  virtual const_reverse_iterator crbegin() const = 0;
+  virtual const_reverse_iterator crend() const = 0;
+  virtual size_type size() const = 0;
+  virtual bool empty() const = 0;
+  virtual StateElement*& front() = 0;
+  virtual const StateElement*& front() const = 0;
+  virtual StateElement*& back() = 0;
+  virtual const StateElement*& back() const = 0;
+  virtual iterator insert(const_iterator, StateElement*) = 0;
+  virtual iterator erase(const_iterator) = 0;
+  virtual iterator erase(const_iterator first, const_iterator last) = 0;
 
 };
 
