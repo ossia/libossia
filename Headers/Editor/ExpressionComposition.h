e8f1dfdf93c218f332e0830462dc7e718804f3fb
Clément Bossut <bossut.clement@gmail.com>
2015-02-13 18:35:31 +0100

    Add ExpressionComposition
    Last part of the decomposition of Expression into
    ExpressionAtom and ExpressionComposition

---
+++ Headers/Editor/ExpressionComposition.h
@@ -0,0 +1,49 @@
+/*!
+ * \file ExpressionComposition.h
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#ifndef EXPRESSIONCOMPOSITION_H_
+#define EXPRESSIONCOMPOSITION_H_
+
+namespace OSSIA {
+
+class ExpressionComposition : public Expression {
+
+public:
+
+  enum class Operator {
+    NOT,
+    AND,
+    OR,
+    XOR
+  };
+
+  // Factories, destructor
+  static std::shared_ptr<ExpressionComposition>
+        create(std::shared_ptr<ExpressionAtom>,
+               Operator,
+               std::shared_ptr<ExpressionAtom>/*todo = NO_EXPRESSION*/);
+  virtual std::shared_ptr<ExpressionComposition> clone() const = 0;
+  virtual ~ExpressionComposition() = default;
+
+  // Lecture
+  virtual void evaluate() const override = 0;
+
+  // Accessors
+  virtual ExpressionAtom & getFirstOperand() const = 0;
+  virtual ExpressionAtom & getSecondOperand() const = 0;
+  virtual Operator getOperator() const = 0;
+
+};
+
+}
+
+
+
+#endif /* EXPRESSIONCOMPOSITION_H_ */
