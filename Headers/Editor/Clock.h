5bb3dda6a300e75e1c3ef870ee8972fc5b41de7b
theod <theo.delahogue@gmea.net>
2015-03-18 23:20:12 +0100

    Adding a clock class (instead of a scheduler class)

---
+++ Headers/Editor/Clock.h
@@ -0,0 +1,26 @@
+/*!
+ * \file Clock.h
+ *
+ * \author Clément Bossut
+ * \author Théo de la Hogue
+ *
+ * This code is licensed under the terms of the "CeCILL-C"
+ * http://www.cecill.info
+ */
+
+#ifndef CLOCK_H_
+#define CLOCK_H_
+
+namespace OSSIA {
+
+class Clock {
+
+public:
+
+  virtual ~Clock() = default;
+
+};
+
+}
+
+#endif /* CLOCK_H_ */
