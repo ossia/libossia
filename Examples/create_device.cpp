64300329c0a1f91e1ebfb78909220d461eeb0683
theod <theo.delahogue@gmea.net>
2015-03-25 12:55:55 +0100

    Updating device example

--- Examples/create_device.cpp
+++ Examples/create_device.cpp
@@ -18,27 +18,37 @@
 using namespace OSSIA;
 using namespace std;
 
-int main() {
+int main()
+{
+  // Minuit device creation
+  Minuit minuitDeviceParameters{"MinuitDevice1", "127.0.0.1", 9998};
+  auto minuitDevice = Device::create(minuitDeviceParameters);
+  
+  // Minuit tree building
+  minuitDevice->updateNamespace();
+  
+  // OSC device creation
+  OSC oscDeviceParameters{"127.0.0.1", 9996, 9997};
+  auto oscDevice = Device::create(oscDeviceParameters);
 
-  Minuit myProt{"MinuitDevice1", "1.2.3.4", 66666};
-  auto minuitDevice = Device::create(myProt);
-
-  auto test = minuitDevice->emplace(minuitDevice->begin(), "test");
+  // OSC tree building
+  auto test = oscDevice->emplace(oscDevice->begin(), "test");
   test->createAddress(AddressValue::Type::BOOL);
-  auto done = minuitDevice->emplace(minuitDevice->begin(), "done");
+  
+  auto done = oscDevice->emplace(oscDevice->begin(), "done");
   done->createAddress(AddressValue::Type::NONE);
 
   auto test1 = test->emplace(test->begin(), "1");
   test1->createAddress(AddressValue::Type::INT);
+  
   auto test2 = test->emplace(test->begin(), "2");
   test2->createAddress(AddressValue::Type::STRING);
+  
   auto test3 = test->emplace(test->begin(), "3");
-  /* todo more to think about Tuples :
-  test3->createAddress(AddressValue::Type::INT, AddressValue::Type::INT); */
+  /* todo: more to think about Tuples
+  test3->createAddress(AddressValue::Type::INT, AddressValue::Type::INT); 
+   */
+  
   auto test4 = test->emplace(test->begin(), "4");
-  test4->createAddress(AddressValue::Type::INT);//todo setDomain(0,100)
-
-  OSC PrOSCol{"0.0.0.0", 1000, 1001};
-  auto oscDevice = Device::create(PrOSCol);
-
+  test4->createAddress(AddressValue::Type::INT); // todo: setDomain(0,100)
 }
