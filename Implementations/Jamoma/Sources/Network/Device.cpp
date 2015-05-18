78a017d57f5b2ef7f36ab44b344d0fc0ea9fca45
theod <theo.delahogue@gmea.net>
2015-05-18 12:36:52 +0200

    Removing Jamoma support as it is easy to get it install into /usr/local/jamoma with CMake system

--- Implementations/Jamoma/Sources/Network/Device.cpp
+++ Implementations/Jamoma/Sources/Network/Device.cpp
@@ -13,7 +13,7 @@ class JamomaDevice : public Device, public JamomaNode
 
 private:
   
-  // Implemenatation specific
+  // Implementation specific
   TTObject mApplicationManager;
   TTObject mApplication;
   
@@ -22,9 +22,11 @@ public:
   // Constructor, destructor
   JamomaDevice(Protocol & protocol, string name = "") : JamomaNode(name)
   {
+    TTSymbol device_name(name);
+    
     // todo : we shouldn't init each time we create an object ...
-    TTFoundationInit("/usr/local/jamoma/");
-    TTModularInit("/usr/local/jamoma/");
+    TTFoundationInit("/usr/local/jamoma/extensions/");
+    TTModularInit("/usr/local/jamoma/extensions");
     
     // if no application manager
     if (TTModularApplicationManager == NULL)
@@ -32,37 +34,101 @@ public:
     else
       mApplicationManager = TTObjectBasePtr(TTModularApplicationManager);
     
+    // is the application already exist ?
+    mApplication = mApplicationManager.send("ApplicationFind", device_name);
+    if (mApplication.valid())
+    {
+      TTLogError("%s device created already exist\n");
+      return;
+    }
+    
     // which protocol is it ?
     // todo: this is not a good way to do as if a new protocol appears we have to create a case for it here
     Local* local_protocol = dynamic_cast<Local*>(&protocol);
     if (local_protocol)
     {
-      TTLogMessage("Local device created");
+      // create a local application
+      mApplication = mApplicationManager.send("ApplicationInstantiateLocal", device_name);
+
+      // create
+      TTLogMessage("Local device created\n");
       return;
     }
     
     Minuit* minuit_protocol = dynamic_cast<Minuit*>(&protocol);
     if (minuit_protocol)
     {
-      TTLogMessage("Minuit device created");
+      // create a distante application
+      mApplication = mApplicationManager.send("ApplicationInstantiateDistant", device_name);
+      
+      // create a Minuit protocol unit
+      TTObject protocolMinuit = mApplicationManager.send("ProtocolFind", "Minuit");
+      if (!protocolMinuit.valid())
+        protocolMinuit = mApplicationManager.send("ProtocolInstantiate", "Minuit");
+      
+      // register local application to the Minuit protocol
+      TTSymbol local_device_name;
+      if (!mApplicationManager.get("applicationLocalName", local_device_name))
+        protocolMinuit.send("ApplicationRegister", local_device_name);
+      else
+        TTLogError("Local device doesn't exist\n");
+      
+      // register the application to the Minuit protocol and set paramaters up
+      protocolMinuit.send("Stop");
+      protocolMinuit.send("ApplicationRegister", device_name);
+      protocolMinuit.send("ApplicationSelect", device_name);
+      //protocolMinuit.set("port", minuit_protocol->port);
+      //protocolMinuit.set("ip", TTSymbol(minuit_protocol->ip));
+      protocolMinuit.send("Run");
+      
+      TTLogMessage("Minuit device created\n");
       return;
     }
     
     OSC* osc_protocol = dynamic_cast<OSC*>(&protocol);
     if (osc_protocol)
     {
-      TTLogMessage("OSC device created");
+      // create a distante application
+      mApplication = mApplicationManager.send("ApplicationInstantiateDistant", device_name);
+      
+      // create an OSC protocol unit
+      TTObject protocolOSC = mApplicationManager.send("ProtocolFind", "OSC");
+      if (!protocolOSC.valid())
+        protocolOSC = mApplicationManager.send("ProtocolInstantiate", "OSC");
+      
+      // register local application to the OSC protocol
+      TTSymbol local_device_name;
+      if (!mApplicationManager.get("applicationLocalName", local_device_name))
+        protocolOSC.send("ApplicationRegister", local_device_name);
+      else
+        TTLogError("Local device doesn't exist");
+      
+      // register the application to the OSC protocol and set paramaters up
+      protocolOSC.send("Stop");
+      protocolOSC.send("ApplicationRegister", device_name);
+      protocolOSC.send("ApplicationSelect", device_name);
+      TTValue ports(osc_protocol->in_port, osc_protocol->out_port);
+      protocolOSC.set("port", ports);
+      protocolOSC.set("ip", TTSymbol(osc_protocol->ip));
+      protocolOSC.send("Run");
+      
+      TTLogMessage("OSC device created\n");
       return;
     }
   }
   
   ~JamomaDevice()
-  {}
+  {
+    TTSymbol device_name;
+    mApplication.get("name", device_name);
+    mApplicationManager.send("ApplicationRelease", device_name);
+  }
 
   // Network
   virtual bool updateNamespace() override
   {
-    return false;
+    TTErr err = mApplication.send("DirectoryBuild");
+    return err == kTTErrNone;
   }
 
 };
--- Implementations/Jamoma/support/includes/TTExtensionLoader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// If no path is specified and loadFromBuiltinPaths == false, only try to load from the computed relative path.
-// Else if loadFromBuiltinPaths == true, try to load from the builtin paths
-// Else if there is a path, and loadFromBuiltinPaths == false, only try to load from the given path
-// Else try to load from the given path, and if it does not work, from the computed path,
-// and if it still does not work, from the builtin paths.
-void TTLoadExtensions(const char* pathToLoadFrom = nullptr, bool loadFromBuiltinPaths = false);
