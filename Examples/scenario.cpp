ff655e11c84c510d705400539adb9b22e7aaba04
theod <theo.delahogue@gmea.net>
2015-06-15 17:27:36 +0200

    example of how we want to use the Editor

--- Examples/scenario.cpp
+++ Examples/scenario.cpp
@@ -20,27 +20,79 @@ using namespace std;
 
 int main()
 {
-    // create a scenario
-    auto scenario = Scenario::create();
+    /* 
+     Network setup
+     */
+    
+    // create a Local device "i-score"
+    Local local_device_parameters{};
+    auto local_device = Device::create(local_device_parameters, "i-score");
+    
+    // add a /play address
+    auto local_play_node = localDevice->emplace(localDevice->children().cend(), "play");
+    auto local_play_address = (*local_play_node)->createAddress(AddressValue::Type::BOOL);
+    
+    /* 
+     Main Scenario setup
+     */
+    
+    // create two TimeNodes for the start and the end
+    TimeNode main_start = TimeNode::create(); // implicit creation of a first event into the node
+    TimeNode main_end = TimeNode::create(); // implicit creation of an end event into the node
+    
+    // get TimeEvents of the start and end TimeNodes to make them interactive to /play address
+    TimeEvent main_start_event =  main_start->timeEvents()->begin();
+    TimeEvent main_end_event =  main_end->timeEvents()->begin();
+    
+    // create "/play == true" and "/play == false" expressions
+    auto play_expression_start = ExpressionAtom::create(ExpressionValue::create(local_play_address),
+                                                        ExpressionAtom::Operator::EQUAL,
+                                                        ExpressionValue::create(Bool(true)));
+    
+    auto play_expression_end = ExpressionAtom::create(ExpressionValue::create(local_play_address),
+                                                      ExpressionAtom::Operator::EQUAL,
+                                                      ExpressionValue::create(Bool(false)));
+    
+    // make start event to be interactive to "/play == true" and end event to be interactive to "/play == false"
+    main_start_event->setExpression(play_expression_start);
+    main_end_event->setExpression(play_expression_end);
+    
+    // create the main TimeConstraint passing the first event of each TimeNode
+    TimeValue main_duration(30000);
+    auto main_constraint = TimeConstraint::create(main_duration, main_start_event, main_end_event);
+    
+    // create the main Scenario and add it to the main TimeConstraint
+    auto main_scenario = Scenario::create(); // implicit creation of a start state and end state, implicit creation of first and last TimeNode
+    main_constraint->timeProcesses().push_back(main_scenario);
+    
+    /* 
+     Main Scenario edition
+     */
 
-    // create first node of the scenario
-    auto startScenarioNode = TimeNode::create();
-    scenario->setStartNode(startScenarioNode);
-
-    // create last node of the scenario
-    auto endScenarioNode = TimeNode::create();
-    scenario->setEndNode(endScenarioNode);
-
-    // create a time constraint of 2000 ms
-    TimeValue duration(2000);
-    auto constraint = TimeConstraint::create(duration, duration, duration);
-
-    // add time contraint from start to the end of the scenario
-    scenario->addConstraint(constraint, scenario->getStartNode(), scenario->getEndNode());
-
-    // tell the scenario to kill children processes when it ends
-    scenario->setKiller(true);
-
+    // get the start TimeEvent of the main Scenario
+    auto first_start_event = scenario->getStartNode()->timeEvents()->begin();
+    
+    // create a TimeNode and get its TimeEvent
+    TimeNode first_end_node = TimeNode::create();
+    auto first_end_event = first_start_node->timeEvents()->begin();
+    
+    // create a TimeConstraint between the two TimeEvents
+    TimeValue first_duration(3000);
+    auto first_constraint = TimeConstraint::create(first_duration, first_start_event, first_end_event);
+    
+    // add the new TimeConstraint to the main Scenario
+    main_scenario->addConstraint(main_scenario->getStartNode(), first_constraint, first_end_node);
+    
+    // create an automation into the new TimeConstraint
+    auto first_automation = Automation<double>::create();
+    first_constraint->timeProcesses().push_back(first_automation);
+    
+    //! \todo create a curve
+    
+    //! \todo create an independent state into an event
+    //auto new_event = first_end_event->timeEvents()->begin();
+    //event.addState(...);
+    
     // play the scenario
     scenario->play();
 
