baad420f15b1a2a6731e039c971b6ae94052e61f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-06-15 15:36:49 +0200

    Update ExpressionValue.h

--- Headers/Editor/ExpressionValue.h
+++ Headers/Editor/ExpressionValue.h
@@ -19,8 +19,18 @@ namespace OSSIA {
 class ExpressionValue {
 
     public:
-
+      enum class Type { Address, Value };
       virtual ~ExpressionValue() = default;
+      
+      // What is currently in the union
+      virtual Type getCurrentType() const = 0;
+      
+      // If the ExpressionValue is an address, then it is returned, else an empty shared_ptr
+      virtual std::shared_ptr<Address> getAddress() const = 0;
+      
+      // If the ExpressionValue is a fixed value, then it is returned, else an empty shared_ptr
+      virtual std::shared_ptr<AddressValue> getValue() const = 0;
+      
 
     };
 
