4d65f78171943116e7d559fe389046e73b607e9d
theod <theo.delahogue@gmea.net>
2015-06-18 15:40:38 +0200

    Updating the scenario example

--- Examples/scenario.cpp
+++ Examples/scenario.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <memory>
 
+#include "Editor/Automation.h"
 #include "Editor/Clock.h"
 #include "Editor/Expression.h"
 #include "Editor/ExpressionAtom.h"
@@ -89,7 +90,7 @@ int main()
     main_constraint->timeProcesses().push_back(main_scenario);
 
     /* 
-     Main Scenario edition
+     Main Scenario edition : creation of a TimeConstraint
      */
     
     // get the start node of the main Scenario
@@ -101,30 +102,34 @@ int main()
     // create a TimeEvent inside the scenario start node without Expression
     auto first_start_event = *(scenario_start_node->emplace(scenario_start_node->timeEvents().begin()));
     
-    // add "/test 0." message to start event's state
-    Float zero(0.);
-    auto first_start_message = Message::create(local_test_address, &zero);
-    first_start_event->getState()->stateElements().push_back(first_start_message);
-    
     // create a TimeEvent inside the end node without Expression
     auto first_end_event = *(first_end_node->emplace(first_end_node->timeEvents().begin()));
     
-    // add "/test 1." message to end event's state
+    // create a TimeConstraint between the two TimeEvents
+    TimeValue first_duration(3000.);
+    auto first_constraint = TimeConstraint::create(first_start_event, first_end_event, first_duration, first_duration, first_duration);
+    
+    // add the first TimeConstraint to the main Scenario
+    main_scenario->addConstraint(first_constraint);
+
+    /*
+     Main Scenario edition : creation of an Automation
+     */
+    
+    // create an automation into the first TimeConstraint
+    auto first_automation = Automation<double>::create(first_constraint);
+    
+    // add "/test 0." message to Automation's start state
+    Float zero(0.);
+    auto first_start_message = Message::create(local_test_address, &zero);
+    first_automation->getStartState()->stateElements().push_back(first_start_message);
+    
+    // add "/test 1." message to Automation's end state
     Float one(1.);
     auto first_end_message = Message::create(local_test_address, &one);
-    first_start_event->getState()->stateElements().push_back(first_end_message);
+    first_automation->getEndState()->stateElements().push_back(first_end_message);
     
-    // create a TimeConstraint between the two TimeEvents
-    TimeValue first_duration(3000);
-    auto first_constraint = TimeConstraint::create(first_duration, first_start_event, first_end_event, first_duration, first_duration);
-    
-    // add the new TimeConstraint to the main Scenario
-    main_scenario->addConstraint(first_constraint);
 /*
-    // create an automation into the new TimeConstraint
-    auto first_automation = Automation<double>::create();
-    first_constraint->timeProcesses().push_back(first_automation);
-    
     //! \todo create a curve
     
     //! \todo create an independent state into an event
