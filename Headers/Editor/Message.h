373fd3c2a8f302f5c9d48b89f70312c1cf7a5e69
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-06-26 15:51:54 +0200

    Add Message.cpp to CMakeLists

--- Headers/Editor/Message.h
+++ Headers/Editor/Message.h
@@ -31,33 +31,34 @@ public:
 
 # pragma mark -
 # pragma mark Life cycle
-  
+
   /*! factory
    \param std::shared_ptr<#Address> where to send the value
    \param const #Value the value
    \return std::shared_ptr<#Message> */
   static std::shared_ptr<Message> create(std::shared_ptr<Address>,
                                          const Value*);
-  
+
   /*! clone */
   virtual std::shared_ptr<Message> clone() const = 0;
-  
+
   /*! destructor */
   virtual ~Message() = default;
 
 # pragma mark -
 # pragma mark Execution
-  
+
   /*! send the value to the address */
+  // TODO what is the point of this if launch() is already in the parent class?
   virtual void launch() const override = 0;
 
 # pragma mark -
 # pragma mark Accessors
-  
+
   /*! get message's address
    \return std::shared_ptr<#Address> the address */
   virtual const std::shared_ptr<Address> & getAddress() const = 0;
-  
+
   /*! get message's value
    \return #Value the value */
   virtual const Value * getValue() const = 0;
