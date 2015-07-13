770fb166bb4bfe0a7ea20651f57927901b9535f1
theod <theo.delahogue@gmea.net>
2015-07-13 17:19:08 +0200

    Adding tests for TimeEvent edition functions

--- Tests/Editor/TimeEventTest.cpp
+++ Tests/Editor/TimeEventTest.cpp
@@ -4,35 +4,61 @@
 
 using namespace OSSIA;
 
+void event_callback(TimeEvent::Status newStatus, TimeEvent::Status oldStatus)
+{
+    ;
+}
+
 class TimeEventTest : public QObject
 {
     Q_OBJECT
-
+    
 private Q_SLOTS:
-    void testCase1()
+    
+    void test_basic()
     {
-        auto scenar = Scenario::create();
-        auto startTimeNode = TimeNode::create();
-        auto endTimeNode = TimeNode::create();
-        TimeValue d;
-        auto cstr = TimeConstraint::create(d,d,d);
-        auto autom = Automation<double>::create();
-
-        scenar->addConstraint(*cstr, *startTimeNode, *endTimeNode);
-        cstr->timeProcesses().insert(cstr->timeProcesses().begin(), autom);
-
-        auto startEv = autom->getStartEvent();
-        auto endEv = autom->getEndEvent();
-/*
-        QVERIFY(startEv->getNextProcesses().size() == 1);
-        QVERIFY(endEv->getPreviousProcesses().size() == 1);
-        QCOMPARE(startEv->getState(), autom->getStartState());
-        QCOMPARE(endEv->getState(), autom->getEndState());
-        startEv->getExpression();
-//*/
+        auto node = TimeNode::create();
+        auto expression = ExpressionAtom::create(&True, ExpressionAtom::Operator::EQUAL, &True);
+        auto event = *(node->emplace(node->timeEvents().begin(), &event_callback, expression));
+        QVERIFY(event != nullptr);
+        
+        auto event_node = event->getTimeNode();
+        QVERIFY(event_node == node);
+        
+        auto state = event->getState();
+        QVERIFY(state != nullptr);
+        
+        auto event_expression = event->getExpression();
+        QVERIFY(event_expression == expression);
+        
+        QVERIFY(event->getStatus() == TimeEvent::Status::WAITING);
+    }
+    
+    void test_edition()
+    {
+        auto nodeA = TimeNode::create();
+        auto eventA = *(nodeA->emplace(nodeA->timeEvents().begin(), &event_callback));
+        
+        auto nodeB = TimeNode::create();
+        auto eventB = *(nodeB->emplace(nodeB->timeEvents().begin(), &event_callback));
+        
+        auto nodeC = TimeNode::create();
+        auto eventC = *(nodeC->emplace(nodeC->timeEvents().begin(), &event_callback));
+        
+        auto constraint1 = TimeConstraint::create(eventA, eventB, 1000.);
+        auto constraint2 = TimeConstraint::create(eventB, eventC, 1000.);
+        auto constraint3 = TimeConstraint::create(eventA, eventC, 2000.);
+        
+        QVERIFY(eventA->previousTimeConstraints().size() == 0);
+        QVERIFY(eventA->nextTimeConstraints().size() == 2);
+        
+        QVERIFY(eventB->previousTimeConstraints().size() == 1);
+        QVERIFY(eventB->nextTimeConstraints().size() == 1);
+        
+        QVERIFY(eventC->previousTimeConstraints().size() == 2);
+        QVERIFY(eventC->nextTimeConstraints().size() == 0);
     }
 };
-
 
 QTEST_APPLESS_MAIN(TimeEventTest)
 
