6d9e01bd4eef2a07a857eeffdd9e5da756a7a68c
theod <theo.delahogue@gmea.net>
2015-07-14 17:29:55 +0200

    Adding tests for Automation and Clock

--- Tests/Editor/AutomationTest.cpp
+++ Tests/Editor/AutomationTest.cpp
@@ -4,6 +4,11 @@
 
 using namespace OSSIA;
 
+void automation_callback(const TimeValue&, const TimeValue&, std::shared_ptr<State>)
+{
+    ;
+}
+
 class AutomationTest : public QObject
 {
     Q_OBJECT
@@ -13,11 +18,23 @@ private Q_SLOTS:
     /*! test life cycle and accessors functions */
     void test_basic()
     {
-        ;
+        Local local_protocol{};
+        auto local_device = Device::create(local_protocol, "test");
+
+        local_device->emplace(local_device->children().begin(), "child");
+        auto address = local_device->children().front()->createAddress(Value::Type::FLOAT);
+
+        Float f(0);
+        auto automation = Automation::create(&automation_callback, address, &f);
+        QVERIFY(automation != nullptr);
+
+        QVERIFY(automation->getDrivenAddress() == address);
+
+        QVERIFY(*automation->getDriving() == f);
     }
     
-    /*! test edition functions */
-    void test_edition()
+    /*! test execution functions */
+    void test_execution()
     {
         ;
     }
--- Tests/Editor/ClockTest.cpp
+++ Tests/Editor/ClockTest.cpp
@@ -4,6 +4,11 @@
 
 using namespace OSSIA;
 
+void clock_callback(const TimeValue&, const TimeValue&)
+{
+    ;
+}
+
 class ClockTest : public QObject
 {
     Q_OBJECT
@@ -13,11 +18,37 @@ private Q_SLOTS:
     /*! test life cycle and accessors functions */
     void test_basic()
     {
-        ;
+        auto clock = Clock::create();
+        auto callback = Clock::ExecutionCallback(&clock_callback);
+
+        QVERIFY(clock->getDuration() == 0.);
+        QVERIFY(clock->getGranularity() == 1.);
+        QVERIFY(clock->getOffset() == 0.);
+        QVERIFY(clock->getSpeed() == 1.);
+        QVERIFY(clock->getExternal() == false);
+        QVERIFY(clock->getExecutionCallback() == nullptr);
+
+        clock->setDuration(1000.);
+        clock->setGranularity(50.);
+        clock->setOffset(500.);
+        clock->setSpeed(2.);
+        clock->setExternal(true);
+        clock->setExecutionCallback(callback);
+
+        QVERIFY(clock->getDuration() == 1000.);
+        QVERIFY(clock->getGranularity() == 50.);
+        QVERIFY(clock->getOffset() == 500.);
+        QVERIFY(clock->getSpeed() == 2.);
+        QVERIFY(clock->getExternal() == true);
+        QVERIFY(clock->getExecutionCallback() != nullptr);
+
+        QVERIFY(clock->getRunning() == false);
+        QVERIFY(clock->getPosition() == 0.5);
+        QVERIFY(clock->getDate() == 500.);
     }
     
-    /*! test edition functions */
-    void test_edition()
+    /*! test execution functions */
+    void test_execution()
     {
         ;
     }
