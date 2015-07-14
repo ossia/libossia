4105279cb05e1d1dd806f0ffd3dd087810c8e00d
theod <theo.delahogue@gmea.net>
2015-07-14 18:13:07 +0200

    Adding tests for Curve and Domain

--- Tests/Editor/CurveTest.cpp
+++ Tests/Editor/CurveTest.cpp
@@ -13,13 +13,46 @@ private Q_SLOTS:
     /*! test life cycle and accessors functions */
     void test_basic()
     {
-        ;
-    }
-    
-    /*! test edition functions */
-    void test_edition()
-    {
-        ;
+        auto curve = Curve<float>::create();
+        QVERIFY(curve != nullptr);
+
+        auto linearSegment = CurveSegmentLinear<float>::create(curve);
+
+        curve->setInitialValue(0.);
+        QVERIFY(curve->getInitialValue() == 0.);
+
+        curve->addPoint(1., 1., linearSegment);
+        QVERIFY(curve->getPointsMap().size() == 1);
+        //QVERIFY(curve->segments().size() == 1);
+
+        curve->addPoint(2., 0., linearSegment);
+        QVERIFY(curve->getPointsMap().size() == 2);
+        //QVERIFY(curve->segments().size() == 2);
+
+        QVERIFY(curve->valueAt(0.) == 0.);
+        QVERIFY(curve->valueAt(0.5) == 0.5);
+        QVERIFY(curve->valueAt(1.) == 1.);
+        QVERIFY(curve->valueAt(1.5) == 0.5);
+        QVERIFY(curve->valueAt(2.) == 0.);
+
+        curve->setInitialValue(2.);
+        QVERIFY(curve->getInitialValue() == 2.);
+
+        QVERIFY(curve->valueAt(0.) == 2.);
+        QVERIFY(curve->valueAt(0.5) == 1.5);
+        QVERIFY(curve->valueAt(1.) == 1.);
+        QVERIFY(curve->valueAt(1.5) == 0.5);
+        QVERIFY(curve->valueAt(2.) == 0.);
+
+        curve->removePoint(1.);
+        QVERIFY(curve->getPointsMap().size() == 1);
+        //QVERIFY(curve->segments().size() == 1);
+
+        QVERIFY(curve->valueAt(0.) == 2.);
+        QVERIFY(curve->valueAt(0.5) == 1.5);
+        QVERIFY(curve->valueAt(1.) == 1.);
+        QVERIFY(curve->valueAt(1.5) == 0.5);
+        QVERIFY(curve->valueAt(2.) == 0.);
     }
 };
 
--- Tests/Editor/DomainTest.cpp
+++ Tests/Editor/DomainTest.cpp
@@ -13,13 +13,21 @@ private Q_SLOTS:
     /*! test life cycle and accessors functions */
     void test_basic()
     {
-        ;
-    }
-    
-    /*! test edition functions */
-    void test_edition()
-    {
-        ;
+        auto domain = Domain::create();
+        QVERIFY(domain != nullptr);
+        QVERIFY(*domain->getMin() == Impulse());
+        QVERIFY(*domain->getMax() == Impulse());
+        QVERIFY(domain->getValues().size() == 0);
+
+        domain->setMin(new Int(1));
+        QVERIFY(*domain->getMin() == Int(1));
+
+        domain->setMax(new Int(24));
+        QVERIFY(*domain->getMax() == Int(24));
+
+        std::vector<const Value*> values = {new Int(1), new Int(10), new Int(24)};
+        domain->setValues(values);
+        QVERIFY(domain->getValues().size() == 3);
     }
 };
 
