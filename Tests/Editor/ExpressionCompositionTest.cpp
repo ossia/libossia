c0dd5ed3b7803312e1aa58a2d410e05e8d37eaf6
theod <theo.delahogue@gmea.net>
2015-07-15 10:59:51 +0200

    Testing ExpressionComposition

--- Tests/Editor/ExpressionCompositionTest.cpp
+++ Tests/Editor/ExpressionCompositionTest.cpp
@@ -10,16 +10,88 @@ class ExpressionCompositionTest : public QObject
 
 private Q_SLOTS:
     
-    /*! test life cycle and accessors functions */
-    void test_basic()
+    /*! test AND operator */
+    void test_AND()
     {
-        ;
+        auto exprA = ExpressionAtom::create(new Bool(true),
+                                            ExpressionAtom::Operator::EQUAL,
+                                            new Bool(true));
+
+        auto exprB = ExpressionAtom::create(new Bool(false),
+                                            ExpressionAtom::Operator::EQUAL,
+                                            new Bool(false));
+
+        auto exprC = ExpressionAtom::create(new Bool(false),
+                                            ExpressionAtom::Operator::DIFFERENT,
+                                            new Bool(false));
+
+        auto composition1 = ExpressionComposition::create(exprA,
+                                                          ExpressionComposition::Operator::AND,
+                                                          exprB);
+        QVERIFY(composition1 != nullptr);
+        QVERIFY(composition1->evaluate() == true);
+
+        auto composition2 = ExpressionComposition::create(exprA,
+                                                          ExpressionComposition::Operator::AND,
+                                                          exprC);
+        QVERIFY(composition2 != nullptr);
+        QVERIFY(composition2->evaluate() == false);
     }
-    
-    /*! test edition functions */
-    void test_edition()
+
+    /*! test OR operator */
+    void test_OR()
     {
-        ;
+        auto exprA = ExpressionAtom::create(new Bool(true),
+                                            ExpressionAtom::Operator::EQUAL,
+                                            new Bool(true));
+
+        auto exprB = ExpressionAtom::create(new Bool(false),
+                                            ExpressionAtom::Operator::EQUAL,
+                                            new Bool(false));
+
+        auto exprC = ExpressionAtom::create(new Bool(false),
+                                            ExpressionAtom::Operator::DIFFERENT,
+                                            new Bool(false));
+
+        auto composition1 = ExpressionComposition::create(exprA,
+                                                          ExpressionComposition::Operator::OR,
+                                                          exprB);
+        QVERIFY(composition1 != nullptr);
+        QVERIFY(composition1->evaluate() == true);
+
+        auto composition2 = ExpressionComposition::create(exprA,
+                                                          ExpressionComposition::Operator::OR,
+                                                          exprC);
+        QVERIFY(composition2 != nullptr);
+        QVERIFY(composition2->evaluate() == true);
+    }
+
+    /*! test XOR operator */
+    void test_XOR()
+    {
+        auto exprA = ExpressionAtom::create(new Bool(true),
+                                            ExpressionAtom::Operator::EQUAL,
+                                            new Bool(true));
+
+        auto exprB = ExpressionAtom::create(new Bool(false),
+                                            ExpressionAtom::Operator::EQUAL,
+                                            new Bool(false));
+
+        auto exprC = ExpressionAtom::create(new Bool(false),
+                                            ExpressionAtom::Operator::DIFFERENT,
+                                            new Bool(false));
+
+        auto composition1 = ExpressionComposition::create(exprA,
+                                                          ExpressionComposition::Operator::XOR,
+                                                          exprB);
+        QVERIFY(composition1 != nullptr);
+        QVERIFY(composition1->evaluate() == false);
+
+        auto composition2 = ExpressionComposition::create(exprA,
+                                                          ExpressionComposition::Operator::XOR,
+                                                          exprC);
+        QVERIFY(composition2 != nullptr);
+        QVERIFY(composition2->evaluate() == true);
     }
 };
 
