4da2d6dfbcd73de17357b739e8fc8ee743d11895
theod <theo.delahogue@gmea.net>
2015-07-15 11:06:22 +0200

    Adding tests for Message

--- Tests/Editor/MessageTest.cpp
+++ Tests/Editor/MessageTest.cpp
@@ -13,11 +13,24 @@ private Q_SLOTS:
     /*! test life cycle and accessors functions */
     void test_basic()
     {
-        ;
+        // Local device
+        Local device_parameters{};
+        auto device = Device::create(device_parameters, "test");
+
+        // Local tree building
+        auto localIntNode = *(device->emplace(device->children().cend(), "my_int"));
+        auto localIntAddress = localIntNode->createAddress(Value::Type::INT);
+
+        auto message = Message::create(localIntAddress, new Int(10));
+        QVERIFY(message != nullptr);
+
+        QVERIFY(message->getAddress() == localIntAddress);
+
+        QVERIFY(*message->getValue() == Int(10));
     }
     
-    /*! test edition functions */
-    void test_edition()
+    /*! test execution functions */
+    void test_execution()
     {
         ;
     }
