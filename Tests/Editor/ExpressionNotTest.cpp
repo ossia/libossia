636ee06931974a953c096a50f2f3597e87f02bc9
theod <theo.delahogue@gmea.net>
2015-07-16 14:45:15 +0200

    Adding tests for ExpressionNot

--- Tests/Editor/ExpressionNotTest.cpp
+++ Tests/Editor/ExpressionNotTest.cpp
@@ -13,13 +13,13 @@ private Q_SLOTS:
     /*! test life cycle and accessors functions */
     void test_basic()
     {
-        ;
-    }
-    
-    /*! test edition functions */
-    void test_edition()
-    {
-        ;
+        auto expression = ExpressionAtom::create(new Bool(false),
+                                                 ExpressionAtom::Operator::DIFFERENT,
+                                                 new Bool(false));
+
+        auto not_expression = ExpressionNot::create(expression);
+        QVERIFY(not_expression != nullptr);
+        QVERIFY(not_expression->evaluate() == true);
     }
 };
 
