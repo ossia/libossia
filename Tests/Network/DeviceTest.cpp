7a0240de4beed1f3add7ad87987f0042c6270094
theod <theo.delahogue@gmea.net>
2015-07-21 10:28:47 +0200

    Testing OSC and Minuit device creation

--- Tests/Network/DeviceTest.cpp
+++ Tests/Network/DeviceTest.cpp
@@ -14,8 +14,16 @@ private Q_SLOTS:
     void test_basic()
     {
         Local local_protocol{};
-        auto local_device = Device::create(local_protocol, "test");
+        auto local_device = Device::create(local_protocol, "test_local");
         QVERIFY(local_device != nullptr);
+
+        OSC osc_protocol{"127.0.0.1", 9996, 9997};
+        auto osc_device = Device::create(osc_protocol, "test_osc");
+        QVERIFY(osc_device != nullptr);
+
+        Minuit minuit_protocol{"127.0.0.1", 13579, 13580};
+        auto minuit_device = Device::create(minuit_protocol, "test_minuit");
+        QVERIFY(minuit_device != nullptr);
     }
 };
 
