8c9870c256f3d76a6b225e9d5f9430962c7761fb
theod <theo.delahogue@gmea.net>
2015-07-27 02:41:04 +0200

    commenting setOffset as it still not work

--- Documentation/Examples/scenario.cpp
+++ Documentation/Examples/scenario.cpp
@@ -206,7 +206,8 @@ int main()
     // change main TimeConstraint speed, granularity and offset
     main_constraint->setSpeed(1.);
     main_constraint->setGranularity(50.);
-    main_constraint->setOffset(500.);
+    //! \todo properly setup each constraint offset considering the main constraint offset
+    //! main_constraint->setOffset(500.);
     
     // change first and second TimeConstraint speed and granularity
     first_constraint->setSpeed(1.);
