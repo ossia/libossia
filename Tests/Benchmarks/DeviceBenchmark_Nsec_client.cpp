f7472b31d37749416e99284701e77d30db5c6ea4
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-09-25 16:32:53 +0200

    Add first benchmarks

---
+++ Tests/Benchmarks/DeviceBenchmark_Nsec_client.cpp
@@ -0,0 +1,114 @@
+#include <QtTest>
+#include "../ForwardDeclaration.h"
+#include <iostream>
+#include "Random.hpp"
+#include<thread>
+#include <boost/range/algorithm/find_if.hpp>
+Random r;
+using namespace OSSIA;
+
+std::shared_ptr<OSSIA::Node> goToRandomNode(
+        const std::shared_ptr<OSSIA::Node>& root)
+{
+    // Get a random number between 1 and 100
+    auto depth = 1 + r.getRandomUInt() % 100;
+
+    // Try to go randomly to a node that deep and return it
+    auto currentNode = root;
+    for(int i = 0; i < depth; i++)
+    {
+        if(currentNode->children().empty())
+        {
+            break;
+        }
+
+        auto node_num = r.getRandomUInt() % currentNode->children().size();
+        currentNode = currentNode->children()[node_num];
+
+    }
+
+    return currentNode;
+}
+
+
+class DeviceBenchmark_Nsec_client : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+
+    /*! test life cycle and accessors functions */
+    void test_basic()
+    {
+        // declare this program "A" as Local device
+        auto localProtocol = Local::create();
+        auto localDevice = Device::create(localProtocol, "A");
+
+        // declare a distant program "B" as a Minuit device
+        auto minuitProtocol = Minuit::create("127.0.0.1", 6666, 9999);
+        auto minuitDevice = Device::create(minuitProtocol, "B");
+
+        bool val = minuitDevice->updateNamespace();
+
+        std::shared_ptr<OSSIA::Address> start_addr;
+        std::shared_ptr<OSSIA::Address> stop_addr;
+        for(const auto& node : minuitDevice->children())
+        {
+            if(node->getName() == "startTick")
+                start_addr = node->getAddress();
+            if(node->getName() == "stopTick")
+                stop_addr = node->getAddress();
+            if(start_addr && stop_addr)
+                break;
+        }
+
+        OSSIA::Float f(0);
+
+        bool a = false;
+        bool b = false;
+        std::thread checktime([&] () {
+            a = true;
+            auto t0 = std::chrono::steady_clock::now();
+            while(true)
+            {
+                if(std::chrono::steady_clock::now() - t0 > std::chrono::seconds(10))
+                {
+                    b = true;
+                    break;
+                }
+            }
+        });
+
+        while(!a);
+
+        int num_sent = 0;
+        start_addr->pushValue(new OSSIA::Impulse{});
+        while(!b)
+        {
+            // Select a new node
+            auto n = goToRandomNode(minuitDevice);
+            const auto& name = n->getName();
+            if(name != "startTick" && name != "stopTick")
+            {
+                if(n->getAddress())
+                {
+                    n->getAddress()->pushValue(&f);
+                }
+            }
+
+            num_sent ++;
+        }
+        stop_addr->pushValue(new OSSIA::Impulse{});
+
+        std::cerr << "Sent " << num_sent << " values in 10 seconds" << std::endl;
+
+
+    }
+
+};
+
+
+QTEST_APPLESS_MAIN(DeviceBenchmark_Nsec_client)
+
+#include "DeviceBenchmark_Nsec_client.moc"
+
---
+++ Tests/Benchmarks/Random.hpp
@@ -0,0 +1,109 @@
+#pragma once
+#include <string>
+#include <random>
+#include <unordered_set>
+#include <vector>
+
+void print(const std::vector<std::string>& path)
+{
+    std::string s = "/";
+    for(const auto& e : path)
+        s += e + "/";
+
+    std::cerr << s << std::endl;
+}
+
+struct Random
+{
+        std::mt19937 gen;
+
+        Random():
+            gen(0)
+        {
+
+        }
+
+        // Found on stackoverflow
+        std::string random_string( size_t length )
+        {
+            auto randchar = []() -> char
+            {
+                    static const char charset[] =
+                    "0123456789"
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    "abcdefghijklmnopqrstuvwxyz";
+                    const size_t max_index = (sizeof(charset) - 1);
+                    return charset[ rand() % max_index ]; };
+            std::string str(length,0);
+            std::generate_n( str.begin(), length, randchar );
+            return str;
+        }
+
+        int getRandomInt()
+        {
+            using namespace std;
+            static uniform_int_distribution<int>
+                    dist(numeric_limits<int>::lowest(),
+                         numeric_limits<int>::max());
+
+            return dist(gen);
+        }
+
+        uint32_t getRandomUInt()
+        {
+            using namespace std;
+            static uniform_int_distribution<uint32_t>
+                    dist(numeric_limits<uint32_t>::lowest(),
+                         numeric_limits<uint32_t>::max());
+
+            return dist(gen);
+        }
+
+        float getRandomFloat()
+        {
+            using namespace std;
+            static uniform_real_distribution<float>
+                    dist(numeric_limits<float>::lowest(),
+                         numeric_limits<float>::max());
+
+            return dist(gen);
+        }
+
+
+        std::string getRandomString()
+        {
+            using namespace std;
+            static uniform_int_distribution<int>
+                    dist(1,
+                         15);
+
+            return random_string(dist(gen));
+        }
+
+
+        std::vector<std::vector<std::string>> getRandomAddresses(int num)
+        {
+            std::vector<std::vector<std::string>> dev;
+            dev.push_back({"/"});
+            for(int i = 0; i < num; i++)
+            {
+                int num = rand() % dev.size();
+                auto randompath = dev[num];
+
+                randompath.push_back(random_string(1 + rand() % 20));
+
+                dev.push_back(randompath);
+            }
+
+            // Remove "/" at the beginning of each address
+            for(int i = 0; i < dev.size(); i++)
+            {
+                dev[i].erase(dev[i].begin());
+            }
+
+            // Remove the root address
+            dev.erase(dev.begin());
+            return dev;
+        }
+
+};
