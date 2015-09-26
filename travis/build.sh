c058e3b3ed517b50b33c2ab2b63a8d64577922be
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-09-26 19:58:27 +0200

    Use travis

---
+++ travis/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Note : to make the tests work under travis, they have to be changed in order not to require QApplication but only QCoreApplication
+#    - LD_LIBRARY_PATH=/usr/lib64 make ExperimentalTest
+
+
+mkdir build
+cd build
+export CMAKE_COMMON_FLAGS="-DCMAKE_BUILD_TYPE=$BUILD_TYPE"
+case "$TRAVIS_OS_NAME" in
+  linux)
+    source /opt/qt55/bin/qt55-env.sh
+    /usr/local/bin/cmake $CMAKE_COMMON_FLAGS ..
+        
+    make -j2
+    make ExperimentalTest
+  ;;
+  osx)
+    cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/qt5/5.5.0/lib/cmake;$(pwd)/../Jamoma/share/cmake" $CMAKE_COMMON_FLAGS ..
+        
+    make -j2
+    make ExperimentalTest
+  ;;
+esac
---
+++ travis/deps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -eux
+
+git submodule init
+
+case "$TRAVIS_OS_NAME" in
+  linux)
+    sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+    sudo add-apt-repository --yes ppa:beineri/opt-qt55
+    sudo add-apt-repository --yes ppa:boost-latest/ppa
+
+    sudo apt-get update -qq
+    sudo apt-get install -qq qt55-meta-full libboost1.55-dev libavahi-compat-libdnssd-dev
+
+    wget https://www.dropbox.com/s/3xot58gakn6w898/cmake_3.2.3-3.2.3_amd64.deb?dl=1 -O cmake_3.2.3-3.2.3_amd64.deb
+    sudo dpkg --force-overwrite -i cmake_3.2.3-3.2.3_amd64.deb
+
+    wget https://www.dropbox.com/s/zvfaiylxh6ecp0w/gcc_5.2.0-1_amd64.deb?dl=1 -O gcc.deb
+    sudo dpkg --force-overwrite -i  gcc.deb
+
+  ;;
+  osx)
+    # work around a homebrew bug
+    set +e
+    brew update; brew update 
+    brew install wget
+    wget https://www.dropbox.com/s/n3dsifakgzjbsnh/Jamoma-Darwin20150828.zip?dl=0 -O JamomaDarwin20150828.zip
+    unzip JamomaDarwin20150828.zip
+    mv JamomaDarwin20150828 Jamoma
+    brew install cmake qt5 boost
+    set -e
+  ;;
+esac
