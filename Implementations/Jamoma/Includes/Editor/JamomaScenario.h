3f134d95e81a17194e3a184ebcaee5810b2f8de5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-11-07 09:25:13 +0100

    Handle null state case

--- Implementations/Jamoma/Includes/Editor/JamomaScenario.h
+++ Implementations/Jamoma/Includes/Editor/JamomaScenario.h
@@ -104,5 +104,5 @@ private:
 
   /*! append each message of the state to the current state in order to eliminate address redundancy
   \todo this code is also in JamomaLoop so we should find a way to factorise it */
-  void flattenAndFilter(const shared_ptr<StateElement>);
+  void flattenAndFilter(const shared_ptr<StateElement>&);
 };
--- Implementations/Jamoma/Sources/Editor/Scenario.cpp
+++ Implementations/Jamoma/Sources/Editor/Scenario.cpp
@@ -22,7 +22,7 @@ JamomaTimeProcess(startState, endState)
   // create the start and the end TimeNodes
   mTimeNodes.push_back(TimeNode::create());
   mTimeNodes.push_back(TimeNode::create());
-  
+
   mCurrentState = State::create();
 }
 
@@ -48,7 +48,7 @@ shared_ptr<StateElement> JamomaScenario::state(const TimeValue& position, const 
   {
     // reset internal State
     mCurrentState->stateElements().clear();
-    
+
     // if the time goes backward
     if (position < mLastPosition)
     {
@@ -56,28 +56,28 @@ shared_ptr<StateElement> JamomaScenario::state(const TimeValue& position, const 
       for (const auto& timeNode : mTimeNodes)
       {
         timeNode->setup(date);
-        
+
         // add the state of each HAPPENED TimeEvent
         for (auto& timeEvent : timeNode->timeEvents())
           if (timeEvent->getStatus() == TimeEvent::Status::HAPPENED)
             flattenAndFilter(timeEvent->getState());
       }
-      
+
       // setup each TimeConstraint considering the date
       for (const auto& timeConstraint : mTimeContraints)
         timeConstraint->setup(date);
     }
-    
+
     // process the scenario from the first TimeNode to the running constraints
     Container<TimeEvent> statusChangedEvents;
     shared_ptr<JamomaTimeNode> n = dynamic_pointer_cast<JamomaTimeNode>(mTimeNodes[0]);
     n->process(statusChangedEvents);
-    
+
     // add the state of each newly HAPPENED TimeEvent
     for (const auto& timeEvent : statusChangedEvents)
       if (timeEvent->getStatus() == TimeEvent::Status::HAPPENED)
         flattenAndFilter(timeEvent->getState());
-    
+
     // make the time of each running TimeConstraint flows and add their state
     for (const auto& timeConstraint : mTimeContraints)
     {
@@ -85,11 +85,11 @@ shared_ptr<StateElement> JamomaScenario::state(const TimeValue& position, const 
       {
         if (timeConstraint->getDriveMode() == Clock::DriveMode::EXTERNAL)
           timeConstraint->tick();
-        
+
         flattenAndFilter(timeConstraint->state(timeConstraint->getPosition(), timeConstraint->getDate()));
       }
     }
-    
+
     // if all the TimeEvents are not NONE : the Scenario is done
     bool done = true;
     for (const auto& timeNode : mTimeNodes)
@@ -97,22 +97,22 @@ shared_ptr<StateElement> JamomaScenario::state(const TimeValue& position, const 
       for (auto& timeEvent : timeNode->timeEvents())
       {
         done = timeEvent->getStatus() != TimeEvent::Status::NONE;
-        
+
         if (!done)
           break;
       }
-      
+
       if (!done)
         break;
     }
-    
+
     // if the Scenario is done : stop the parent TimeConstraint
     if (done)
     {
       if (date > mParent->getDurationMin())
         ;//! \todo mParent->stop(); // if the parent TimeConstraint's Clock is in EXTERNAL drive mode, it creates a deadlock.
     }
-    
+
     mLastPosition = position;
   }
 
@@ -170,7 +170,7 @@ void JamomaScenario::addTimeConstraint(const shared_ptr<TimeConstraint> timeCons
 
   // store TimeConstraint's end node if it is not already stored
   addTimeNode(timeConstraint->getEndEvent()->getTimeNode());
-  
+
   // set TimeConstraint's clock in external mode
   shared_ptr<JamomaClock> clock = dynamic_pointer_cast<JamomaClock>(timeConstraint);
   clock->setDriveMode(Clock::DriveMode::EXTERNAL);
@@ -179,7 +179,7 @@ void JamomaScenario::addTimeConstraint(const shared_ptr<TimeConstraint> timeCons
 void JamomaScenario::removeTimeConstraint(const shared_ptr<TimeConstraint> timeConstraint)
 {
   mTimeContraints.erase(find(mTimeContraints.begin(), mTimeContraints.end(), timeConstraint));
-  
+
   // set the TimeConstraint's clock in none external mode
   shared_ptr<JamomaClock> clock = dynamic_pointer_cast<JamomaClock>(timeConstraint);
   clock->setDriveMode(Clock::DriveMode::INTERNAL);
@@ -230,14 +230,17 @@ const Container<TimeConstraint>& JamomaScenario::timeConstraints() const
 # pragma mark -
 # pragma mark Implementation specific
 
-void JamomaScenario::flattenAndFilter(const shared_ptr<StateElement> element)
+void JamomaScenario::flattenAndFilter(const shared_ptr<StateElement>& element)
 {
+  if(!element)
+      return;
+
   switch (element->getType())
   {
     case StateElement::Type::MESSAGE :
     {
       shared_ptr<Message> messageToAppend = dynamic_pointer_cast<Message>(element);
-      
+
       // find message with the same address to replace it
       bool found = false;
       for (auto it = mCurrentState->stateElements().begin();
@@ -245,7 +248,7 @@ void JamomaScenario::flattenAndFilter(const shared_ptr<StateElement> element)
            it++)
       {
         shared_ptr<Message> messageToCheck = dynamic_pointer_cast<Message>(*it);
-        
+
         // replace if addresses are the same
         if (messageToCheck->getAddress() == messageToAppend->getAddress())
         {
@@ -254,17 +257,17 @@ void JamomaScenario::flattenAndFilter(const shared_ptr<StateElement> element)
           break;
         }
       }
-      
+
       // if not found append it
       if (!found)
         mCurrentState->stateElements().push_back(element);
-      
+
       break;
     }
     case StateElement::Type::STATE :
     {
       shared_ptr<State> stateToFlatAndFilter = dynamic_pointer_cast<State>(element);
-      
+
       for (const auto& e : stateToFlatAndFilter->stateElements())
       {
         flattenAndFilter(e);
