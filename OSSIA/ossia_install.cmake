296ad994fb435cc60a825616feb93b23bada8827
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-08-02 17:24:05 +0200

    work on cmake

---
+++ OSSIA/ossia_install.cmake
@@ -0,0 +1,59 @@
+# Install
+install(TARGETS ossia
+    EXPORT ossia-targets
+    LIBRARY DESTINATION lib
+    ARCHIVE DESTINATION lib/static
+    RUNTIME DESTINATION bin
+    INCLUDES DESTINATION include)
+
+# Install headers
+foreach(file ${API_HEADERS})
+    get_filename_component( dir ${file} DIRECTORY)
+    string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}" "" dir_clean "${dir}")
+    install(
+      FILES "${file}" 
+      DESTINATION "include/${dir_clean}" 
+      COMPONENT Devel)
+endforeach()
+
+# Install export header
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/ossia_export.h
+        DESTINATION include/
+        COMPONENT Devel)
+
+# Install used libraries headers
+install(DIRECTORY ${OSSIA_3RDPARTY_FOLDER}/variant/include/
+        DESTINATION include/variant
+        COMPONENT Devel)
+
+install(DIRECTORY ${OSSIA_3RDPARTY_FOLDER}/nano-signal-slot/include/
+        DESTINATION include/nano-signal-slot
+        COMPONENT Devel)
+
+install(DIRECTORY ${OSSIA_3RDPARTY_FOLDER}/oscpack/oscpack
+        DESTINATION include
+        COMPONENT Devel)
+
+
+include(CMakePackageConfigHelpers)
+write_basic_package_version_file(
+  "${CMAKE_CURRENT_BINARY_DIR}/ossia/ossiaConfigVersion.cmake"
+  VERSION ${ossia_VERSION}
+  COMPATIBILITY AnyNewerVersion
+)
+
+export(EXPORT ossia-targets
+  FILE "${CMAKE_CURRENT_BINARY_DIR}/ossia/ossiaTargets.cmake"
+  NAMESPACE ossia::
+)
+
+configure_file(../CMake/ossiaConfig.cmake.in
+  "${CMAKE_CURRENT_BINARY_DIR}/ossia/ossiaConfig.cmake"
+  COPYONLY
+)
+
+set(ConfigPackageLocation lib/cmake/ossia)
+install(EXPORT ossia-targets
+        DESTINATION "${ConfigPackageLocation}"
+        NAMESPACE ossia::)
+        
---
+++ OSSIA/ossia_tooling.cmake
@@ -0,0 +1,35 @@
+
+# Coverage
+if(OSSIA_COVERAGE)
+    setup_target_for_coverage(
+        ossia_test_coverage
+        "$ENV{CMAKE_BIN}"
+        coverage_out
+        "--build;.;--target;ExperimentalTest")
+endif()
+
+# Taken from http://www.labri.fr/perso/fleury/posts/programming/using-clang-tidy-and-clang-format.html
+# Adding clang-format target if executable is found
+find_program(CLANG_FORMAT "clang-format")
+if(CLANG_FORMAT)
+    add_custom_target(
+        clang-format
+        COMMAND ${CLANG_FORMAT}
+        -i
+        ${SRCS}
+        )
+endif()
+
+# Adding clang-tidy target if executable is found
+find_program(CLANG_TIDY "clang-tidy")
+if(CLANG_TIDY)
+    add_custom_target(
+        clang-tidy
+        COMMAND ${CLANG_TIDY}
+        ${SRCS}
+        -config=''
+        --
+        -std=c++14
+        ${INCLUDE_DIRECTORIES}
+        )
+endif()
