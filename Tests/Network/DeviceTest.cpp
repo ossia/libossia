9b178a5e5fc8d718aa6fbd0cf5495caeee70d93c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-09-26 21:47:01 +0200

    add pull to tests

--- Tests/Network/DeviceTest.cpp
+++ Tests/Network/DeviceTest.cpp
@@ -68,6 +68,40 @@ ossia::net::domain make_domain(ossia::val_type t)
   }
 }
 
+struct remote_data
+{
+  remote_data(
+      std::unique_ptr<ossia::net::protocol_base> local_proto,
+      std::unique_ptr<ossia::net::protocol_base> remote_proto):
+    local_device{std::make_unique<ossia::net::local_protocol>(), "i-score" },
+    remote_device{std::move(remote_proto), "i-score-remote"}
+  {
+    int N = 10;
+
+    for(int i = 0; i < N; i++)
+    {
+      auto cld = local_device.createChild(std::to_string(i));
+      local_addr.push_back(cld->createAddress((ossia::val_type) i));
+    }
+
+    auto& proto_p = static_cast<ossia::net::local_protocol&>(local_device.getProtocol());
+    proto_p.exposeTo(std::move(local_proto));
+
+    for(int i = 0; i < N; i++)
+    {
+      auto cld = remote_device.createChild(std::to_string(i));
+      remote_addr.push_back(cld->createAddress((ossia::val_type) i));
+    }
+  }
+
+  ossia::net::generic_device local_device;
+  ossia::net::generic_device remote_device;
+
+  std::vector<ossia::net::address_base*> local_addr;
+  std::vector<ossia::net::address_base*> remote_addr;
+
+};
+
 class DeviceTest : public QObject
 {
   Q_OBJECT
@@ -102,6 +136,33 @@ private Q_SLOTS:
   {
     test_comm_generic(std::make_unique<ossia::net::minuit_protocol>("i-score-remote", "127.0.0.1", 13579, 13580),
                       std::make_unique<ossia::net::minuit_protocol>("i-score-remote", "127.0.0.1", 13580, 13579));
+
+
+    int N = 10;
+    auto proto = std::make_unique<ossia::net::local_protocol>();
+    auto proto_p = proto.get();
+    ossia::net::generic_device local_device{std::move(proto), "i-score"};
+
+    for(int i = 0; i < N; i++)
+    {
+      auto cld = local_device.createChild(std::to_string(i));
+      cld->createAddress((ossia::val_type) i);
+    }
+
+    proto_p->exposeTo(
+          std::make_unique<ossia::net::minuit_protocol>("i-score-remote", "127.0.0.1", 13579, 13580));
+
+    ossia::net::generic_device remote_device{
+      std::make_unique<ossia::net::minuit_protocol>("i-score-remote", "127.0.0.1", 13580, 13579), "i-score-remote"};
+    remote_device.getProtocol().update(remote_device);
+
+    for(auto& n : remote_device.children())
+    {
+      if(auto a = n->getAddress())
+      {
+        a->pullValue();
+      }
+    }
   }
 
   void test_http()
@@ -139,35 +200,14 @@ private Q_SLOTS:
 
 
 private:
-
   void test_comm_generic(
       std::unique_ptr<ossia::net::protocol_base> local_proto,
       std::unique_ptr<ossia::net::protocol_base> remote_proto)
   {
     int N = 10;
-    auto proto = std::make_unique<ossia::net::local_protocol>();
-    auto proto_p = proto.get();
-    ossia::net::generic_device local_device{
-      std::move(proto), "i-score" };
-
-    std::vector<ossia::net::address_base*> local_addr, remote_addr;
-    for(int i = 0; i < N; i++)
-    {
-      auto cld = local_device.createChild(std::to_string(i));
-      local_addr.push_back(cld->createAddress((ossia::val_type) i));
-    }
-
-    proto_p->exposeTo(std::move(local_proto));
-
-    ossia::net::generic_device remote_device{
-      std::move(remote_proto), "i-score-remote"};
-
-    for(int i = 0; i < N; i++)
-    {
-      auto cld = remote_device.createChild(std::to_string(i));
-      auto addr = cld->createAddress((ossia::val_type) i);
-      remote_addr.push_back(addr);
-    }
+    remote_data rem{std::move(local_proto), std::move(remote_proto)};
+    auto& local_addr = rem.local_addr;
+    auto& remote_addr = rem.remote_addr;
 
     auto push_all_values = [&] {
       for(int i = 0; i < N; i++)
@@ -188,27 +228,28 @@ private:
 
     push_all_values();
 
+
     for(auto val : local_addr)
     {
-      proto_p->observe(*val, true);
+      rem.local_device.getProtocol().observe(*val, true);
     }
     for(auto val : remote_addr)
     {
-      proto_p->observe(*val, true);
+      rem.remote_device.getProtocol().observe(*val, true);
     }
     push_all_values();
 
 
     auto test_all_values = [&] (std::vector<ossia::net::address_base*>& vec)
     {
-      for(int access_i = 0; access_i < 4; access_i++)
+      for(int access_i = 0; access_i < 3; access_i++)
       {
         for(int i = 0; i < N; i++)
         {
           vec[i]->setAccessMode(access_mode(access_i));
         }
         push_all_values();
-        for(int bounding_i = 0 ; bounding_i < 4; bounding_i++)
+        for(int bounding_i = 0 ; bounding_i < 6; bounding_i++)
         {
           for(int i = 0; i < N; i++)
           {
@@ -237,6 +278,7 @@ private:
 
     test_all_values(local_addr);
     test_all_values(remote_addr);
+
   }
 };
 
