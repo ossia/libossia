ab0b6c5e51c8568b1f94b4e73d5342144c5fc402
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-09-26 14:48:09 +0200

    increase coverage for devices

--- Tests/Network/DeviceTest.cpp
+++ Tests/Network/DeviceTest.cpp
@@ -30,6 +30,118 @@ private Q_SLOTS:
     }
     }
 
+    void test_comm_osc()
+    {
+      int N = 10;
+      auto proto = std::make_unique<ossia::net::local_protocol>();
+      auto proto_p = proto.get();
+      ossia::net::generic_device local_device{
+        std::move(proto), "test" };
+
+      std::vector<ossia::net::address_base*> local_addr, remote_addr;
+      for(int i = 0; i < N; i++)
+      {
+        auto cld = local_device.createChild(std::to_string(i));
+        local_addr.push_back(cld->createAddress((ossia::val_type) i));
+      }
+
+      proto_p->exposeTo(std::make_unique<ossia::net::osc_protocol>("127.0.0.1", 9996, 9997));
+
+      auto remote_proto = std::make_unique<ossia::net::osc_protocol>("127.0.0.1", 9997, 9996);
+      ossia::net::generic_device remote_device{
+        std::move(remote_proto), "test" };
+
+      for(int i = 0; i < N; i++)
+      {
+        auto cld = remote_device.createChild(std::to_string(i));
+        auto addr = cld->createAddress((ossia::val_type) i);
+        remote_addr.push_back(addr);
+      }
+
+      for(int i = 0; i < N; i++)
+      {
+        local_addr[i]->pushValue(ossia::init_value((ossia::val_type) i));
+      }
+      for(int i = 0; i < N; i++)
+      {
+        remote_addr[i]->pushValue(ossia::init_value((ossia::val_type) i));
+      }
+
+      for(auto val : local_addr)
+      {
+        proto_p->observe(*val, true);
+      }
+      for(auto val : remote_addr)
+      {
+        proto_p->observe(*val, true);
+      }
+
+      for(int i = 0; i < N; i++)
+      {
+        local_addr[i]->pushValue(ossia::init_value((ossia::val_type) i));
+      }
+      for(int i = 0; i < N; i++)
+      {
+        remote_addr[i]->pushValue(ossia::init_value((ossia::val_type) i));
+      }
+    }
+
+    void test_comm_minuit()
+    {
+      int N = 10;
+      auto proto = std::make_unique<ossia::net::local_protocol>();
+      auto proto_p = proto.get();
+      ossia::net::generic_device local_device{
+        std::move(proto), "i-score" };
+
+      std::vector<ossia::net::address_base*> local_addr, remote_addr;
+      for(int i = 0; i < N; i++)
+      {
+        auto cld = local_device.createChild(std::to_string(i));
+        local_addr.push_back(cld->createAddress((ossia::val_type) i));
+      }
+
+      proto_p->exposeTo(std::make_unique<ossia::net::minuit_protocol>("i-score-remote", "127.0.0.1", 13579, 13580));
+
+      auto remote_proto = std::make_unique<ossia::net::minuit_protocol>("i-score-remote", "127.0.0.1", 13580, 13579);
+      ossia::net::generic_device remote_device{
+        std::move(remote_proto), "i-score-remote" };
+
+      for(int i = 0; i < N; i++)
+      {
+        auto cld = remote_device.createChild(std::to_string(i));
+        auto addr = cld->createAddress((ossia::val_type) i);
+        remote_addr.push_back(addr);
+      }
+
+      for(int i = 0; i < N; i++)
+      {
+        local_addr[i]->pushValue(ossia::init_value((ossia::val_type) i));
+      }
+      for(int i = 0; i < N; i++)
+      {
+        remote_addr[i]->pushValue(ossia::init_value((ossia::val_type) i));
+      }
+
+      for(auto val : local_addr)
+      {
+        proto_p->observe(*val, true);
+      }
+      for(auto val : remote_addr)
+      {
+        proto_p->observe(*val, true);
+      }
+
+      for(int i = 0; i < N; i++)
+      {
+        local_addr[i]->pushValue(ossia::init_value((ossia::val_type) i));
+      }
+      for(int i = 0; i < N; i++)
+      {
+        remote_addr[i]->pushValue(ossia::init_value((ossia::val_type) i));
+      }
+    }
+
     void test_http()
     {
       int argc{}; char** argv{};
