50f081dc8e395a8f646e3c60ece62f8dbae620af
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-10-24 14:30:30 +0200

    update

--- travis/deps.sh
+++ travis/deps.sh
@@ -8,10 +8,10 @@ case "$TRAVIS_OS_NAME" in
     sudo apt-get update -qq
     sudo apt-get install -qq g++-6 ninja-build gcovr lcov qt57-meta-minimal libasound2-dev
 
-    sudo wget https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.bz2 -O /opt/boost.tar.bz2
+    sudo wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.bz2 -O /opt/boost.tar.bz2
     (cd /opt; sudo tar xaf boost.tar.bz2; sudo mv boost_* boost ; sudo chmod -R a+rwx boost)
 
-    wget https://cmake.org/files/v3.6/cmake-3.6.0-rc1-Linux-x86_64.tar.gz -O cmake-linux.tgz
+    wget https://cmake.org/files/v3.6/cmake-3.7.0-rc2-Linux-x86_64.tar.gz -O cmake-linux.tgz
     tar xaf cmake-linux.tgz
     mv cmake-*-x86_64 cmake
   ;;
@@ -20,7 +20,7 @@ case "$TRAVIS_OS_NAME" in
     set +e
 
     brew install wget gnu-tar
-    wget https://github.com/OSSIA/iscore-sdk/releases/download/2.0-OSX/homebrew-cache.tar.gz -O homebrew-cache.tar.gz
+    wget https://github.com/OSSIA/iscore-sdk/releases/download/6.0-osx/homebrew-cache.tar.gz -O homebrew-cache.tar.gz
     gtar xhzf homebrew-cache.tar.gz --directory /usr/local/Cellar
     brew link --force boost cmake ninja qt5 wget
 
