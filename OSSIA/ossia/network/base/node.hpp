ea026f99290fa8ff71b56cec00f0e812fe31d0f9
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-12-23 18:14:30 +0100

    extended attributes for nodes

--- OSSIA/ossia/network/base/node.hpp
+++ OSSIA/ossia/network/base/node.hpp
@@ -3,11 +3,12 @@
 #include <ossia/detail/ptr_container.hpp>
 #include <ossia/network/common/address_properties.hpp>
 #include <ossia/network/base/name_validation.hpp>
+#include <ossia/detail/optional.hpp>
+#include <boost/any.hpp>
+#include <hopscotch_map.h>
 #include <functional>
 #include <memory>
 #include <string>
-#include <ossia/detail/optional.hpp>
-
 #include <nano_signal_slot.hpp>
 #include <ossia_export.h>
 
@@ -58,25 +59,52 @@ public:
 
   virtual ~node_base();
 
+  //! The device in which this node is.
   virtual device_base& getDevice() const = 0;
+
+  //! Parent of this node. May be null if it is the device (i.e. root).
   virtual node_base* getParent() const = 0;
 
+  /**
+   * \brief The name of this node, e.g. "foo".
+   *
+   * \see ossia::net::address_string_from_node
+   */
   virtual std::string getName() const = 0;
   virtual node_base& setName(std::string) = 0;
 
-  virtual address_base* getAddress() const = 0;
+  //! Allows a node to carry a value
   virtual address_base* createAddress(val_type = val_type::IMPULSE) = 0;
+  virtual bool removeAddress() = 0;
+  virtual address_base* getAddress() const = 0;
 
+  //! Allows to add arbitrary metadata to nodes.
+  using extended_attributes = tsl::hopscotch_map<std::string, boost::any>;
+  const extended_attributes& getExtendedAttributes() const;
+  void setExtendedAttributes(const extended_attributes&);
+
+  //! Get a specific attribute.
+  boost::any getExtendedAttribute(const std::string& str) const;
+  void setExtendedAttribute(const std::string& str, const boost::any&);
+
+  //! TODO move me in ExtendedAttributes.
   ossia::optional<instance_bounds> getDynamicInstances() const;
   void setDynamicInstances(ossia::optional<instance_bounds>);
 
-  virtual bool removeAddress() = 0;
 
-  // The parent has ownership
+  /**
+   * @brief createChild Adds a sub-child of the given name.
+   *
+   * @note The name of the child may be modified, so it should be checked after creation.
+   *
+   * @return A pointer to the child if it could be created, else nullptr.
+   */
   node_base* createChild(const std::string& name);
   node_base* findChild(const std::string& name);
+
   bool removeChild(const std::string& name);
   bool removeChild(const node_base& name);
+
   void clearChildren();
 
   const std::vector<std::unique_ptr<node_base>>& children() const
@@ -88,14 +116,15 @@ public:
   mutable Nano::Signal<void(const node_base&)> aboutToBeDeleted;
 
 protected:
-  /** Should return nullptr if no child is to be added */
+  //! Should return nullptr if no child is to be added.
   virtual std::unique_ptr<node_base> makeChild(const std::string& name) = 0;
 
-  /** Should return false if the child must not be removed */
+  //! Reimplement for a specific removal action.
   virtual void removingChild(node_base& node_base) = 0;
 
   std::vector<std::unique_ptr<node_base>> mChildren;
   ossia::optional<instance_bounds> mInstances;
+  tsl::hopscotch_map<std::string, boost::any> mExtended;
 };
 
 }
