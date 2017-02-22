00006439b5d5c9f203b26f2d05ea7ae66a586e4b
theod <theo.delahogue@gmea.net>
2017-02-22 15:16:48 +0100

    some try to have ossia value operator

--- OSSIA/ossia-python/ossia_python.cpp
+++ OSSIA/ossia-python/ossia_python.cpp
@@ -1,41 +1,45 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+#include <pybind11/operators.h>
+
 #include <ossia/network/generic/generic_node.hpp>
 #include <ossia/network/generic/generic_device.hpp>
 #include <ossia/network/generic/generic_address.hpp>
+
 #include <ossia/network/local/local.hpp>
 #include <ossia/network/osc/osc.hpp>
 #include <ossia/network/minuit/minuit.hpp>
 
-#include <pybind11/pybind11.h>
-
 namespace py = pybind11;
+
 class python_local_device
 {
-  ossia::net::generic_device m_dev;
-  ossia::net::local_protocol& m_local;
+  ossia::net::generic_device m_device;
+  ossia::net::local_protocol& m_local_protocol;
 
 public:
   python_local_device(std::string name):
-    m_dev{std::make_unique<ossia::net::local_protocol>(), std::move(name)},
-    m_local{static_cast<ossia::net::local_protocol&>(m_dev.getProtocol())}
+    m_device{std::make_unique<ossia::net::local_protocol>(), std::move(name)},
+    m_local_protocol{static_cast<ossia::net::local_protocol&>(m_device.getProtocol())}
   {
 
   }
 
-  bool exposeToMinuit(std::string ip, int remoteport, int localport)
+  bool asMinuitServer(std::string ip, int remoteport, int localport)
   {
     try {
-      m_local.exposeTo(
+      m_local_protocol.exposeTo(
             std::make_unique<ossia::net::minuit_protocol>(
-              m_dev.getName(), std::move(ip), localport, remoteport));
+              m_device.getName(), std::move(ip), localport, remoteport));
       return true;
     } catch(...) { }
     return false;
   }
 
-  bool exposeToOsc(std::string ip, int remoteport, int localport)
+  bool asOscServer(std::string ip, int remoteport, int localport)
   {
     try {
-      m_local.exposeTo(std::make_unique<ossia::net::osc_protocol>(
+      m_local_protocol.exposeTo(std::make_unique<ossia::net::osc_protocol>(
                          std::move(ip), localport, remoteport));
       return true;
     } catch(...) { }
@@ -44,12 +48,12 @@ public:
 
   ossia::net::node_base* addNode(const std::string& address)
   {
-    return &ossia::net::find_or_create_node(m_dev.getRootNode(), address);
+    return &ossia::net::find_or_create_node(m_device.getRootNode(), address);
   }
 
   ossia::net::node_base* findNode(const std::string& address)
   {
-    return ossia::net::find_node(m_dev.getRootNode(), address);
+    return ossia::net::find_node(m_device.getRootNode(), address);
   }
 };
 
@@ -58,8 +62,8 @@ PYBIND11_PLUGIN(ossia_python) {
 
     py::class_<python_local_device>(m, "LocalDevice")
         .def(py::init<std::string>())
-        .def("exposeToMinuit", &python_local_device::exposeToMinuit)
-        .def("exposeToOsc", &python_local_device::exposeToOsc)
+        .def("asMinuitServer", &python_local_device::asMinuitServer)
+        .def("asOscServer", &python_local_device::asOscServer)
         .def("addNode", &python_local_device::addNode, py::return_value_policy::reference)
         .def("findNode", &python_local_device::findNode, py::return_value_policy::reference)
         ;
@@ -87,12 +91,17 @@ PYBIND11_PLUGIN(ossia_python) {
         .def("get_vec2f",  [] (const ossia::value& val) { return val.get<std::array<float, 2>>(); })
         .def("get_vec3f",  [] (const ossia::value& val) { return val.get<std::array<float, 3>>(); })
         .def("get_vec4f",  [] (const ossia::value& val) { return val.get<std::array<float, 4>>(); })
+        //.def(py::self + py::self)
+        //.def("__repr__", [] (const ossia::value& val) -> std::string { return value_to_pretty_string(val); });
         ;
+
+    m.def("value_to_pretty_string", &ossia::value_to_pretty_string, "A function to print an ossia value");
 
     py::class_<ossia::net::address_base>(m, "Address")
         .def("pullValue", &ossia::net::address_base::pullValue)
         .def("cloneValue",  [] (ossia::net::address_base& addr) -> ossia::value { return addr.cloneValue(); })
         .def("pushValue", [] (ossia::net::address_base& addr, const ossia::value& v) { addr.pushValue(v); })
+        .def("addCallback", [] (ossia::net::address_base& addr, ossia::value_callback clbk) { addr.add_callback(clbk); })
         ;
 
     return m.ptr();
