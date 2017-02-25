3691a639db223f48b4a4d64937abc5840d99ed9d
reno- <contact@rubiano.fr>
2017-02-25 01:57:24 +0100

    work in progress - time to sleep - to be continued

--- OSSIA/ossia-python/vplayer.py
+++ OSSIA/ossia-python/vplayer.py
@@ -4,16 +4,47 @@ class VPlayer(object):
 	"""
 	Video Player
 	"""
-	def __init__(self):
+	def __init__(self, device):
 		super(VPlayer, self).__init__()
-		# create the Video Player Device
-		self._me = ossia.LocalDevice('VPlayer')
-		self._me.create_oscquery_server(22222, 33333)
 		# init play property to False
-		self._play_status = self._me.add_node('play_status')
+		self._play_status = device.add_node('play_status')
 		self._play_status = self._play_status.create_address(ossia.ValueType.Bool)
 		self._play_status.push_value(ossia.Value(False))
+		self._play = device.add_node('play')
+		self._play = self._play.create_address(ossia.ValueType.Impulse)
+		self._play.set_access_mode(ossia.AccessMode.Set)
+		self._stop = device.add_node('stop')
+		self._stop = self._stop.create_address(ossia.ValueType.Impulse)
+		self._stop.set_access_mode(ossia.AccessMode.Set)
+		self._elapsed = device.add_node('elapsed')
+		self._elapsed = self._elapsed.create_address(ossia.ValueType.Float)
+		self._elapsed.set_access_mode(ossia.AccessMode.Get)
+	
 
+	def play(self):
+		"""
+		Play the video
+		"""
+		self.play_status = True
+
+	def pause(self):
+		"""
+		Pause the video
+		"""
+		self.play_status = inverse(self.play_status)
+
+	def stop(self):
+		"""
+		Play the video
+		"""
+		self.play_status = False
+
+	@property
+	def elapsed(self):
+		"""
+		Time ......
+		"""
+		return self.elapsed.fetch_value()
 
 	@property
 	def play_status(self):
@@ -28,7 +59,13 @@ class VPlayer(object):
 		self._play_status.push_value(ossia.Value(play_status))
 
 
-vplayer = VPlayer()
+# create the Video Player Device
+my_device = ossia.LocalDevice('my_video_device')
+my_device.create_oscquery_server(22222, 33333)
+# create an instance of a video player
+vplayer = VPlayer(my_device)
+
+
 
 from time import sleep
 
