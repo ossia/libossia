db9e67735167f4f38718d8fc4b586b8e4b60459d
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-04-25 11:01:08 +0200

    minor

--- OSSIA/ossia/dataflow/dataflow.hpp
+++ OSSIA/ossia/dataflow/dataflow.hpp
@@ -1,27 +1,3 @@
 #pragma once
-/*
-#include <ossia/editor/scenario/time_process.hpp>
-#include <ossia/editor/scenario/time_value.hpp>
-#include <ossia/editor/scenario/time_process.hpp>
-#include <ossia/editor/scenario/time_constraint.hpp>
-#include <ossia/editor/state/state_element.hpp>
-#include <ossia/network/common/path.hpp>
-#include <ossia/network/base/device.hpp>
-#include <boost/graph/topological_sort.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/container/flat_set.hpp>
-#include <boost/bimap.hpp>
-#include <boost/functional/hash.hpp>
-#include <boost/graph/connected_components.hpp>
-#include <boost/range/adaptors.hpp>
-#include <ModernMIDI/midi_message.h>
-#include <deque>
-namespace ossia
-{
-
-
-}
-*/
-
 #include <ossia/dataflow/graph.hpp>
 #include <ossia/dataflow/node_process.hpp>
