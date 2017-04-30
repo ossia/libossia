3c82c6346aa62e5e5994a1db56dca98d758749ff
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-04-30 11:33:36 +0200

    re-switch to gcc

--- travis/build.sh
+++ travis/build.sh
@@ -16,8 +16,8 @@ cd build
 
 case "$TRAVIS_OS_NAME" in
   linux)
-    export CC=/usr/bin/clang-4.0
-    export CXX=/usr/bin/clang++-4.0
+#    export CC=/usr/bin/clang-4.0
+#    export CXX=/usr/bin/clang++-4.0
     export BOOST_ROOT=/opt/boost
 #    export VERBOSE=1
     QT_ENV_SCRIPT=$(find /opt -name 'qt*-env.sh')
@@ -42,7 +42,7 @@ case "$TRAVIS_OS_NAME" in
         $CMAKE_BIN --build . --target ExperimentalTest
         rm -rf **/*.o
         lcov --compat-libtool --directory .. --capture --output-file coverage.info
-        lcov --remove coverage.info '*.moc' 'moc_*' 'qrc_*' 'ui_*' 'tests/*' '/usr/*' '/opt/*' '3rdparty/*' --output-file coverage.info.cleaned
+        lcov --remove coverage.info '*.moc' '*/moc_*' '*/qrc_*' '*/ui_*' '*/tests/*' '/usr/*' '/opt/*' '*/3rdparty/*' --output-file coverage.info.cleaned
 
         mv coverage.info.cleaned coverage.info
         coveralls-lcov coverage.info
