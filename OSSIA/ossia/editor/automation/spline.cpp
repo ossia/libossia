5f5be275f3db545cd336f580044921b3673ad5e5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-06-05 19:05:52 +0200

    put tinyspline in source

---
+++ OSSIA/ossia/editor/automation/spline.cpp
@@ -0,0 +1,73 @@
+#include <ossia/editor/automation/spline.hpp>
+#include <ossia/editor/state/state_element.hpp>
+#include <ossia/editor/scenario/time_constraint.hpp>
+
+namespace ossia
+{
+
+spline_automation::spline_automation()
+{
+
+}
+
+void spline_automation::set_destination(Destination a)
+{
+  m_address = std::move(a);
+}
+
+state_element spline_automation::offset(time_value)
+{
+  return {};
+}
+
+void spline_automation::set_spline(const spline_data& t)
+{
+  m_spline = tinyspline::BSpline(
+        3,
+        2,
+        t.points.size(),
+        TS_CLAMPED
+        );
+
+  ts_bspline_set_ctrlp(m_spline.data(),
+        reinterpret_cast<const tinyspline::real*>(t.points.data())
+                       , m_spline.data());
+}
+
+ossia::state_element spline_automation::state()
+{
+  if(m_address)
+  {
+    auto& addr = *m_address;
+    auto t = parent()->get_date() / parent()->get_nominal_duration();
+
+    auto p = m_spline.evaluate(t);
+    auto d = p.data();
+    return ossia::message{
+      addr,
+      ossia::vec2f{
+        float(m_x + m_scaleX * d->result[0]),
+        float(m_y + m_scaleY * d->result[1])
+      }
+    };
+  }
+  return {};
+}
+
+void spline_automation::start()
+{
+}
+
+void spline_automation::stop()
+{
+}
+
+void spline_automation::pause()
+{
+}
+
+void spline_automation::resume()
+{
+}
+
+}
---
+++ OSSIA/ossia/editor/automation/tinysplinecpp.cpp
@@ -0,0 +1,37 @@
+#include "tinysplinecpp.h"
+
+/********************************************************
+*                                                       *
+* Utils                                                 *
+*                                                       *
+********************************************************/
+tinyspline::BSpline tinyspline::Utils::interpolateCubic(
+	const std::vector<tinyspline::real> *points, const size_t dim)
+{
+	if (dim == 0)
+		throw std::runtime_error(ts_enum_str(TS_DIM_ZERO));
+	if (points->size() % dim != 0)
+		throw std::runtime_error("#points % dim == 0 failed");
+	tinyspline::BSpline bspline;
+	const tsError err = ts_bspline_interpolate_cubic(
+		points->data(), points->size()/dim, dim, bspline.data());
+	if (err < 0)
+		throw std::runtime_error(ts_enum_str(err));
+	return bspline;
+}
+
+bool tinyspline::Utils::fequals(
+	const tinyspline::real x, const tinyspline::real y)
+{
+	return ts_fequals(x, y) == 1;
+}
+
+std::string tinyspline::Utils::enum_str(const tsError err)
+{
+	return std::string(ts_enum_str(err));
+}
+
+tsError tinyspline::Utils::str_enum(const std::string str)
+{
+	return ts_str_enum(str.c_str());
+}
