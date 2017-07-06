1b4357e5f8f94d76b04093bed63ebbaff995e778
Antoine Villeret <antoine.villeret@gmail.com>
2017-07-06 10:20:09 +0200

    force the use of gcc 6 for crosscompiling
    please install it before using it...
    
    [skip appveyor]

--- travis/deps.sh
+++ travis/deps.sh
@@ -8,18 +8,18 @@ fi
 case "$TRAVIS_OS_NAME" in
   linux)
     sudo wget https://github.com/OSSIA/iscore-sdk/releases/download/sdk7/boost.tar.bz2 -O /opt/boost.tar.bz2 &
-    
+
     wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz -O cmake-linux.tgz &
-    
+
     echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main' | sudo tee /etc/apt/sources.list.d/llvm.list
     sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 1397BC53640DB551
     sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
     sudo add-apt-repository --yes ppa:beineri/opt-qt59-trusty
     sudo apt-get update -qq
-    sudo apt-get install -qq --yes --force-yes g++-6 binutils ninja-build gcovr lcov qt59-meta-minimal libasound2-dev clang-4.0 lld-4.0
+    sudo apt-get install -qq --yes --force-yes g++-6 binutils ninja-build gcovr lcov qt59-meta-minimal libasound2-dev clang-4.0 lld-4.0 gcc-6-arm-linux-gnueabihf g++-6-arm-linux-gnueabihf
 
     wait wget || true
-    
+
     (cd /opt; sudo tar xaf boost.tar.bz2; sudo mv boost_* boost ; sudo chmod -R a+rwx boost)
 
     tar xaf cmake-linux.tgz
