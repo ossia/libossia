573b86809da88e1b761fbd86dc389edd11594aa4
Antoine Villeret <antoine.villeret@gmail.com>
2017-07-15 23:08:16 +0200

    make ossia package by hand

--- ci/after_build.ps1
+++ ci/after_build.ps1
@@ -3,6 +3,14 @@ if ( $env:APPVEYOR_BUILD_TYPE -eq "testing" ){
 }
 
 if ( $env:APPVEYOR_BUILD_TYPE -eq "pd" ){
-  cmake --build . --target install
+  mkdir ossia-pd-package\ossia
+  mkdir ossia-pd-package\ossia\helps
+  mkdir ossia-pd-package\ossia\examples
+
+  copy OSSIA\ossia-pd\%configuration%\ossia.dll ossia-pd-package\ossia\
+  copy %APPVEYOR_BUILD_FOLDER%\OSSIA\ossia-pd\helps\* ossia-pd-package\ossia\helps\
+  copy %APPVEYOR_BUILD_FOLDER%\OSSIA\ossia-pd\examples\* ossia-pd-package\ossia\examples\
+  # install target fails with error MSB3073, see https://ci.appveyor.com/project/JeanMichalCelerier/libossia/build/job/65o4lytwm9gr74n2
+  # cmake --build . --target install
   # 7z a ossia-pd-windows-x86_64.zip %APPVEYOR_BUILD_FOLDER%\ossia-pd-package\*
 }
