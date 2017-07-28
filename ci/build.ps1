1a3f311d703862a2ca3f94f8c87a5701c8ec9049
Antoine Villeret <antoine.villeret@gmail.com>
2017-07-28 18:11:00 +0200

    change to folder where .sln file is

--- ci/build.ps1
+++ ci/build.ps1
@@ -1,5 +1,7 @@
+cd C:\projects\libossia\build\
 msbuild C:\projects\libossia\build\OSSIA.sln /m
 
 if ( $env:APPVEYOR_BUILD_TYPE -eq "max"){
+  cd C:\projects\libossia\build-32bit
   msbuild C:\projects\libossia\build-32bit\OSSIA.sln /m
 }
