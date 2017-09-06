b80b28d8729b4b939edd0caa37148b906d84363f
Antoine Villeret <antoine.villeret@gmail.com>
2017-09-06 14:21:25 +0200

    appveyor configure in the root directory, ugly I'm sorry

--- ci/build.ps1
+++ ci/build.ps1
@@ -1,9 +1,13 @@
 Set-PSDebug -Trace 1
 
-cd C:\projects\libossia\build\
 msbuild C:\projects\libossia\build\OSSIA.sln /m > C:\projects\libossia\build-${env:APPVEYOR_BUILD_TYPE}.log
 
 if ( $env:APPVEYOR_BUILD_TYPE -eq "max"){
-  cd C:\projects\libossia\build-32bit
-  msbuild C:\projects\libossia\build-32bit\OSSIA.sln /m > C:\projects\libossia\build-${env:APPVEYOR_BUILD_TYPE}-32bit.log
+  cmake --build . --target install
+
+  cmake --build . --target clean
+  cmake -G ${env:CMAKE_GENERATOR_NAME} -DCMAKE_BUILD_TYPE=${env:CONFIGURATION} ${env:CMAKE_OPTION}  . > C:\projects\libossia\config-${env:APPVEYOR_BUILD_TYPE}-32bit.log
+  cmake --build .
+  cmake --build . --target install
+
 }
