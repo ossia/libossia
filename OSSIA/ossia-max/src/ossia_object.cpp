cf8ed3d489584263e2445ca1e2200b7de5f6536a
Antoine Villeret <antoine.villeret@gmail.com>
2017-09-10 17:57:14 +0200

    move ossia_ossia_setup outside ossia::max namespace to try to fix windows linking issue
    see https://ci.appveyor.com/project/JeanMichalCelerier/libossia/build/1.0.662/job/920rw9hj2tkhs9ah#L4975

--- OSSIA/ossia-max/src/ossia_object.cpp
+++ OSSIA/ossia-max/src/ossia_object.cpp
@@ -3,6 +3,24 @@
 
 using t_ossia = ossia::max::device;
 
+using namespace ossia::max;
+
+extern "C" void ossia_ossia_setup()
+{
+  t_class* c = class_new(
+      "ossia", (method)ossia_object_new, (method)ossia_object_free,
+      (short)sizeof(t_ossia), 0L, A_GIMME, 0);
+
+  node_base::class_setup(c);
+  class_addmethod(c, (method)device::expose, "expose", A_GIMME, 0);
+  class_addmethod(c, (method)device::name, "name", A_GIMME, 0);
+
+  class_register(CLASS_BOX, c);
+
+  auto& ossia_library = ossia::max::ossia_max::instance();
+  ossia_library.ossia_ossia_class = c;
+}
+
 namespace ossia
 {
 namespace max
@@ -26,23 +44,6 @@ extern "C" void ossia_object_free(t_ossia *x)
 {
   outlet_delete(x->m_dumpout);
 }
-
-extern "C" void ossia_ossia_setup()
-{
-  t_class* c = class_new(
-      "ossia", (method)ossia_object_new, (method)ossia_object_free,
-      (short)sizeof(t_ossia), 0L, A_GIMME, 0);
-
-  node_base::class_setup(c);
-  class_addmethod(c, (method)device::expose, "expose", A_GIMME, 0);
-  class_addmethod(c, (method)device::name, "name", A_GIMME, 0);
-
-  class_register(CLASS_BOX, c);
-
-  auto& ossia_library = ossia::max::ossia_max::instance();
-  ossia_library.ossia_ossia_class = c;
-}
-
 
 } // namespace max
 } // namespace ossia
