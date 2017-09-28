f2ecaa6a6be734dab08f999251385f242739e726
Antoine Villeret <antoine.villeret@gmail.com>
2017-09-28 22:20:32 +0200

    try to fix syntax error

--- ci/deps.sh
+++ ci/deps.sh
@@ -51,8 +51,8 @@ case "$TRAVIS_OS_NAME" in
 
         # Copy boost to system path and image path
         sudo ln -s /opt/boost/boost /usr/include/boost
-    elif [[ "$BUILD_TYPE" == "python" ]]; then
-      sudo apt install python${python}
+    elif [[ "x$BUILD_TYPE" == "xpython" ]]; then
+      sudo apt install python${PYTHON_VERSION}
     fi
 
   ;;
@@ -72,8 +72,8 @@ case "$TRAVIS_OS_NAME" in
       wget -nv "https://cycling74.s3.amazonaws.com/download/$MAXSDKARCHIVE"
       tar xf "$MAXSDKARCHIVE"
       ls
-    elif [[ "$BUILD_TYPE" == "python" ]]; then
-      if [[ "$python" == "2.*"]]; then
+    elif [[ "x$BUILD_TYPE" == "xpython" ]]; then
+      if [[ "x$PYTHON_VERSION" == "x2.*" ]]; then
         brew install python2
       else
         brew install python3
