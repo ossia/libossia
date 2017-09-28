6a142e2cfa0fe2e60925efc053686434e11039ff
Antoine Villeret <antoine.villeret@gmail.com>
2017-09-28 13:21:49 +0200

    where is python ?

--- ci/before_build.ps1
+++ ci/before_build.ps1
@@ -84,9 +84,19 @@ if ( $env:APPVEYOR_BUILD_TYPE -eq "testing" ){
   $LogFile = "c:\projects\libossia\configure-${env:APPVEYOR_BUILD_TYPE}-${env:platform}.log"
 
   if ( $env:platform -eq "x64" ){
+
+    dir C:\${env:python}-x64\
+
+    Get-ChildItem -Recurse C:\${env:python}-x64\
+
     cmake -G "Visual Studio 15 2017 Win64" -DPYTHON_EXECUTABLE:FILEPATH=C:\${env:python}-x64\python.exe -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/install" -DOSSIA_STATIC=1 -DOSSIA_PD=0 -DOSSIA_QT=0 -DOSSIA_EXAMPLES=0 -DOSSIA_CI=1 -DOSSIA_TESTING=0 -DOSSIA_PYTHON=1 -DOSSIA_QML=0 -DBOOST_ROOT="${env:BOOST_ROOT}" c:\projects\libossia > $LogFile
     CheckLastExitCode
   } else {
+
+    dir C:\${env:python}\
+
+    Get-ChildItem -Recurse C:\${env:python}\
+
     cmake -G "Visual Studio 15 2017" -DPYTHON_EXECUTABLE:FILEPATH=C:\${env:python}\python.exe -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${env:APPVEYOR_BUILD_FOLDER}/install" -DOSSIA_STATIC=1 -DOSSIA_PD=0 -DOSSIA_QT=0 -DOSSIA_EXAMPLES=0 -DOSSIA_CI=1 -DOSSIA_TESTING=0 -DOSSIA_PYTHON=1 -DOSSIA_QML=0 -DBOOST_ROOT="${env:BOOST_ROOT}" c:\projects\libossia > $LogFile
     CheckLastExitCode
   }
