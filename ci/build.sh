4cac4e8efe9a1845feee87c0bfa78a3c85b68b08
Antoine Villeret <antoine.villeret@gmail.com>
2017-09-29 11:29:11 +0200

    restore and round out build matrix
    also add more artifact to deploy

--- ci/build.sh
+++ ci/build.sh
@@ -18,6 +18,8 @@ case "$TRAVIS_OS_NAME" in
 esac
 export CTEST_OUTPUT_ON_FAILURE=1
 
+tar -xzf ossia-src-unix.tar.gz .
+
 mkdir build
 cd build
 
@@ -48,7 +50,7 @@ case "$TRAVIS_OS_NAME" in
           -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
           -DOSSIA_C=1 \
           -DOSSIA_CPP=1 \
-          -DOSSIA_UNITY=1 \
+          -DOSSIA_UNITY3D=1 \
           -DOSSIA_STATIC=$OSSIA_STATIC \
           -DOSSIA_TESTING=1 \
           -DOSSIA_EXAMPLES=1 \
@@ -60,11 +62,25 @@ case "$TRAVIS_OS_NAME" in
         $CMAKE_BIN --build . --target install
 
 
-        cd $TRAVIS_BUILD_DIR/install
         if [[ $OSSIA_STATIC == 1 ]]; then
-          tar -czf $TRAVIS_BUILD_DIR/libossia-linux_x86_64-static.tar.gz .
+          cd $TRAVIS_BUILD_DIR/install
+          tar -czf $TRAVIS_BUILD_DIR/ossia-native-linux_x86_64-static.tar.gz .
+
+          # make unity3d package
+          mkdir $TRAVIS_BUILD_DIR/unity3d
+          mkdir $TRAVIS_BUILD_DIR/unity3d/Assets
+          mkdir $TRAVIS_BUILD_DIR/unity3d/Assets/Plugins
+          mkdir $TRAVIS_BUILD_DIR/unity3d/Assets/ossia
+          cp $TRAVIS_BUILD_DIR/OSSIA/ossia-unity3d/* $TRAVIS_BUILD_DIR/unity3d/Assets/ossia/
+          mv $TRAVIS_BUILD_DIR/unity3d/Assets/ossia/README.md $TRAVIS_BUILD_DIR/unity3d/
+          cp $TRAVIS_BUILD_DIR/install/lib/libossia.dylib $TRAVIS_BUILD_DIR/unity3d/Assets/Plugins
+
+          cd $TRAVIS_BUILD_DIR/unity3d/
+          tar -czf $TRAVIS_BUILD_DIR/ossia-unity3d-linux_x86_64.tar.gz .
+
         else
-          tar -czf $TRAVIS_BUILD_DIR/libossia-linux_x86_64.tar.gz .
+          cd $TRAVIS_BUILD_DIR/install
+          tar -czf $TRAVIS_BUILD_DIR/ossia-native-linux_x86_64.tar.gz .
         fi
 
       ;;
@@ -88,6 +104,23 @@ case "$TRAVIS_OS_NAME" in
 
         cd $TRAVIS_BUILD_DIR/ossia-pd-package
         tar -czf $TRAVIS_BUILD_DIR/ossia-pd-linux_arm.tar.gz ossia
+      ;;
+      RpiRelease)
+        #setup some environment variable to help CMAKE to find libraries
+        export RPI_ROOT_PATH=/tmp/rpi/root
+        export PKG_CONFIG_SYSROOT_DIR=$RPI_ROOT_PATH
+        export PKG_CONFIG_LIBDIR=${RPI_ROOT_PATH}/usr/lib/pkgconfig:${RPI_ROOT_PATH}/usr/share/pkgconfig:${RPI_ROOT_PATH}/usr/lib/arm-linux-gnueabihf/pkgconfig/
+
+        $CMAKE_BIN -DCMAKE_TOOLCHAIN_FILE="$PWD/../CMake/toolchain/arm-linux-gnueabihf.cmake" -DBOOST_ROOT="/usr/include/boost" -DOSSIA_PD=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$TRAVIS_BUILD_DIR/install" -DOSSIA_STATIC=$OSSIA_STATIC -DOSSIA_TESTING=0 -DOSSIA_EXAMPLES=0 -DOSSIA_CI=1 -DOSSIA_QT=0 -DOSSIA_PYTHON=0 -DOSSIA_C=1 -DOSSIA_CPP=1 ..
+        $CMAKE_BIN --build . -- -j2
+        $CMAKE_BIN --build . --target install > /dev/null
+
+        cd $TRAVIS_BUILD_DIR/install
+        if [[ $OSSIA_STATIC =  1]]; then
+          tar -czf $TRAVIS_BUILD_DIR/ossia-native-linux_arm-static.tar.gz .
+        else
+          tar -czf $TRAVIS_BUILD_DIR/ossia-native-linux_arm.tar.gz .
+        fi
       ;;
       python)
         $CMAKE_BIN -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -DBOOST_ROOT="$BOOST_ROOT" \
@@ -295,10 +328,37 @@ case "$TRAVIS_OS_NAME" in
                -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
                -DOSSIA_CI=1 \
                -DOSSIA_QT=1 \
+               -DOSSIA_C=1 \
+               -DOSSIA_CPP=1 \
+               -DOSSIA_UNITY3D=1 \
+               -DOSSIA_OSX_RETROCOMPATIBILITY=1 \
+               -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/install
                ..
 
       $CMAKE_BIN --build . -- -j2
       $CMAKE_BIN --build . --target ExperimentalTest
+      $CMAKE_BIN --build . --target install
+
+      if [[ "$BUILD_TYPE" == "Release" ] && [ $OSSIA_STATIC == 1 ]]; then
+        # make unity3d package
+        mkdir $TRAVIS_BUILD_DIR/unity3d
+        mkdir $TRAVIS_BUILD_DIR/unity3d/Assets
+        mkdir $TRAVIS_BUILD_DIR/unity3d/Assets/Plugins
+        mkdir $TRAVIS_BUILD_DIR/unity3d/Assets/ossia
+        cp $TRAVIS_BUILD_DIR/OSSIA/ossia-unity3d/* $TRAVIS_BUILD_DIR/unity3d/Assets/ossia/
+        mv $TRAVIS_BUILD_DIR/unity3d/Assets/ossia/README.md $TRAVIS_BUILD_DIR/unity3d/
+        cp $TRAVIS_BUILD_DIR/install/lib/libossia.dylib $TRAVIS_BUILD_DIR/unity3d/Assets/Plugins
+
+        cd $TRAVIS_BUILD_DIR/unity3d/
+        tar -czf $TRAVIS_BUILD_DIR/ossia-unity3d-macos.tar.gz .
+
+        cd $TRAVIS_BUILD_DIR/install
+        if [[ $OSSIA_STATIC == 1 ]]; then
+          tar -czf $TRAVIS_BUILD_DIR/ossia-native-macos-static.tar.gz .
+        else
+          tar -czf $TRAVIS_BUILD_DIR/ossia-native-macos.tar.gz .
+        fi
+      fi
     fi
   ;;
 esac
