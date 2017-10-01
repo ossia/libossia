0a04160231c4dff3c01373e37eff51db4271cbee
Antoine Villeret <antoine.villeret@gmail.com>
2017-10-01 18:56:58 +0200

    ~/bin is not in standard macos PATH, so use fullpath to Deken
    also fix ${0%/*} uses

--- ci/push_deken.sh
+++ ci/push_deken.sh
@@ -4,20 +4,21 @@
 
 if [[ "$BUILD_TYPE" == *Pd* ]]; then
 
+  mkdir -p ~/bin
   curl https://raw.githubusercontent.com/pure-data/deken/master/developer/deken > ~/bin/deken
   chmod 755 ~/bin/deken
   # dry run to configure and create ~/.deken
-  deken || true
+  ~/bin/deken || true
 
   # decrypt deken configuration file
   openssl aes-256-cbc -K $encrypted_7a0fc0b5101e_key -iv $encrypted_7a0fc0b5101e_iv -in ${0%/*}/deken.config.enc -out ~/.deken/config -d
 
   # decrypt GPG key
-  openssl aes-256-cbc -K $encrypted_7a0fc0b5101e_key -iv $encrypted_7a0fc0b5101e_iv -in {0%/*}/signingkey.asc.enc -out {0%/*}/signingkey.asc -d
-  gpg --fast-import {0%/*}/signingkey.asc
+  openssl aes-256-cbc -K $encrypted_7a0fc0b5101e_key -iv $encrypted_7a0fc0b5101e_iv -in ${0%/*}/signingkey.asc.enc -out ${0%/*}/signingkey.asc -d
+  gpg --fast-import ${0%/*}/signingkey.asc
 
   cd $TRAVIS_BUILD_DIR/ossia-pd-package
-  deken upload -v $TRAVIS_TAG ossia
+  ~/bin/deken upload -v $TRAVIS_TAG ossia
 
 fi
 
