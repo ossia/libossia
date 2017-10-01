f95573930127bbcd433211ebb63dc76d00e57a19
Antoine Villeret <antoine.villeret@gmail.com>
2017-10-01 14:30:56 +0200

    disable tag condition for testing

--- ci/push_deken.sh
+++ ci/push_deken.sh
@@ -2,7 +2,7 @@
 
 # the GPG configuration have been setup thanks to : http://www.debonair.io/post/maven-cd/
 
-if [[ "$BUILD_TYPE" == *Pd* && "$TRAVIS_TAG" != "" ]]; then
+if [[ "$BUILD_TYPE" == *Pd* ]]; then
 
   curl https://raw.githubusercontent.com/pure-data/deken/master/developer/deken > ~/bin/deken
   chmod 755 ~/bin/deken
