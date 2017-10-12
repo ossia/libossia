833f9d38fb29eb11743b838d3961138b4d89249d
reno- <contact@rubiano.fr>
2017-10-12 17:36:11 +0200

    python wheel is now done with cmake

--- ci/build.sh
+++ ci/build.sh
@@ -139,17 +139,14 @@ case "$TRAVIS_OS_NAME" in
         $CMAKE_BIN --build . --target install > /dev/null
         ls
          if [[ "$PYTHON_VERSION" == "2.7" ]]; then
-          pip wheel ../OSSIA/ossia-python/
           pip install twine --user
           #pip install -ve ../OSSIA/ossia-python/
           #python ../OSSIA/ossia-python/tests/test_.py
         elif [[ "$PYTHON_VERSION" == "3.5" ]]; then
-          python3.5 -m pip wheel ../OSSIA/ossia-python/
           pip3 install twine --user
           #python3.5 -m pip install -ve ../OSSIA/ossia-python/
           #python3.5 ../OSSIA/ossia-python/tests/test_.py
         elif [[ "$PYTHON_VERSION" == "3.6" ]]; then
-          python3.6 -m pip wheel ../OSSIA/ossia-python/
           pip3 install twine --user
           #python3.6 -m pip install -ve ../OSSIA/ossia-python/
           #python3 ../OSSIA/ossia-python/tests/test_.py
@@ -309,17 +306,14 @@ case "$TRAVIS_OS_NAME" in
       $CMAKE_BIN --build . --target install > /dev/null
       ls
        if [[ "$PYTHON_VERSION" == "2.7" ]]; then
-        pip wheel ../OSSIA/ossia-python/
         pip install twine --user
         #pip install -ve ../OSSIA/ossia-python/
         #python ../OSSIA/ossia-python/tests/test_.py
       elif [[ "$PYTHON_VERSION" == "3.5" ]]; then
-        python3.5 -m pip wheel ../OSSIA/ossia-python/
         pip3 install twine --user
         #python3.5 -m pip install -ve ../OSSIA/ossia-python/
         #python3.5 ../OSSIA/ossia-python/tests/test_.py
       elif [[ "$PYTHON_VERSION" == "3.6" ]]; then
-        python3.6 -m pip wheel ../OSSIA/ossia-python/
         pip3 install twine --user
         #python3.6 -m pip install -ve ../OSSIA/ossia-python/
         #python3 ../OSSIA/ossia-python/tests/test_.py
