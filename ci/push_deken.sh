a78fbd0d1ff748c2c40b5c02868e037a1bc9a593
Antoine Villeret <antoine.villeret@gmail.com>
2017-10-13 16:31:32 +0200

    try to use python 3 for deken

--- ci/push_deken.sh
+++ ci/push_deken.sh
@@ -2,8 +2,10 @@
 
 # the GPG configuration have been setup thanks to : http://www.debonair.io/post/maven-cd/
 
-if [[ "$BUILD_TYPE" == *Pd* && "$TRAVIS_TAG" != "" ]]; then
+#if [[ "$BUILD_TYPE" == *Pd* && "$TRAVIS_TAG" != "" ]]; then
+if [[ "$BUILD_TYPE" == *Pd* ]]; then
 
+  export python=$PYTHON_BIN
   mkdir -p ~/bin
   curl https://raw.githubusercontent.com/pure-data/deken/master/developer/deken > ~/bin/deken
   chmod 755 ~/bin/deken
