502a9c187faeb87b15c588f378e03bffb8bff863
reno- <contact@rubiano.fr>
2017-10-14 13:24:56 +0200

    fix build failed because of outputs

--- ci/deps.sh
+++ ci/deps.sh
@@ -54,15 +54,15 @@ case "$TRAVIS_OS_NAME" in
     elif [[ "x$BUILD_TYPE" == "xpython" || $BUILD_TYPE == *Pd* ]] ; then
       if [[ "$PYTHON_VERSION" == "2.7" ]]; then
         sudo apt-get update -qq
-        sudo apt install -qq python python-dev python-pip
+        travis_wait 15 sudo apt install -qq python python-dev python-pip
       elif [[ "$PYTHON_VERSION" == "3.5" ]]; then
         sudo add-apt-repository --yes ppa:fkrull/deadsnakes
-        sudo apt-get update -qq
+        travis_wait 15 sudo apt-get update -qq
         sudo apt install -qq python3 python3-dev python3-pip
       elif [[ "$PYTHON_VERSION" == "3.6" ]]; then
         sudo add-apt-repository --yes ppa:jonathonf/python-3.6
         sudo apt-get update -qq
-        sudo apt install -qq python3 python3-dev python3-pip
+        travis_wait 15 sudo apt install -qq python3 python3-dev python3-pip
       fi
     fi
 
