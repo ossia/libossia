950d36a6a097af4ab1e3fe987b6aa30a9eeaf407
reno- <contact@rubiano.fr>
2017-10-14 18:55:36 +0200

    simplify python deoloyments. Python 2.7 and 3.6

--- ci/deps.sh
+++ ci/deps.sh
@@ -54,15 +54,11 @@ case "$TRAVIS_OS_NAME" in
     elif [[ "x$BUILD_TYPE" == "xpython" || $BUILD_TYPE == *Pd* ]] ; then
       if [[ "$PYTHON_VERSION" == "2.7" ]]; then
         sudo apt-get update -qq
-        travis_wait 15 sudo apt install -qq python python-dev python-pip
-      elif [[ "$PYTHON_VERSION" == "3.5" ]]; then
-        sudo add-apt-repository --yes ppa:fkrull/deadsnakes
-        travis_wait 15 sudo apt-get update -qq
-        sudo apt install -qq python3 python3-dev python3-pip
+        sudo apt install -qq python python-dev python-pip
       elif [[ "$PYTHON_VERSION" == "3.6" ]]; then
         sudo add-apt-repository --yes ppa:jonathonf/python-3.6
         sudo apt-get update -qq
-        travis_wait 15 sudo apt install -qq python3 python3-dev python3-pip
+        sudo apt install -qq python3 python3-dev python3-pip
       fi
     fi
 
