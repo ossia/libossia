d301a7a917156180589f8be0cb4b9c30e702271e
Antoine Villeret <antoine.villeret@gmail.com>
2017-10-17 02:07:02 +0200

    don't use buggy deken but upload directly with curl
    let's try it !

--- ci/push_deken.sh
+++ ci/push_deken.sh
@@ -5,17 +5,6 @@
 #if [[ "$BUILD_TYPE" == *Pd* && "$TRAVIS_TAG" != "" ]]; then
 if [[ "$BUILD_TYPE" == *Pd* ]]; then
 
-  alias python=$PYTHON_BIN
-
-  python --version &
-  sleep 1
-
-  mkdir -p ~/bin
-  curl https://raw.githubusercontent.com/pure-data/deken/master/developer/deken > ~/bin/deken
-  chmod 755 ~/bin/deken
-  # dry run to configure and create ~/.deken
-  ~/bin/deken || true
-
   # decrypt deken configuration file
   openssl aes-256-cbc -K $encrypted_7a0fc0b5101e_key -iv $encrypted_7a0fc0b5101e_iv -in ${0%/*}/deken.config.enc -out ~/.deken/config -d
 
@@ -27,9 +16,41 @@ if [[ "$BUILD_TYPE" == *Pd* ]]; then
   cd $TRAVIS_BUILD_DIR/ossia-pd-package
 
   VERSION="test-${TRAVIS_TAG}"
+  if [[ "$TRAVIS_TAG" != "" ]]; then
+    VERSION=${TRAVIS_TAG}
+  fi
 
-  mv $TRAVIS_BUILD_DIR/ossia-src-unix.tar.gz "./ossia-v${VERSION}-(Sources)-externals.tar.gz"
+  #Create folder on Webdav
+  curl --user '${DEKEN_USER}:${DEKEN_PASSWORD}'  -X MKCOL  "https://puredata.info/Members/ossia/software/ossia/$VERSION/"
 
-  ~/bin/deken upload -v $VERSION --no-source-error ossia
-  ~/bin/deken upload "./ossia-v${VERSION}-(Sources)-externals.tar.gz"
+  ARCHIVE_NAME="./ossia-v${VERSION}-(Sources)-externals.tar.gz"
+  mv $TRAVIS_BUILD_DIR/ossia-src-unix.tar.gz ${ARCHIVE_NAME}
+  read HASH FILE <<< `sha256sum "${ARCHIVE_NAME}"`
+  echo $HASH > $FILE.sha256
+  gpg -ab ${ARCHIVE_NAME}
+
+  if [[ "$BUILD_TYPE" == "PdRelease" ]]; then
+    curl --user '${DEKEN_USER}:${DEKEN_PASSWORD}'  -T "${ARCHIVE_NAME}"        "https://puredata.info/Members/ossia/software/ossia/${VERSION}/${ARCHIVE_NAME}" --basic
+    curl --user '${DEKEN_USER}:${DEKEN_PASSWORD}'  -T "${ARCHIVE_NAME}.sha256" "https://puredata.info/Members/ossia/software/ossia/${VERSION}/${ARCHIVE_NAME}.sha256" --basic
+    curl --user '${DEKEN_USER}:${DEKEN_PASSWORD}'  -T "${ARCHIVE_NAME}.asc"    "https://puredata.info/Members/ossia/software/ossia/${VERSION}/${ARCHIVE_NAME}.asc" --basic
+  fi
+
+  OS="(Linux-amd64-64)"
+  if [[ "$BUILD_TYPE" == *Rpi* ]]; then
+    OS="(Linux-arm-32)"
+  fi
+
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    OS="(Darwin-x86_64-64)"
+  fi
+
+  ARCHIVE_NAME="ossia-v${VERSION}-(${OS})-externals.tar.gz"
+  tar -czf ${ARCHIVE_NAME}
+  read HASH FILE <<< `sha256sum "${ARCHIVE_NAME}"`
+  echo $HASH > $FILE.sha256
+
+  curl --user '${DEKEN_USER}:${DEKEN_PASSWORD}'  -T "${ARCHIVE_NAME}"        "https://puredata.info/Members/ossia/software/ossia/${VERSION}/${ARCHIVE_NAME}"        --basic
+  curl --user '${DEKEN_USER}:${DEKEN_PASSWORD}'  -T "${ARCHIVE_NAME}.sha256" "https://puredata.info/Members/ossia/software/ossia/${VERSION}/${ARCHIVE_NAME}.sha256" --basic
+  curl --user '${DEKEN_USER}:${DEKEN_PASSWORD}'  -T "${ARCHIVE_NAME}.asc"    "https://puredata.info/Members/ossia/software/ossia/${VERSION}/${ARCHIVE_NAME}.asc" --basic
+
 fi
