6165a4414e583db8166af911a125764e36ee878b
reno- <contact@rubiano.fr>
2017-10-21 11:56:06 +0200

    Maybe this is useless?

--- OSSIA/ossia-python/setup.py
+++ OSSIA/ossia-python/setup.py
@@ -22,15 +22,6 @@ with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
 import versioneer
 __version__ = versioneer.get_version()
 
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-    class bdist_wheel(_bdist_wheel):
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-except ImportError:
-    bdist_wheel = None
-
 
 setup(
     name = 'pyossia',
