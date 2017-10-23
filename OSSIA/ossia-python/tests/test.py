6cd1f9db76743dd19056ed8649646be049b6bf16
reno- <contact@rubiano.fr>
2017-10-23 17:45:32 +0200

    testing default_value and more

--- OSSIA/ossia-python/tests/test.py
+++ OSSIA/ossia-python/tests/test.py
@@ -43,10 +43,12 @@ class TestAll(unittest.TestCase):
     my_bool = my_device.add_param('special/bool', value_type='bool', default_value=True)
     my_string = my_device.add_param('string',value_type='string', default_value='Hello world !', domain=['once', 'loop'])
     my_vec2f = my_device.add_param('list/vec2f', value_type='vec2f', default_value=[0.5, 0.5],  domain=[0, 1])
-    my_vec3f = my_device.add_param('list/vec3f', value_type='vec3f', default_value=[0, 0.57, 0.81],  domain=[0, 360])
-    my_vec4f = my_device.add_param('list/vec4f', value_type='vec4f', default_value=[0, 146, 207],  domain=[0, 255])
+    my_vec3f = my_device.add_param('list/vec3f', value_type='vec3f', default_value=[-270, 180, 45],  domain=[0, 360])
+    my_vec4f = my_device.add_param('list/vec4f', value_type='vec4f', default_value=[0, 146, 207, 127],  domain=[0, 255])
     my_list = my_device.add_param('list/list', value_type='list', default_value=[44100, "my_track.wav", 0.6])
     my_char = my_device.add_param('special/char', value_type='char', default_value=chr(97))
+
+    my_device.root_node.init()
 
     def test_version(self):
         """
@@ -58,8 +60,11 @@ class TestAll(unittest.TestCase):
         """
         test a parameter @value_type float
         """
-        self.assertEqual(self.my_int.value, 0.0)
+        self.assertEqual(self.my_int.value, 66)
         self.assertEqual(self.my_int.value_type, ossia.ValueType.Int)
+        self.assertEqual(self.my_int.have_domain(), True)
+        self.assertEqual(self.my_int.domain.min, -100)
+        self.assertEqual(self.my_int.domain.max, 100)
         self.my_int.value = 60
         self.assertAlmostEqual(self.my_int.value, 60)
 
@@ -67,28 +72,41 @@ class TestAll(unittest.TestCase):
         """
         test a parameter @value_type float
         """
-        self.assertEqual(self.my_float.value, 0.0)
+        self.assertAlmostEqual(self.my_float.value, 0.123456789)
         self.assertEqual(self.my_float.value_type, ossia.ValueType.Float)
+        self.assertEqual(self.my_float.have_domain(), True)
+        self.assertEqual(self.my_float.domain.min, -2.0)
+        self.assertEqual(self.my_float.domain.max, 2.0)
         self.my_float.value = 0.123456
         self.assertAlmostEqual(self.my_float.value, 0.123456)
+
+    def test_char(self):
+        self.assertEqual(self.my_char.value, 'a')
+        self.my_char.value = '@'
+        self.assertEqual(self.my_char.value, '@')
+        self.my_char.value = 'Kj'
+        self.assertEqual(self.my_char.value, '\x00')
+        self.my_char.value = '@'
+        self.assertEqual(self.my_char.value, '@')
 
     def test_string(self):
         """
         test a parameter @value_type float
         """
         self.assertEqual(isinstance(self.my_string.value, str), True)
-        self.assertEqual(self.my_string.value, '')
+        self.assertEqual(self.my_string.value, 'Hello world !')
         self.assertEqual(self.my_string.value_type, ossia.ValueType.String)
-        self.my_string.value = 'hello world'
-        self.assertEqual(self.my_string.value, 'hello world')
+        self.my_string.value = 'What a new string ?'
+        self.assertEqual(self.my_string.value, 'What a new string ?')
 
     def test_vec2f(self):
         """
         test a parameter @value_type float
         """
         self.assertEqual(len(self.my_vec2f.value), 2)
-        self.assertCountEqual(self.my_vec2f.value, [0.0, 0.0])
+        self.assertCountEqual(self.my_vec2f.value, [0.5, 0.5])
         self.assertEqual(self.my_vec2f.value_type, ossia.ValueType.Vec2f)
+        self.assertEqual(self.my_vec2f.have_domain(), True)
         self.my_vec2f.value = [-1, 1.2]
         self.assertAlmostEqual(self.my_vec2f.value[0], -1)
         self.assertAlmostEqual(self.my_vec2f.value[1], 1.2)
@@ -98,9 +116,10 @@ class TestAll(unittest.TestCase):
         """
         test a parameter @value_type float
         """
-        self.assertCountEqual(self.my_vec3f.value, [0.0, 0.0, 0.0])
+        self.assertCountEqual(self.my_vec3f.value, [-270, 180, 45])
         self.assertEqual(len(self.my_vec3f.value), 3)
         self.assertEqual(self.my_vec3f.value_type, ossia.ValueType.Vec3f)
+        self.assertEqual(self.my_vec2f.have_domain(), True)
         self.my_vec3f.value = [-1, -270, 360]
         self.assertEqual(len(self.my_vec3f.value), 3)
         self.assertAlmostEqual(self.my_vec3f.value[0], -1)
@@ -112,9 +131,10 @@ class TestAll(unittest.TestCase):
         """
         test a parameter @value_type float
         """
-        self.assertCountEqual(self.my_vec4f.value, [0.0, 0.0, 0.0, 0.0])
+        self.assertCountEqual(self.my_vec4f.value, [0, 146, 207, 127])
         self.assertEqual(len(self.my_vec4f.value), 4)
         self.assertEqual(self.my_vec4f.value_type, ossia.ValueType.Vec4f)
+        self.assertEqual(self.my_vec2f.have_domain(), True)
         self.my_vec4f.value = [0, 0.6, 0.86, 0.8]
         self.assertAlmostEqual(self.my_vec4f.value[0], 0)
         self.assertAlmostEqual(self.my_vec4f.value[1], 0.6)
@@ -144,6 +164,8 @@ class TestAll(unittest.TestCase):
         self.assertEqual(len(self.my_device.root_node.get_nodes()[0].get_parameters()), 2)
         # How many parameters directly attached to the second child of root_node?
         self.assertEqual(len(self.my_device.root_node.get_nodes()[1].get_parameters()), 4)
+        # Testing NODES
+        self.assertEqual(self.my_device.find_node('/special/bool'), self.my_bool.node)
 
 
 if __name__ == '__main__':
