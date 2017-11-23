f24ebce3b3657aa90f6108584570445bc47f14d6
reno- <contact@rubiano.fr>
2017-11-23 23:28:49 +0100

    compile during building wheel

--- OSSIA/ossia-python/setup.py
+++ OSSIA/ossia-python/setup.py
@@ -20,15 +20,14 @@ with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
 import versioneer
 __version__ = versioneer.get_version()
 
+from setuptools.extension import Extension
 
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-    class bdist_wheel(_bdist_wheel):
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-except ImportError:
-    bdist_wheel = None
+ossia_python = Extension(
+    name='ossia_python',
+    include_dirs=['./../'],
+    sources=['./ossia_python.cpp'],
+    extra_compile_args=['-std=c++14'],
+)
 
 setup(
     name = 'pyossia',
@@ -50,13 +49,7 @@ setup(
     ],
     keywords = ['creative', 'controls', 'osc', 'oscquery', 'websocket', 'libossia', 'midi'],
     packages = find_packages(),
-    cmdclass={
-    
-    'bdist_wheel': bdist_wheel
-    },
-    package_data={
-        'pyossia': ['*.so'],
-    },
+    ext_modules=[ossia_python],
     include_package_data=True,
     zip_safe=False
 )
