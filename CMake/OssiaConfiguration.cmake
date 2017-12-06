8e173755ff8a8874c1ebd66a99dbf4acd3cc6c25
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-12-06 23:57:10 +0100

    oops

--- CMake/OssiaConfiguration.cmake
+++ CMake/OssiaConfiguration.cmake
@@ -134,9 +134,9 @@ set(CMAKE_POSITION_INDEPENDENT_CODE 1)
 set(CMAKE_EXPORT_COMPILE_COMMANDS 1)
 set(CMAKE_CXX_STANDARD 17)
 if(MSVC)
-  set(CMAKE_CXX_FLAGS "-std=c++17 ${CMAKE_CXX_FLAGS}")
-else()
   set(CMAKE_CXX_FLAGS "/std:c++latest ${CMAKE_CXX_FLAGS}")
+else()
+  set(CMAKE_CXX_FLAGS "-std=c++17 ${CMAKE_CXX_FLAGS}")
 endif()
   
 # So that make install after make all_unity does not rebuild everything :
