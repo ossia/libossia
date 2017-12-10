e93bfa44bcf0bbfc1ff54f5075f302b4aea7b471
Antoine Villeret <antoine.villeret+dev@gmail.com>
2017-12-10 21:11:04 +0100

    install ossia on macos for testing

--- ci/build.sh
+++ ci/build.sh
@@ -477,12 +477,14 @@ case "$TRAVIS_OS_NAME" in
       $CMAKE_BIN --build . -- -j2
       $CMAKE_BIN --build . --target install > /dev/null
 
+      mkdir -p ~/Documents/Pd/externals
+      mv $TRAVIS_BUILD_DIR/ossia-pd-package/ossia ~/Documents/Pd/externals
+
       wget http://msp.ucsd.edu/Software/pd-0.48-1test3.mac.tar.gz
       tar xf pd-0.48-1test3.mac.tar.gz
       export PATH="${PWD}/Pd-0.48-1test3.app/Contents/Resources/bin:${PATH}"
 
       $CMAKE_BIN --build . --target test
-
 
     elif [[ "$BUILD_TYPE" == "MaxRelease" ]]; then
       $CMAKE_BIN -DCMAKE_BUILD_TYPE=Release \
