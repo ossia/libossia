b43d86f5f3baecc19fa9f8d5ead27419fc204d55
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-03-08 11:44:37 +0100

    wip phidgets macos

--- CMake/FindPhidgets.cmake
+++ CMake/FindPhidgets.cmake
@@ -4,17 +4,26 @@
 #  LIBPHIDGETS_INCLUDE_DIR - the libphidgets include directory
 #  LIBPHIDGETS_LIBRARIES - Link these to use libphidgets
 
-find_path(LIBPHIDGETS_INCLUDE_DIR phidget22.h
-  HINTS
-    ${PHIDGETS_INCLUDE_DIR}
-    ${CMAKE_CURRENT_SOURCE_DIR}/../3rdparty/libphidgets
-  )
+if(APPLE)
+    set(CMAKE_FIND_FRAMEWORK 1)
+    find_path(LIBPHIDGETS_INCLUDE_DIR phidget22.h
+      HINTS
+        /Library/Frameworks
+      )
 
-find_library(LIBPHIDGETS_LIBRARIES NAMES libphidget22.a
-  HINTS
-  ${PHIDGETS_LIB_DIR}
-  ${CMAKE_CURRENT_SOURCE_DIR}/../3rdparty/libphidgets/.libs)
+    find_library(LIBPHIDGETS_LIBRARIES Phidget22)
+elseif(UNIX)
+    find_path(LIBPHIDGETS_INCLUDE_DIR phidget22.h
+      HINTS
+        ${PHIDGETS_INCLUDE_DIR}
+        ${CMAKE_CURRENT_SOURCE_DIR}/../3rdparty/libphidgets
+      )
 
+    find_library(LIBPHIDGETS_LIBRARIES NAMES libphidget22.a
+      HINTS
+      ${PHIDGETS_LIB_DIR}
+      ${CMAKE_CURRENT_SOURCE_DIR}/../3rdparty/libphidgets/.libs)
+endif()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBPHIDGETS DEFAULT_MSG LIBPHIDGETS_LIBRARIES LIBPHIDGETS_INCLUDE_DIR)
 mark_as_advanced(LIBPHIDGETS_INCLUDE_DIR LIBPHIDGETS_LIBRARIES)
