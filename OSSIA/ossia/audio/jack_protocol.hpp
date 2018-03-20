43f3710ff6b20c12d5441c1223e2459d050fa4c1
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-03-20 17:42:53 +0100

    ok audio

--- OSSIA/ossia/audio/jack_protocol.hpp
+++ OSSIA/ossia/audio/jack_protocol.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#if __has_include(<jack/jack.h>)
+#if 1 || __has_include(<jack/jack.h>)
 #include <ossia/audio/audio_protocol.hpp>
 #define __x86_64__ 1
 #define USE_WEAK_JACK 1
@@ -33,6 +33,15 @@ namespace ossia
 jack_engine::jack_engine()
 {
   std::cerr << "JACK: " << have_libjack() << std::endl;
+  client = jack_client_open("score", JackNullOption, nullptr);
+  if (!client)
+  {
+    std::cerr << "JACK server not running?" << std::endl;
+    exit(0);
+  }
+  jack_set_process_callback(client, process, this);
+  jack_set_sample_rate_callback (client, JackSampleRateCallback{}, this);
+  jack_on_shutdown (client, JackShutdownCallback{}, this);
 }
 
 jack_engine::~jack_engine()
@@ -40,70 +49,58 @@ jack_engine::~jack_engine()
   stop();
   if(protocol)
     protocol.load()->engine = nullptr;
+  jack_client_close(client);
 }
 
 void jack_engine::reload(ossia::audio_protocol* p)
 {
-  if(this->protocol)
-    this->protocol.load()->engine = nullptr;
-  this->protocol = p;
   stop();
+  this->protocol = p;
 
   if(!p)
     return;
   auto& proto = *p;
   proto.engine = this;
   auto& dev = proto.get_device();
-  client = jack_client_open(dev.get_name().c_str(), JackNullOption, nullptr);
-  if (!client)
-  {
-    std::cerr << "JACK server not running?" << std::endl;
-    exit(0);
-    return;
-  }
   std::cerr << "=== STARTING PROCESS ==" << std::endl;
 
-  jack_set_process_callback(client, process, this);
 
-  jack_set_sample_rate_callback (client, JackSampleRateCallback{}, this);
-
-  jack_on_shutdown (client, JackShutdownCallback{}, this);
-
-
-  constexpr const int count = 32;
-  proto.inputs = 32;
-  proto.outputs = 32;
-  proto.audio_ins.clear();
-  proto.audio_outs.clear();
-
-  for(int i = 0; i < count; i++)
+  if(input_ports.empty())
   {
-    auto in = jack_port_register (client, ("in-" + std::to_string(i)).c_str(), JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
-    auto out = jack_port_register (client, ("out-" + std::to_string(i)).c_str(), JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-    assert(in);
-    input_ports.push_back(in);
-    assert(out);
-    output_ports.push_back(out);
-    proto.audio_ins.push_back(ossia::net::find_or_create_parameter<ossia::audio_parameter>(dev.get_root_node(), "/in/" + std::to_string(i)));
-    proto.audio_outs.push_back(ossia::net::find_or_create_parameter<ossia::audio_parameter>(dev.get_root_node(), "/out/" + std::to_string(i)));
+    constexpr const int count = 32;
+    proto.inputs = 32;
+    proto.outputs = 32;
+    proto.audio_ins.clear();
+    proto.audio_outs.clear();
+
+    for(int i = 0; i < count; i++)
+    {
+      auto in = jack_port_register (client, ("in-" + std::to_string(i)).c_str(), JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+      auto out = jack_port_register (client, ("out-" + std::to_string(i)).c_str(), JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+      assert(in);
+      input_ports.push_back(in);
+      assert(out);
+      output_ports.push_back(out);
+      proto.audio_ins.push_back(ossia::net::find_or_create_parameter<ossia::audio_parameter>(dev.get_root_node(), "/in/" + std::to_string(i)));
+      proto.audio_outs.push_back(ossia::net::find_or_create_parameter<ossia::audio_parameter>(dev.get_root_node(), "/out/" + std::to_string(i)));
+    }
+
+    proto.main_audio_in = ossia::net::find_or_create_parameter<ossia::audio_parameter>(dev.get_root_node(), "/in/main");
+    proto.main_audio_out = ossia::net::find_or_create_parameter<ossia::audio_parameter>(dev.get_root_node(), "/out/main");
+
+    proto.main_audio_in->audio.resize(proto.inputs);
+
+    for(int i = 0; i < proto.inputs; i++)
+    {
+      proto.audio_ins[i]->audio.resize(1);
+    }
+
+    proto.main_audio_out->audio.resize(proto.outputs);
+    for(int i = 0; i < proto.outputs; i++)
+    {
+      proto.audio_outs[i]->audio.resize(1);
+    }
   }
-
-  proto.main_audio_in = ossia::net::find_or_create_parameter<ossia::audio_parameter>(dev.get_root_node(), "/in/main");
-  proto.main_audio_out = ossia::net::find_or_create_parameter<ossia::audio_parameter>(dev.get_root_node(), "/out/main");
-
-  proto.main_audio_in->audio.resize(proto.inputs);
-
-  for(int i = 0; i < proto.inputs; i++)
-  {
-    proto.audio_ins[i]->audio.resize(1);
-  }
-
-  proto.main_audio_out->audio.resize(proto.outputs);
-  for(int i = 0; i < proto.outputs; i++)
-  {
-    proto.audio_outs[i]->audio.resize(1);
-  }
-
 
   std::cerr << "=== stream start ===\n";
 
@@ -118,20 +115,13 @@ void jack_engine::reload(ossia::audio_protocol* p)
 
 void jack_engine::stop()
 {
-  //std::cerr << "=== STOPPED PROCESS ==" << std::endl;
-  if(client)
-  {
-    stop_processing = true;
-    auto clt = client.load();
-    client = nullptr;
-    protocol = nullptr;
-    jack_deactivate(clt);
-    jack_set_process_callback(clt, [] (jack_nframes_t nframes, void* arg) -> int { return 0; }, nullptr);
-    jack_client_close(clt);
-    while(processing) std::this_thread::sleep_for(std::chrono::milliseconds(100)) ;
-    input_ports.clear();
-    output_ports.clear();
-  }
+  std::cerr << "=== STOPPED PROCESS ==" << std::endl;
+  if(this->protocol)
+    this->protocol.load()->engine = nullptr;
+  stop_processing = true;
+  protocol = nullptr;
+  jack_deactivate(client.load());
+  while(processing) std::this_thread::sleep_for(std::chrono::milliseconds(100)) ;
 }
 
 int jack_engine::process(jack_nframes_t nframes, void* arg)
@@ -143,9 +133,8 @@ int jack_engine::process(jack_nframes_t nframes, void* arg)
     return 0;
   }
 
-  auto clt = self.client.load();
   auto proto = self.protocol.load();
-  if(clt && proto)
+  if(proto)
   {
     self.processing = true;
     auto float_input = (float**)alloca(sizeof(float*) * proto->inputs);
