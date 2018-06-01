8196f6a37dc577093c0e2862091e55be2f39b923
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-06-01 23:19:52 +0200

    fix dummy audio bug

--- OSSIA/ossia/audio/dummy_protocol.hpp
+++ OSSIA/ossia/audio/dummy_protocol.hpp
@@ -5,26 +5,32 @@ namespace ossia
 class dummy_engine final
     : public audio_engine
 {
+  int m_rate{}, m_bs{};
   public:
     dummy_engine(std::string_view name, int& inputs, int& outputs, int rate, int bs)
     {
       inputs = 0;
       outputs = 0;
+      m_rate = rate;
+      m_bs = bs;
 
-      int us_per_buffer = 1e6 * double(bs) / double(rate);
+      setup_thread();
+    }
+
+    void setup_thread()
+    {
+      int us_per_buffer = 1e6 * double(m_bs) / double(m_rate);
       m_runThread = std::thread{[=] {
         while(!stop_processing)
         {
           if(auto proto = protocol.load())
           {
-            proto->process_generic(*proto, nullptr, nullptr, 0, 0, bs);
-            proto->audio_tick(bs, 0);
+            proto->process_generic(*proto, nullptr, nullptr, 0, 0, m_bs);
+            proto->audio_tick(m_bs, 0);
           }
           std::this_thread::sleep_for(std::chrono::microseconds(us_per_buffer));
         }
-
       }};
-
     }
 
     ~dummy_engine() override
@@ -48,14 +54,17 @@ class dummy_engine final
       proto.setup_tree(0, 0);
 
       stop_processing = false;
-
+      setup_thread();
     }
+
     void stop() override
     {
       stop_processing = true;
       protocol = nullptr;
 
       while(processing) std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      if(m_runThread.joinable())
+        m_runThread.join();
     }
 
   private:
