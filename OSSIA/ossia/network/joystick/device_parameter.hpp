91bdb8ef6eeb3a0caa5fdfd5b1727b389020c0af
aliefhooghe <aliefhooghe@enseirb-matmeca.fr>
2018-07-25 11:15:57 +0200

    reformating

--- OSSIA/ossia/network/joystick/device_parameter.hpp
+++ OSSIA/ossia/network/joystick/device_parameter.hpp
@@ -1,90 +1,96 @@
 
 #pragma once
 
-#include <ossia/network/value/value.hpp>
 #include <ossia/network/base/node.hpp>
 #include <ossia/network/base/protocol.hpp>
-#include <ossia/network/oscquery/oscquery_server.hpp>
 #include <ossia/network/common/complex_type.hpp>
 #include <ossia/network/domain/domain.hpp>
+#include <ossia/network/oscquery/oscquery_server.hpp>
+#include <ossia/network/value/value.hpp>
 
-namespace ossia
+namespace ossia::net
 {
-namespace net 
+
+class device_parameter : public ossia::net::parameter_base
 {
 
-    class device_parameter : public ossia::net::parameter_base
-    {
+public:
+  device_parameter(
+      ossia::net::node_base& node, const ossia::val_type type,
+      const ossia::bounding_mode bounding, const ossia::access_mode access,
+      const ossia::domain domain);
 
-        public:
-            device_parameter(
-                ossia::net::node_base& node,
-                const ossia::val_type type,
-                const ossia::bounding_mode bounding,
-                const ossia::access_mode access,
-                const ossia::domain domain);
+  virtual ~device_parameter();
 
-            virtual ~device_parameter();
+  void device_value_change_event(const ossia::value& value);
 
-            void device_value_change_event(const ossia::value& value);
+  void pull_value() override;
+  ossia::value value() const override;
 
-            void pull_value() override;
-            ossia::value value() const override;
-            
-            ossia::net::parameter_base& push_value(const ossia::value& val) override;
-            ossia::net::parameter_base& push_value(ossia::value&& val) override;
-            ossia::net::parameter_base& push_value() override;
+  ossia::net::parameter_base& push_value(const ossia::value& val) override;
+  ossia::net::parameter_base& push_value(ossia::value&& val) override;
+  ossia::net::parameter_base& push_value() override;
 
-            ossia::net::parameter_base& set_value(const ossia::value& val) override;
-            ossia::net::parameter_base& set_value(ossia::value&& v) override;
+  ossia::net::parameter_base& set_value(const ossia::value& val) override;
+  ossia::net::parameter_base& set_value(ossia::value&& v) override;
 
-            ossia::val_type get_value_type() const override;
-            ossia::net::parameter_base& set_value_type(ossia::val_type) override {return *this; }
+  ossia::val_type get_value_type() const override;
+  ossia::net::parameter_base& set_value_type(ossia::val_type) override
+  {
+    return *this;
+  }
 
-            ossia::access_mode get_access() const override;
-            ossia::net::parameter_base& set_access(ossia::access_mode) override { return *this; }
+  ossia::access_mode get_access() const override;
+  ossia::net::parameter_base& set_access(ossia::access_mode) override
+  {
+    return *this;
+  }
 
-            const ossia::domain& get_domain() const override;
-            ossia::net::parameter_base& set_domain(const ossia::domain&) override { return *this; }
+  const ossia::domain& get_domain() const override;
+  ossia::net::parameter_base& set_domain(const ossia::domain&) override
+  {
+    return *this;
+  }
 
-            ossia::bounding_mode get_bounding() const override;
-            ossia::net::parameter_base& set_bounding(ossia::bounding_mode) override { return *this; }
+  ossia::bounding_mode get_bounding() const override;
+  ossia::net::parameter_base& set_bounding(ossia::bounding_mode) override
+  {
+    return *this;
+  }
 
-            template<class ParamType = device_parameter, class ...T>
-            static ParamType *create_device_parameter(
-                ossia::net::node_base& root_node,
-                const std::string& path,
-                const ossia::value& initial_value,                
-                const T& ...ctor_args)
-            {
-                static_assert(std::is_base_of<device_parameter, ParamType>::value);
-                    
-                auto& param_node = 
-                    ossia::net::create_node(root_node, path);
-                auto param = 
-                    std::make_unique<ParamType>(param_node, ctor_args...);
+  template <class ParamType = device_parameter, class... T>
+  static ParamType* create_device_parameter(
+      ossia::net::node_base& root_node, const std::string& path,
+      const ossia::value& initial_value, const T&... ctor_args)
+  {
+    static_assert(std::is_base_of<device_parameter, ParamType>::value);
 
-                ParamType *ret = param.get();
+    auto& param_node = ossia::net::create_node(root_node, path);
+    auto param = std::make_unique<ParamType>(param_node, ctor_args...);
 
-                param->set_value(initial_value);
-                param_node.set_parameter(std::move(param));
+    ParamType* ret = param.get();
 
-                return ret;
-            }
+    param->set_value(initial_value);
+    param_node.set_parameter(std::move(param));
 
-        protected:
-            virtual void device_update_value() {}
+    return ret;
+  }
 
-            auto& get_protocol() { return get_node().get_device().get_protocol(); }
+protected:
+  virtual void device_update_value()
+  {
+  }
 
-            ossia::value m_current_value;
+  auto& get_protocol()
+  {
+    return get_node().get_device().get_protocol();
+  }
 
-            const ossia::val_type m_type;
-            const ossia::bounding_mode m_bounding;
-            const ossia::access_mode m_access;
-            const ossia::domain m_domain;
-    };
+  ossia::value m_current_value;
 
+  const ossia::val_type m_type;
+  const ossia::bounding_mode m_bounding;
+  const ossia::access_mode m_access;
+  const ossia::domain m_domain;
+};
 }
-}
-
