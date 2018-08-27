74f6cf8aa04a74e6f30270df5d5c03f50d4a3ade
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-08-27 13:55:03 +0200

    try to fix test build

--- Tests/C/unit-tests/unit-tests-main.cpp
+++ Tests/C/unit-tests/unit-tests-main.cpp
@@ -1,4 +1,5 @@
 // This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include <ossia/detail/config.hpp>
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
