f6a82673214c0a90c0e1a1b3fabcda6ab304dcba
aliefhooghe <31956383+aliefhooghe@users.noreply.github.com>
2018-09-14 15:22:18 +0200

    Wiimote Support added (#443)
    * wiimote integration in libossia
    
    * wiiuse 3rdparty
    
    * wiimote
    
    * add unit
    
    * joytick hat unit
    
    * new third party
    
    * printf deleted

---
+++ OSSIA/ossia/network/wiimote/wiimote_parameter.cpp
@@ -0,0 +1,68 @@
+
+#include <wiiuse.h>
+#include "wiimote_parameter.hpp"
+
+using namespace ossia;
+using namespace device;
+
+namespace ossia {
+namespace net {
+    //-------
+
+    rumble_parameter::rumble_parameter(
+        net::node_base& node,
+        struct wiimote_t *wiimote)
+        : device_parameter(
+            node, val_type::BOOL, 
+            bounding_mode::FREE, access_mode::SET,
+            init_domain(val_type::BOOL)),
+            m_wiimote(wiimote)
+    {
+    }
+
+    void rumble_parameter::device_update_value()
+    {
+        const bool v = value().get<bool>();
+        wiiuse_rumble(m_wiimote, v);
+    }
+
+    //-------
+
+    std::map<wiimote_t*, uint8_t> led_parameter::m_led_mask;
+
+    led_parameter::led_parameter(
+        net::node_base& node,
+        struct wiimote_t *wiimote,
+        const uint8_t led)
+        : device_parameter(
+            node, val_type::BOOL, 
+            bounding_mode::FREE, access_mode::SET,
+            init_domain(val_type::LIST)),
+            m_wiimote(wiimote),
+            m_led(led)
+    {
+        if (m_led_mask.find(wiimote) != m_led_mask.end())
+            m_led_mask[m_wiimote] = 0u;
+    }
+
+    led_parameter::~led_parameter()
+    {
+        m_led_mask.erase(m_wiimote);
+    }
+
+    void led_parameter::device_update_value()
+    {
+        const bool v = value().get<bool>();
+        const uint8_t mask = 0x10 << m_led;
+
+        uint8_t& led_mask = m_led_mask[m_wiimote];
+
+        if (v)
+            led_mask |= mask;
+        else
+            led_mask &= (~mask);
+
+        wiiuse_set_leds(m_wiimote, led_mask);
+    }
+}
+}
---
+++ OSSIA/ossia/network/wiimote/wiimote_protocol.hpp
@@ -0,0 +1,79 @@
+
+#pragma once
+
+#include <thread>
+#include <cstdint>
+#include <array>
+#include <map>
+
+#include <ossia/network/base/protocol.hpp>
+#include <ossia/network/common/complex_type.hpp>
+#include <ossia/network/domain/domain.hpp>
+
+#include "device_parameter.hpp"
+#include "wiimote_parameter.hpp"
+
+#define MAX_WIIMOTES_COUNT 4
+
+struct wiimote_t;
+
+namespace ossia {
+namespace net {
+
+    class OSSIA_EXPORT wiimote_protocol final :
+        public ossia::net::protocol_base
+    {
+
+        typedef struct {
+                std::map<uint16_t, device::device_parameter*> button_parameters;
+
+                device::device_parameter *wiimote_axis{};
+                device::device_parameter *wiimote_gravity{};
+
+                device::device_parameter *nunchuk_button_c{};
+                device::device_parameter *nunchuk_button_z{};
+
+                device::device_parameter *nunchuk_axis{};
+                device::device_parameter *nunchuk_gravity{};
+                device::device_parameter *nunchuk_joystick{};
+
+                device::device_parameter *ir_cursor;
+                device::device_parameter *ir_detection;
+        } wiimote_parameters;
+
+        public:
+            wiimote_protocol(const bool enable_ir);
+            ~wiimote_protocol();
+
+            void set_device(ossia::net::device_base& dev) override;
+
+            bool pull(ossia::net::parameter_base& param) override;
+            bool push(const ossia::net::parameter_base& param) override;
+            bool push_raw(const ossia::net::full_parameter_data&) override;
+            bool observe(ossia::net::parameter_base& param, bool enable) override;
+    
+            bool update(ossia::net::node_base&) override;
+        
+        private:
+            void create_wiimote_parameters(const unsigned int wiimote_id);
+            void close_event_loop();
+
+            static void wiimote_event_loop(wiimote_protocol *self);
+            static void handle_wiimote_event(
+                    wiimote_protocol *self,
+                    const unsigned int wiimote_id);
+        
+            std::array<wiimote_parameters, MAX_WIIMOTES_COUNT> m_wiimotes_parameters;
+
+            bool m_running;
+            bool m_ready;
+            const bool m_enable_ir;
+            std::thread m_event_thread;
+
+            struct wiimote_t **m_wiimotes{};
+            unsigned int m_wiimote_count;
+            ossia::net::device_base* m_device{};
+    };
+
+}   
+}
