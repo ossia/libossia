1fe25ab9c8b8f7b7d65d6a14e1597f6e8048002f
aliefhooghe <aliefhooghe@enseirb-matmeca.fr>
2018-09-21 10:27:10 +0200

    work in progress

--- OSSIA/ossia/network/artnet/artnet_parameter.cpp
+++ OSSIA/ossia/network/artnet/artnet_parameter.cpp
@@ -1,4 +1,4 @@
-
+#include <cstdio>
 #include "artnet_parameter.hpp"
 
 namespace ossia
@@ -6,5 +6,36 @@ namespace ossia
 namespace net
 {
 
+    artnet_parameter::artnet_parameter(
+        net::node_base& node,
+        artnet_node artnet_node,
+        const unsigned int channel)
+    :   device_parameter(
+            node, val_type::INT, 
+            bounding_mode::CLIP, 
+            access_mode::SET,
+            make_domain(0, 255)), 
+        m_node(artnet_node), m_channel(channel)
+    {
+    }
+
+    artnet_parameter::~artnet_parameter()
+    {
+    }
+
+    void artnet_parameter::device_update_value()
+    {
+        uint8_t data[DMX_CHANNEL_COUNT] = {0u};
+        const int v = value().get<int>();
+        
+        std::printf("Sending channel %u = %d\n", m_channel, v);
+
+        data[m_channel] = 
+            static_cast<uint8_t>(v);
+
+        artnet_send_dmx(
+            m_node, 0, DMX_CHANNEL_COUNT, data);
+
+    }
 }
 }
--- OSSIA/ossia/network/artnet/artnet_parameter.hpp
+++ OSSIA/ossia/network/artnet/artnet_parameter.hpp
@@ -2,9 +2,9 @@
 #pragma once
 
 #include "device_parameter.hpp"
-
+#include "artnet_protocol.hpp"
+#include <artnet/artnet.h>
 #include <cstdint>
-
 
 
 namespace ossia
@@ -12,8 +12,21 @@ namespace ossia
 namespace net
 {
 
+  class artnet_parameter : public device_parameter {
 
+    public:
+      artnet_parameter(
+        net::node_base& node,
+        artnet_node artnet_node,
+        const unsigned int channel); 
+      ~artnet_parameter();
+    
+    private:
+      void device_update_value() override;
 
+      artnet_node m_node;
+      const unsigned int m_channel;
+  };
 
 }
 }
