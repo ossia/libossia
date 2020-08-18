4191a8746696b0c5f899fb08a3175b33ee6a3819
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-08-18 09:55:21 +0200

    Fix for archive removed from msp server

--- ci/build.sh
+++ ci/build.sh
@@ -497,9 +497,10 @@ def get_versions():
       mkdir -p ~/Documents/Pd/externals
       mv $TRAVIS_BUILD_DIR/ossia-pd-package/ossia ~/Documents/Pd/externals
 
-      wget http://msp.ucsd.edu/Software/pd-0.48-1test3.mac.tar.gz
-      tar xf pd-0.48-1test3.mac.tar.gz
-      export PATH="${PWD}/Pd-0.48-1test3.app/Contents/Resources/bin:${PATH}"
+      export PD_VERSION=0.48-2
+      wget http://msp.ucsd.edu/Software/pd-$PD_VERSION.mac.tar.gz
+      tar xf pd-$PD_VERSION.mac.tar.gz
+      export PATH="${PWD}/Pd-$PD_VERSION.app/Contents/Resources/bin:${PATH}"
 
       echo "Test Pd loading on MacOS"
       pd -path "${HOME}/Documents/Pd/externals" -nogui -lib ossia 2>&1 -send "pd quit;"
