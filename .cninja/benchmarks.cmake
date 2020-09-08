cc112c86fc5c95b5484ce25479162b50b67cf2a9
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-09-08 12:29:31 +0200

    Work on azure builds...

---
+++ .cninja/benchmarks.cmake
@@ -0,0 +1,4 @@
+# Enable build options for benchmarks
+set_cache(OSSIA_STATIC 1)
+set_cache(OSSIA_TESTING 1)
+set_cache(OSSIA_BENCHMARKS 1)
---
+++ .cninja/minimal.cmake
@@ -0,0 +1,5 @@
+# Only enable what is necessary for OSC & OSCQuery support
+set_cache(OSSIA_EDITOR 0)
+set_cache(OSSIA_DATAFLOW 0)
+set_cache(OSSIA_DNSSD 0)
+set_cache(OSSIA_OVERRIDE_PROTOCOLS "OSC;OSCQUERY")
