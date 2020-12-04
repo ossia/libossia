963969d0b640032c34ff32afa736b8e675ee985e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-12-04 23:32:45 +0100

    Forgot to add files

---
+++ src/ossia/editor/scenario/execution_log.cpp
@@ -0,0 +1,160 @@
+#include <ossia/editor/scenario/execution_log.hpp>
+#if defined(OSSIA_EXECUTION_LOG)
+namespace ossia
+{
+
+OSSIA_EXPORT execution_log g_exec_log;
+on_destruct execution_log::init()
+{
+  buf.Clear();
+  buf.Reserve(1e8);
+  w.Reset(buf);
+
+  w.StartArray();
+  return on_destruct{[this] {
+      commit();
+    }};
+}
+
+on_destruct execution_log::start_tick()
+{
+  w.StartObject();
+  return on_destruct{[this] {
+      w.EndObject();
+    }};
+}
+
+on_destruct execution_log::start_temporal()
+{
+  w.Key("Temporal");
+  w.StartArray();
+
+  return on_destruct{[this] {
+      w.EndArray();
+    }};
+}
+
+on_destruct execution_log::interval_start_start_tick(const std::string& name)
+{/*
+    w.StartObject();
+    w.Key("Name");
+    w.String(name);
+
+    w.Key("IntervalStart");
+
+    w.StartObject();
+    w.Key("Processes");
+    w.StartArray();*/
+  return on_destruct{[this] {/*
+        w.EndArray();
+        w.EndObject();
+
+        w.EndObject();*/
+    }};
+}
+
+on_destruct execution_log::interval_start_run_tick(const std::string& name, time_value old_date, time_value new_date, time_value offset)
+{
+    w.StartObject();
+    w.Key("Name");
+    w.String(name);
+
+    w.Key("IntervalRun");
+
+    w.StartObject();
+    w.Key("OldDate");
+    w.Int64(old_date.impl);
+    w.Key("NewDate");
+    w.Int64(new_date.impl);
+    w.Key("Offset");
+    w.Int64(offset.impl);
+    w.Key("Processes");
+    w.StartArray();
+  return on_destruct{[this] {
+        w.EndArray();
+        w.EndObject();
+
+        w.EndObject();
+    }};
+}
+
+on_destruct execution_log::process_state(const std::string& name)
+{
+    w.StartObject();
+    w.Key("Name");
+    w.String(name);
+
+    w.Key("Children");
+    w.StartArray();
+    return on_destruct{[this] {
+        w.EndArray();
+        w.EndObject();
+    }};
+}
+
+on_destruct execution_log::start_dataflow()
+{
+    w.Key("Dataflow");
+    w.StartObject();
+  return on_destruct{[this] {
+        w.EndObject();
+    }};
+}
+
+on_destruct execution_log::log_executed_nodes(
+    const graph_t& g,
+    const std::vector<graph_node*>& nodes)
+{
+  w.Key("Graph");
+  {
+    std::stringstream s;
+    boost::write_graphviz(
+      s, g,
+      [&](auto& out, auto v) {
+        const std::shared_ptr<ossia::graph_node>& n = g[v];
+        if (n && !n->label().empty())
+          out << fmt::format("[label=\"{}\",color={}]", n->label(), n->executed() ? "green" : "red");
+        else
+          out << fmt::format("[color={}]", n->executed() ? "green" : "red");
+      },
+      [](auto&&...) {});
+
+    w.String(s.str());
+  }
+
+  w.Key("Order");
+  w.StartArray();
+  std::string s;
+  for(auto n : nodes) {
+    w.String(n->label());
+  }
+  w.EndArray();
+
+  return []{};
+}
+
+on_destruct execution_log::start_commit()
+{
+    w.Key("Commit");
+    w.StartObject();
+  return on_destruct{[this] {
+        w.EndObject();
+    }};
+}
+
+void execution_log::commit()
+{
+  w.EndArray();
+  const char* str = buf.GetString();
+  const auto sz = buf.GetSize();
+
+  using namespace std;
+  std::ofstream f;
+
+  f.open ("/tmp/score_execution_log.json");
+  f.write(str, sz);
+  f.close();
+}
+
+}
+#endif
---
+++ src/ossia/editor/scenario/execution_log.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#include <ossia-config.hpp>
+
+#if 0//!defined(NDEBUG)
+#define OSSIA_EXECUTION_LOG 1
+#include <ossia/detail/json.hpp>
+#include <ossia/dataflow/token_request.hpp>
+#include <ossia/dataflow/graph_node.hpp>
+#include <ossia/dataflow/graph/graph_utils.hpp>
+#include <rapidjson/prettywriter.h>
+#include <iostream>
+#include <functional>
+#include <fstream>
+namespace ossia
+{
+struct on_destruct {
+  std::function<void()> dest;
+  template<typename F>
+  inline on_destruct(F t): dest{std::move(t)} { }
+  inline on_destruct(on_destruct&& t): dest{std::move(t.dest)} { t.dest = [] {}; }
+  inline on_destruct& operator=(on_destruct&& t) { dest = std::move(t.dest); t.dest = [] {}; return *this; }
+
+  inline on_destruct() = delete;
+  inline on_destruct(const on_destruct& t) = delete;
+  inline on_destruct& operator=(const on_destruct& t) = delete;
+
+  inline ~on_destruct() { dest(); }
+};
+struct OSSIA_EXPORT execution_log
+{
+  rapidjson::StringBuffer buf;
+  rapidjson::PrettyWriter<rapidjson::StringBuffer> w{buf};
+
+
+  [[nodiscard]]
+  on_destruct init();
+
+  [[nodiscard]]
+  on_destruct start_tick();
+
+  [[nodiscard]]
+  on_destruct start_temporal();
+
+  [[nodiscard]]
+  on_destruct interval_start_start_tick(const std::string& name);
+
+  [[nodiscard]]
+  on_destruct interval_start_run_tick(const std::string& name, time_value old_date, time_value new_date, time_value offset);
+
+  [[nodiscard]]
+  on_destruct process_state(const std::string& name);
+
+  [[nodiscard]]
+  on_destruct start_dataflow();
+
+  [[nodiscard]]
+  on_destruct log_executed_nodes(const ossia::graph_t&, const std::vector<graph_node*>& );
+
+  [[nodiscard]]
+  on_destruct start_commit();
+
+
+  void commit();
+};
+
+extern OSSIA_EXPORT execution_log g_exec_log;
+}
+
+#endif
