6f4634bfc7a6909e6e7c7d523061fdba48c547d8
Antoine Villeret <antoine@ossia.io>
2021-02-08 22:01:58 +0100

    generate unity package configuration and info

--- cmake/ossia_git_info.cmake
+++ cmake/ossia_git_info.cmake
@@ -1,4 +1,31 @@
 
+function(get_tag _var)
+  if(NOT GIT_FOUND)
+    find_package(Git QUIET)
+  endif()
+  if(NOT GIT_FOUND)
+    set(${_var} "GIT-NOTFOUND" PARENT_SCOPE)
+    return()
+  endif()
+
+  execute_process(COMMAND
+    "${GIT_EXECUTABLE}"
+    describe --always
+    WORKING_DIRECTORY
+    "${CMAKE_CURRENT_SOURCE_DIR}"
+    RESULT_VARIABLE
+    res
+    OUTPUT_VARIABLE
+    out
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  if(NOT res EQUAL 0)
+    set(out "${out}-${res}-NOTFOUND")
+  endif()
+
+  set(${_var} "${out}" PARENT_SCOPE)
+endfunction()
+
 function(get_commit_date _var)
   if(NOT GIT_FOUND)
     find_package(Git QUIET)
