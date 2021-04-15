7f23caadc3fe954279b0332afca75104df4fe20e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-04-15 19:20:00 +0200

    Add support for JACK transport to jack protocol

--- src/ossia/audio/audio_tick.hpp
+++ src/ossia/audio/audio_tick.hpp
@@ -1,15 +1,42 @@
 #pragma once
 #include <cinttypes>
-
+#include <optional>
 namespace ossia
 {
+enum transport_mode
+{
+  none,
+  client,
+  master
+};
+
+enum transport_status
+{
+  stopped,
+  starting,
+  playing
+};
+
 struct audio_tick_state
 {
+  // Audio samples. Array of arrays pointing to individual channels
   float* const* inputs{};
   float** outputs{};
+
+  // Number of input and output channels
   int32_t n_in{};
   int32_t n_out{};
+
+  // Number of frames to playback
   uint64_t frames{};
+
+  // Absolute transport position in seconds
   double seconds{};
+
+  // Absolute transport position in frames
+  std::optional<uint64_t> position_in_frames{};
+
+  // Current transport status
+  std::optional<transport_status> status{};
 };
 }
