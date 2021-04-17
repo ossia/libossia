60319253f946ee87db096e7add246f4f0cad3625
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-04-17 19:06:14 +0200

    Add QDataStream for time_value

--- src/ossia-qt/time_value.hpp
+++ src/ossia-qt/time_value.hpp
@@ -2,5 +2,20 @@
 #include <ossia/editor/scenario/time_value.hpp>
 #include <verdigris>
 
+#include <QDataStream>
+
+inline QDataStream& operator<<(QDataStream& i, const ossia::time_value& sel)
+{
+  return i << qint64(sel.impl);
+}
+
+inline QDataStream& operator>>(QDataStream& i, ossia::time_value& sel)
+{
+  qint64 r;
+  i >> r;
+  sel.impl = r;
+  return i;
+}
+
 Q_DECLARE_METATYPE(ossia::time_value)
 W_REGISTER_ARGTYPE(ossia::time_value)
