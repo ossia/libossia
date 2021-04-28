ad9bd33d38a50ba706cec5e16ab4faf3e274aa96
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-04-28 19:45:45 +0200

    Add an useful algorithm

--- src/ossia/detail/algorithms.hpp
+++ src/ossia/detail/algorithms.hpp
@@ -331,4 +331,9 @@ auto emplace_sorted(Container& vec, const K& k, Comp&& comp, Args&&... args)
   return vec.emplace(it, std::forward<Args>(args)...);
 }
 
+template<typename Container, typename T>
+auto insert_at_end(Container& dest, std::vector<T>&& src)
+{
+  dest.insert(dest.end(), std::make_move_iterator(src.begin()), std::make_move_iterator(src.end()));
+}
 }
