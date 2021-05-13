cff10832e167b865e945bd7b5c678645ef25e8b6
Antoine Villeret <antoine@makula.be>
2021-05-13 23:08:29 +0200

    install span.hpp
    fix https://github.com/ossia/libossia/issues/728

--- src/ossia_sources.cmake
+++ src/ossia_sources.cmake
@@ -40,6 +40,7 @@ set(API_HEADERS
     "${CMAKE_CURRENT_SOURCE_DIR}/ossia/detail/safe_vec.hpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/ossia/detail/size.hpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/ossia/detail/small_vector.hpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/ossia/detail/span.hpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/ossia/detail/string_map.hpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/ossia/detail/string_view.hpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/ossia/detail/thread.hpp"
