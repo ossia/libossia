efa152719bf719c54aeca1d0dbad45f6e9584281
Antoine Villeret <antoine@makula.be>
2021-05-14 10:49:11 +0200

    include only active protocols
    fix https://github.com/ossia/libossia/issues/729

--- src/ossia/network/network.hpp
+++ src/ossia/network/network.hpp
@@ -5,5 +5,11 @@
 #include <ossia/network/generic/generic_node.hpp>
 #include <ossia/network/generic/generic_parameter.hpp>
 #include <ossia/network/local/local.hpp>
+
+#ifdef OSSIA_PROTOCOL_MINUIT
 #include <ossia/network/minuit/minuit.hpp>
+#endif
+
+#ifdef OSSIA_PROTOCOL_OSC
 #include <ossia/network/osc/osc.hpp>
+#endif
