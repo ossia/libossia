595111ab7e9fde72d6345676423731ef80868dad
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-07-24 23:31:33 +0200

    run_async: forward the function argument

--- src/ossia-qt/invoke.hpp
+++ src/ossia-qt/invoke.hpp
@@ -10,7 +10,7 @@ namespace ossia::qt
 template<typename T, typename Fun>
 inline void run_async(T* self, Fun&& fun)
 {
-  QMetaObject::invokeMethod(self, fun, Qt::QueuedConnection);
+  QMetaObject::invokeMethod(self, std::forward<Fun>(fun), Qt::QueuedConnection);
 }
 #else
 
