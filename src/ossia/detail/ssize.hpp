ee8f4b275b1b99ef0830e124bea07c3c6d299320
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-10-09 18:50:31 +0200

    Mac/WASM build fix

--- src/ossia/detail/ssize.hpp
+++ src/ossia/detail/ssize.hpp
@@ -2,13 +2,14 @@
 
 #if __has_include(<version>)
 #include <version>
-#if __cpp_lib_ssize >= 201902L
+#if __cpp_lib_ssize >= 201902L || defined(__APPLE__) || defined(__EMSCRIPTEN__) // MacOSX11.3 SDK provides ssize but does not define it...
 #include <iterator>
 #define OSSIA_HAS_STD_SSIZE 1
 #endif
 #endif
 
 #if !defined(OSSIA_HAS_STD_SSIZE)
+#include <type_traits>
 namespace std
 {
 template <class T>
