1591b58d8d8693d1ca688819a4cbdec05ccb7f50
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-10-29 10:22:12 +0200

    proper fix for ssize, hopefully ?

--- src/ossia/detail/ssize.hpp
+++ src/ossia/detail/ssize.hpp
@@ -2,10 +2,11 @@
 
 #if __has_include(<version>)
 #include <version>
-#if __cpp_lib_ssize >= 201902L || defined(__APPLE__) || defined(__EMSCRIPTEN__) // MacOSX11.3 SDK provides ssize but does not define it...
-#include <iterator>
-#define OSSIA_HAS_STD_SSIZE 1
-#endif
+  // MacOSX11.3 SDK provides ssize but does not define it...
+  #if __cpp_lib_ssize >= 201902L || (defined(__APPLE__) && defined(__MAC_11_3)) || defined(__EMSCRIPTEN__)
+    #include <iterator>
+    #define OSSIA_HAS_STD_SSIZE 1
+  #endif
 #endif
 
 #if !defined(OSSIA_HAS_STD_SSIZE)
