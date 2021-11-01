9feb5ef845b10ec0c11b417d90a0bd3f29b8dcf8
Antoine Villeret <antoine@makula.be>
2021-11-02 00:56:57 +0100

    WIP on python workflow

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -9,22 +9,7 @@ done
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/python" get-pip.py
     "${PYBIN}/pip" install setuptools
-	$CMAKE_BIN -DCMAKE_C_COMPILER="$CC" \
-      -DCMAKE_CXX_COMPILER="$CXX" \
-      -DBOOST_ROOT="$BOOST_ROOT" \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX="$TRAVIS_BUILD_DIR/ossia-python" \
-      -DPYTHON_EXECUTABLE=${PYBIN}/python \
-      -DOSSIA_STATIC=1 \
-      -DOSSIA_TESTING=0 \
-      -DOSSIA_EXAMPLES=0 \
-      -DOSSIA_CI=1 \
-      -DOSSIA_PD=0 \
-      -DOSSIA_QT=0 \
-      -DOSSIA_QML=0 \
-      -DOSSIA_PYTHON=1
-      -DOSSIA_EDITOR=OFF \
-      -DOSSIA_DATAFLOW=OFF \ ..
+	$CMAKE_BIN -DCMAKE_BUILD_TYPE=Release -DOSSIA_PYTHON_ONLY=1 ..
 
 	$CMAKE_BIN --build . -- -j2
 	# now we just want to install the wheel and run the tests
