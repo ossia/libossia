8e46b9ffa1953632f07b4251255a8e9b19d723a2
Antoine Villeret <antoine@makula.be>
2021-11-20 22:05:46 +0100

    remove extra /
    smallest commit ever

--- tools/cross_compile_rpi.sh
+++ tools/cross_compile_rpi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cmake -S libossia -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=/libossia//cmake/toolchain/arm-linux-gnueabihf.cmake -DCMAKE_INSTALL_PREFIX=/ossia-install
+cmake -S libossia -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE=/libossia/cmake/toolchain/arm-linux-gnueabihf.cmake -DCMAKE_INSTALL_PREFIX=/ossia-install
 cmake --build build
 cmake --build build --target install
