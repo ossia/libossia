f9a9b610324759574c65d3c5fc4962014bb7e954
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-12-17 01:24:27 +0100

    [json] Use rapidjson fwd header directly

--- src/ossia/detail/json_fwd.hpp
+++ src/ossia/detail/json_fwd.hpp
@@ -1,12 +1,3 @@
 #pragma once
 #include <ossia/detail/config.hpp>
-
-namespace rapidjson
-{
-class CrtAllocator;
-template <typename Encoding, typename Allocator>
-class GenericStringBuffer;
-template <typename CharType>
-struct UTF8;
-using StringBuffer = GenericStringBuffer<UTF8<char>, CrtAllocator>;
-}
+#include <rapidjson/fwd.h>
