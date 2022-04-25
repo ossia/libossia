48838ed24ee12c2dc0a5bc8156e9514014d978cf
Antoine Villeret <antoine@makula.be>
2022-04-25 15:51:12 +0200

    increase timeout to 5 minutes

--- src/ossia-max/unit-test/logger-server.js
+++ src/ossia-max/unit-test/logger-server.js
@@ -138,11 +138,11 @@ async function main()
                 if( isWin )
                 {
                     await exec("Start-Process \"C:/Program Files/Cycling '74/Max 8/Max.exe\" -Wait -NoNewWindow " + patcher_path ,
-                                {shell:'powershell.exe', timeout : 180000});
+                                {shell:'powershell.exe', timeout : 300000});
                 }
                 else
                 {
-                    await exec('open -F -W -n ' + patcher_path, { timeout : 180000 });
+                    await exec('open -F -W -n ' + patcher_path, { timeout : 300000 });
                 }
                 if(assert_failed + assert_success == 0)
                 {
