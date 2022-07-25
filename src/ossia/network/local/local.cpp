e4b3e8d121fd60aabd0faa82df6c399edf045b84
Alex Norman <xnor@cycling74.com>
2022-07-25 14:19:28 -0700

    wrap multiplex_protocol protocol iteration in mutex
    RE #780

--- src/ossia/network/local/local.cpp
+++ src/ossia/network/local/local.cpp
@@ -41,6 +41,7 @@ bool multiplex_protocol::pull(ossia::net::parameter_base& address)
 bool multiplex_protocol::push(const ossia::net::parameter_base& address, const ossia::value& v)
 {
   bool b = true;
+  std::lock_guard<std::mutex> guard(m_protocols_mutex);
   for (auto& proto : m_protocols)
     b &= proto->push(address, v);
 
@@ -50,6 +51,7 @@ bool multiplex_protocol::push(const ossia::net::parameter_base& address, const o
 bool multiplex_protocol::push_raw(const full_parameter_data& dat)
 {
   bool b = true;
+  std::lock_guard<std::mutex> guard(m_protocols_mutex);
   for (auto& proto : m_protocols)
     b &= proto->push_raw(dat);
 
@@ -60,6 +62,7 @@ bool multiplex_protocol::observe(
     ossia::net::parameter_base& address, bool enable)
 {
   bool b = true;
+  std::lock_guard<std::mutex> guard(m_protocols_mutex);
   for (auto& proto : m_protocols)
     b &= proto->observe_quietly(address, enable);
 
@@ -74,6 +77,7 @@ bool multiplex_protocol::update(ossia::net::node_base& node)
 bool multiplex_protocol::echo_incoming_message(const message_origin_identifier& id, const parameter_base& param, const value& v)
 {
   bool b = true;
+  std::lock_guard<std::mutex> guard(m_protocols_mutex);
   for (auto& proto : m_protocols)
     b &= proto->echo_incoming_message(id, param, v);
   return b;
@@ -81,6 +85,7 @@ bool multiplex_protocol::echo_incoming_message(const message_origin_identifier& 
 
 void multiplex_protocol::stop()
 {
+  std::lock_guard<std::mutex> guard(m_protocols_mutex);
   for (auto& proto : m_protocols)
     proto->stop();
 }
@@ -88,6 +93,7 @@ void multiplex_protocol::stop()
 void multiplex_protocol::set_device(device_base& dev)
 {
   m_device = &dev;
+  std::lock_guard<std::mutex> guard(m_protocols_mutex);
   for(auto& p : m_protocols_to_register)
   {
     p->set_device(*m_device);
@@ -111,6 +117,7 @@ void multiplex_protocol::expose_to(std::unique_ptr<protocol_base> p)
        return;
     }
 
+    std::lock_guard<std::mutex> guard(m_protocols_mutex);
     if(m_device)
     {
       p->set_device(*m_device);
@@ -129,12 +136,14 @@ void multiplex_protocol::expose_to(std::unique_ptr<protocol_base> p)
 
 void multiplex_protocol::stop_expose_to(const protocol_base& p)
 {
+  std::lock_guard<std::mutex> guard(m_protocols_mutex);
   ossia::remove_erase_if(m_protocols_to_register, [&](const auto& ptr) { return ptr.get() == &p; });
   ossia::remove_erase_if(m_protocols, [&](const auto& ptr) { return ptr.get() == &p; });
 }
 
 void ossia::net::multiplex_protocol::clear()
 {
+  std::lock_guard<std::mutex> guard(m_protocols_mutex);
   m_protocols.clear();
 }
 }
