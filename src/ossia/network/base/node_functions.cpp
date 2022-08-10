c8c0903e4c9b3b2a8fc8c1b763d1dd19b5e70f1f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-08-10 16:13:07 +0200

    [node] Fix a deeper sorting issue in list_all_children

--- src/ossia/network/base/node_functions.cpp
+++ src/ossia/network/base/node_functions.cpp
@@ -24,6 +24,161 @@
 #include <utility>
 #include <vector>
 
+namespace
+{
+/*
+  strnatcmp.c -- Perform 'natural order' comparisons of strings in C.
+  Copyright (C) 2000, 2004 by Martin Pool <mbp sourcefrog net>
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Alteration:
+  - simplify for the ascii usecase as we don't need locales, and only space is ' '.
+  - put the case folding option at compile-time
+*/
+using nat_char = char;
+static constexpr inline int nat_isdigit(nat_char a)
+{
+  return a >= '0' && a <= '9';
+}
+
+static constexpr inline int nat_isspace(nat_char a)
+{
+  return a == ' ';
+}
+
+static constexpr inline nat_char nat_toupper(nat_char a)
+{
+  return a >= 'a' && a <= 'z' ? a - ('a' - 'A') : a;
+}
+
+static constexpr int nat_compare_right(nat_char const* a, nat_char const* b)
+{
+  int bias = 0;
+
+  /* The longest run of digits wins.  That aside, the greatest
+     value wins, but we can't know that it will until we've scanned
+     both numbers to know that they have the same magnitude, so we
+     remember it in BIAS. */
+  for(;; a++, b++)
+  {
+    if(!nat_isdigit(*a) && !nat_isdigit(*b))
+      return bias;
+    if(!nat_isdigit(*a))
+      return -1;
+    if(!nat_isdigit(*b))
+      return +1;
+    if(*a < *b)
+    {
+      if(!bias)
+        bias = -1;
+    }
+    else if(*a > *b)
+    {
+      if(!bias)
+        bias = +1;
+    }
+    else if(!*a && !*b)
+      return bias;
+  }
+
+  return 0;
+}
+
+static constexpr int nat_compare_left(nat_char const* a, nat_char const* b)
+{
+  /* Compare two left-aligned numbers: the first to have a
+     different value wins. */
+  for(;; a++, b++)
+  {
+    if(!nat_isdigit(*a) && !nat_isdigit(*b))
+      return 0;
+    if(!nat_isdigit(*a))
+      return -1;
+    if(!nat_isdigit(*b))
+      return +1;
+    if(*a < *b)
+      return -1;
+    if(*a > *b)
+      return +1;
+  }
+
+  return 0;
+}
+
+template <bool fold_case>
+static constexpr int strnatcmp(nat_char const* a, nat_char const* b)
+{
+  int ai, bi;
+  nat_char ca, cb;
+  int fractional, result;
+
+  ai = bi = 0;
+  while(true)
+  {
+    ca = a[ai];
+    cb = b[bi];
+
+    /* skip over leading spaces or zeros */
+    while(nat_isspace(ca))
+      ca = a[++ai];
+
+    while(nat_isspace(cb))
+      cb = b[++bi];
+
+    /* process run of digits */
+    if(nat_isdigit(ca) && nat_isdigit(cb))
+    {
+      fractional = (ca == '0' || cb == '0');
+
+      if(fractional)
+      {
+        if((result = nat_compare_left(a + ai, b + bi)) != 0)
+          return result;
+      }
+      else
+      {
+        if((result = nat_compare_right(a + ai, b + bi)) != 0)
+          return result;
+      }
+    }
+
+    if(!ca && !cb)
+    {
+      /* The strings compare the same.  Perhaps the caller
+         will want to call strcmp to break the tie. */
+      return 0;
+    }
+
+    if constexpr(fold_case)
+    {
+      ca = nat_toupper(ca);
+      cb = nat_toupper(cb);
+    }
+
+    if(ca < cb)
+      return -1;
+
+    if(ca > cb)
+      return +1;
+
+    ++ai;
+    ++bi;
+  }
+}
+}
+
 namespace ossia::net
 {
 namespace
@@ -663,61 +818,44 @@ ossia::net::address_scope get_address_scope(ossia::string_view addr)
   return type;
 }
 
-std::vector<ossia::net::node_base*>
-list_all_children(ossia::net::node_base* node, unsigned int depth)
+static void list_all_children_rec(
+    ossia::net::node_base* node, unsigned int depth,
+    std::vector<ossia::net::node_base*>& list)
 {
   std::vector<ossia::net::node_base*> children = node->children_copy();
-  std::vector<ossia::net::node_base*> list;
 
-  constexpr auto pattern = ctre::search<"\\.([0-9]+)">;
   ossia::sort(children, [&](auto n1, auto n2) {
     const auto& s1 = n1->get_name();
     const auto& s2 = n2->get_name();
-
-    auto r1 = pattern(s1);
-    auto r2 = pattern(s2);
-    if(r1.matched() && r2.matched())
-    {
-      auto p1 = std::string_view(s1.data(), r1.data() - s1.data());
-      auto p2 = std::string_view(s2.data(), r2.data() - s2.data());
-      if(p1 == p2)
-      {
-        int aidx = r1.template get<1>().to_number();
-        int bidx = r2.template get<1>().to_number();
-        return aidx < bidx;
-      }
-      else
-      {
-        return p1 < p2;
-      }
-    }
-
-#if defined(_MSC_VER)
-    return stricmp(s1.c_str(), s2.c_str()) < 0;
-#else
-    return strcasecmp(s1.c_str(), s2.c_str()) < 0;
-#endif
-  });
-
-  // then sort by priority
-  ossia::sort(children, [](auto n1, auto n2) {
-    return ossia::net::get_priority(*n1) > ossia::net::get_priority(*n2);
+    int order = strnatcmp<true>(s1.data(), s2.data());
+    if(order < 0)
+      return true;
+    else if(order == 0)
+      return ossia::net::get_priority(*n1) > ossia::net::get_priority(*n2);
+    else
+      return false;
   });
 
   int next_depth = -1;
   if(depth > 0)
     next_depth = depth - 1;
 
-  for(auto it = children.begin(); it != children.end(); it++)
+  for(auto* child : children)
   {
-    list.push_back(*it);
+    list.push_back(child);
     if(next_depth != 0)
     {
-      auto nested_list = list_all_children(*it, next_depth);
-      list.insert(list.end(), nested_list.begin(), nested_list.end());
+      list_all_children_rec(child, next_depth, list);
     }
   }
+}
 
+std::vector<ossia::net::node_base*>
+list_all_children(ossia::net::node_base* node, unsigned int depth)
+{
+  std::vector<ossia::net::node_base*> list;
+  list.reserve(250);
+  list_all_children_rec(node, depth, list);
   return list;
 }
 
