329b9b14be31affce711061d4bc67a7e42aa5154
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-05-28 12:48:23 -0400

    [tests] Fix API change of curve segment

--- tests/Editor/CurveSegment/CurveSegmentPowerTest.cpp
+++ tests/Editor/CurveSegment/CurveSegmentPowerTest.cpp
@@ -14,7 +14,7 @@
 TEST_CASE("bool", "[bool]")
 {
   {
-    auto curveSegment = ossia::curve_segment_power<bool>{}(1.);
+    auto curveSegment = ossia::curve_segment_power<bool, double>{{.gamma = 1.}};
     REQUIRE(curveSegment(0., false, true) == false);
     REQUIRE(curveSegment(0.25, false, true) == true);
     REQUIRE(curveSegment(0.5, false, true) == true);
@@ -23,7 +23,7 @@ TEST_CASE("bool", "[bool]")
   }
 
   {
-    auto curveSegment = ossia::curve_segment_power<bool>{}(2.);
+    auto curveSegment = ossia::curve_segment_power<bool, double>{{.gamma = 2.}};
 
     REQUIRE(curveSegment(0., false, true) == false);
     REQUIRE(curveSegment(0.25, false, true) == true);
@@ -36,7 +36,7 @@ TEST_CASE("bool", "[bool]")
 TEST_CASE("int", "[int]")
 {
   {
-    auto curveSegment = ossia::curve_segment_power<int>{}(1.);
+    auto curveSegment = ossia::curve_segment_power<int, double>{{.gamma = 1.}};
     REQUIRE(curveSegment(0., 0, 1) == 0);
     REQUIRE(curveSegment(0.25, 0, 1) == 0);
     REQUIRE(curveSegment(0.5, 0, 1) == 0);
@@ -45,7 +45,7 @@ TEST_CASE("int", "[int]")
   }
 
   {
-    auto curveSegment = ossia::curve_segment_power<int>{}(2.);
+    auto curveSegment = ossia::curve_segment_power<int, double>{{.gamma = 2.}};
 
     REQUIRE(curveSegment(0., 0, 1) == 0);
     REQUIRE(curveSegment(0.25, 0, 1) == 0);
@@ -58,7 +58,7 @@ TEST_CASE("int", "[int]")
 TEST_CASE("float", "[float]")
 {
   {
-    auto curveSegment = ossia::curve_segment_power<float>{}(1.);
+    auto curveSegment = ossia::curve_segment_power<float, double>{{.gamma = 1.}};
     REQUIRE(curveSegment(0., 0., 1.) == 0.);
     REQUIRE(curveSegment(0.25, 0., 1.) == 0.25);
     REQUIRE(curveSegment(0.5, 0., 1.) == 0.5);
@@ -67,7 +67,7 @@ TEST_CASE("float", "[float]")
   }
 
   {
-    auto curveSegment = ossia::curve_segment_power<float>{}(2.);
+    auto curveSegment = ossia::curve_segment_power<float, double>{{.gamma = 2.}};
     REQUIRE(curveSegment(0., 0., 1.) == 0.);
     REQUIRE(curveSegment(0.25, 0., 1.) == 0.0625);
     REQUIRE(curveSegment(0.5, 0., 1.) == 0.25);
