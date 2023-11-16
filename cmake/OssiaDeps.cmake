c1b2af4370b68c9be7ded59449f6fde99bdf2e73
Alex Norman <xnor@cycling74.com>
2023-11-16 08:46:22 -0800

    remove extra find_package

--- cmake/OssiaDeps.cmake
+++ cmake/OssiaDeps.cmake
@@ -63,7 +63,6 @@ if(Git_FOUND AND OSSIA_SUBMODULE_AUTOUPDATE)
     set(OSSIA_SUBMODULES ${OSSIA_SUBMODULES} Catch2)
   endif()
 
-  find_package(Git REQUIRED)
   execute_process(COMMAND ${GIT_EXECUTABLE} submodule sync --recursive
                   WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
                   COMMAND_ERROR_IS_FATAL ANY)
