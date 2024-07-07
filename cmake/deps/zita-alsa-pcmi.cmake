a481c5a6c8224d2b4788aa1c60424b725f524a56
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-07-07 03:42:49 -0400

    [alsa] Find asoundlib.h for bsd compat

--- cmake/deps/zita-alsa-pcmi.cmake
+++ cmake/deps/zita-alsa-pcmi.cmake
@@ -3,13 +3,18 @@ if(OSSIA_USE_SYSTEM_LIBRARIES)
 endif()
 
 if(NOT TARGET zita-alsa-pcmi)
-  add_library(zita-alsa-pcmi STATIC
-      "${OSSIA_3RDPARTY_FOLDER}/zita-alsa-pcmi/zita-alsa-pcmi-ardour.cc"
-      "${OSSIA_3RDPARTY_FOLDER}/zita-alsa-pcmi/zita-alsa-pcmi-ardour.h"
-  )
-  target_include_directories(zita-alsa-pcmi
-      PRIVATE "${OSSIA_SOURCE_FOLDER}/src"
-      PUBLIC "${OSSIA_3RDPARTY_FOLDER}/zita-alsa-pcmi"
-  )
+  find_path(ASOUNDLIB_INCLUDE_DIRS alsa/asoundlib.h)
+  if(ASOUNDLIB_INCLUDE_DIRS)
+    add_library(zita-alsa-pcmi STATIC
+        "${OSSIA_3RDPARTY_FOLDER}/zita-alsa-pcmi/zita-alsa-pcmi-ardour.cc"
+        "${OSSIA_3RDPARTY_FOLDER}/zita-alsa-pcmi/zita-alsa-pcmi-ardour.h"
+    )
+    target_include_directories(zita-alsa-pcmi
+        PRIVATE "${OSSIA_SOURCE_FOLDER}/src"
+        PUBLIC
+          "${OSSIA_3RDPARTY_FOLDER}/zita-alsa-pcmi"
+          "${ASOUNDLIB_INCLUDE_DIRS}"
+    )
+  endif()
 endif()
 
