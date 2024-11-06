393e20afd76721b51dc6b0757eb9e320c0da3e7c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-11-05 20:59:31 -0500

    [libremidi] Fix msvc build

--- cmake/deps/libremidi.cmake
+++ cmake/deps/libremidi.cmake
@@ -12,5 +12,15 @@ block()
   endif()
   set(WEAKJACK_FOLDER "${OSSIA_3RDPARTY_FOLDER}")
   set(BUILD_SHARED_LIBS 0)
+  add_definitions(
+    BOOST_NO_RTTI=1
+    BOOST_MATH_DISABLE_FLOAT128=1
+  )
+
+  if(WIN32)
+    if(MSVC)
+      add_definitions(BOOST_ASIO_SEPARATE_COMPILATION=1)
+    endif()
+  endif()
   add_subdirectory("${OSSIA_3RDPARTY_FOLDER}/libremidi" EXCLUDE_FROM_ALL)
 endblock()
