ef18944ef20fc3e582f824747b8942ed6cbe1f90
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-12-09 14:51:05 -0500

    serial: try to enable non-standard baud rates when possible

--- src/ossia/network/sockets/serial_socket.hpp
+++ src/ossia/network/sockets/serial_socket.hpp
@@ -8,6 +8,15 @@
 #include <boost/asio/write.hpp>
 
 #include <nano_signal_slot.hpp>
+#if defined(__linux__)
+#include <linux/serial.h>
+#include <linux/tty_flags.h>
+#include <sys/ioctl.h>
+
+#include <termios.h>
+#elif defined(__APPLE__)
+#include <sys/ioctl.h>
+#endif
 
 namespace ossia::net
 {
@@ -30,11 +39,78 @@ public:
   {
   }
 
+  int set_custom_baud_rate(int rate)
+  {
+#if defined(__linux__)
+    // https://stackoverflow.com/questions/9366249/boostasioserialport-alternative-that-supports-non-standard-baud-rates
+    auto fd = m_port.native_handle();
+    struct serial_struct serinfo;
+    memset(&serinfo, 0, sizeof(serinfo));
+
+    // Get the current serial status
+    if(ioctl(fd, TIOCGSERIAL, &serinfo) < 0)
+      return -1;
+
+    // Device can handle any rate e.g. it's using straight USB packetization
+    if(serinfo.baud_base == 0)
+      return 0;
+
+    // Approximate the closest baud rate
+    serinfo.flags &= ~ASYNC_SPD_MASK;
+    serinfo.flags |= ASYNC_SPD_CUST;
+    serinfo.custom_divisor = (serinfo.baud_base + (rate / 2)) / rate;
+    if(serinfo.custom_divisor < 1)
+    {
+      serinfo.baud_base = rate;
+      serinfo.custom_divisor = 1;
+    }
+
+    // Apply and check
+    if(ioctl(fd, TIOCSSERIAL, &serinfo) < 0)
+      return -1;
+    if(ioctl(fd, TIOCGSERIAL, &serinfo) < 0)
+      return -1;
+
+    fcntl(fd, F_SETFL, 0);
+
+    // Apply terminal options
+    struct termios options;
+    tcgetattr(fd, &options);
+    cfsetispeed(&options, B38400);
+    cfsetospeed(&options, B38400);
+    cfmakeraw(&options);
+    options.c_cflag |= (CLOCAL | CREAD);
+    options.c_cflag &= ~CRTSCTS;
+    if(tcsetattr(fd, TCSANOW, &options) != 0)
+      return -1;
+
+#elif defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__) \
+    || defined(__OpenBSD__) || defined(__DragonFly__)
+    auto fd = m_port.native_handle();
+#define IOSSIOSPEED _IOW('T', 2, speed_t)
+    if(ioctl(fd, IOSSIOSPEED, &rate, 1) < 0)
+      return -1;
+#undef IOSSIOSPEED
+#endif
+
+    return 0;
+  }
+
   void connect()
   {
     m_port.open(m_conf.port);
 
-    m_port.set_option(proto::baud_rate(m_conf.baud_rate));
+    try
+    {
+      m_port.set_option(proto::baud_rate(m_conf.baud_rate));
+    }
+    catch(...)
+    {
+      m_port.set_option(proto::baud_rate(38400));
+
+      int new_baud = static_cast<int>(m_conf.baud_rate);
+      set_custom_baud_rate(new_baud);
+    }
     m_port.set_option(proto::character_size(m_conf.character_size));
     m_port.set_option(proto::flow_control(
         static_cast<proto::flow_control::type>(m_conf.flow_control)));
