b1c92e0c6aae363c990c807ae18da44f35c67757
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-12-09 14:51:26 -0500

    core: add a geometry parameter type

--- src/ossia/gfx/texture_parameter.cpp
+++ src/ossia/gfx/texture_parameter.cpp
@@ -76,4 +76,77 @@ net::parameter_base& texture_parameter::set_bounding(bounding_mode)
 {
   return *this;
 }
+
+void geometry_parameter::pull_geometry(ossia::gfx::port_index idx) { }
+void geometry_parameter::pull_value() { }
+
+net::parameter_base& geometry_parameter::push_value(const ossia::value&)
+{
+  return *this;
+}
+
+net::parameter_base& geometry_parameter::push_value(ossia::value&&)
+{
+  return *this;
+}
+
+net::parameter_base& geometry_parameter::push_value()
+{
+  return *this;
+}
+
+value geometry_parameter::value() const
+{
+  return {};
+}
+
+ossia::value geometry_parameter::set_value(const ossia::value&)
+{
+  return {};
+}
+
+ossia::value geometry_parameter::set_value(ossia::value&&)
+{
+  return {};
+}
+
+val_type geometry_parameter::get_value_type() const noexcept
+{
+  return {};
+}
+
+net::parameter_base& geometry_parameter::set_value_type(val_type)
+{
+  return *this;
+}
+
+access_mode geometry_parameter::get_access() const noexcept
+{
+  return {};
+}
+
+net::parameter_base& geometry_parameter::set_access(access_mode)
+{
+  return *this;
+}
+
+const domain& geometry_parameter::get_domain() const noexcept
+{
+  throw;
+}
+
+net::parameter_base& geometry_parameter::set_domain(const domain&)
+{
+  return *this;
+}
+
+bounding_mode geometry_parameter::get_bounding() const noexcept
+{
+  return {};
+}
+
+net::parameter_base& geometry_parameter::set_bounding(bounding_mode)
+{
+  return *this;
+}
 }
--- src/ossia/gfx/texture_parameter.hpp
+++ src/ossia/gfx/texture_parameter.hpp
@@ -34,4 +34,34 @@ public:
   ossia::bounding_mode get_bounding() const noexcept override;
   ossia::net::parameter_base& set_bounding(ossia::bounding_mode) override;
 };
+class OSSIA_EXPORT geometry_parameter : public ossia::net::parameter_base
+{
+public:
+  explicit geometry_parameter(ossia::net::node_base& n)
+      : parameter_base{n}
+  {
+    m_type = parameter_type::GEOMETRY;
+  }
+
+  virtual ~geometry_parameter();
+
+  // For input
+  virtual void pull_geometry(ossia::gfx::port_index idx);
+
+  void pull_value() override;
+  ossia::net::parameter_base& push_value(const ossia::value&) override;
+  ossia::net::parameter_base& push_value(ossia::value&&) override;
+  ossia::net::parameter_base& push_value() override;
+  ossia::value value() const override;
+  ossia::value set_value(const ossia::value&) override;
+  ossia::value set_value(ossia::value&&) override;
+  ossia::val_type get_value_type() const noexcept override;
+  ossia::net::parameter_base& set_value_type(ossia::val_type) override;
+  ossia::access_mode get_access() const noexcept override;
+  ossia::net::parameter_base& set_access(ossia::access_mode) override;
+  const ossia::domain& get_domain() const noexcept override;
+  ossia::net::parameter_base& set_domain(const ossia::domain&) override;
+  ossia::bounding_mode get_bounding() const noexcept override;
+  ossia::net::parameter_base& set_bounding(ossia::bounding_mode) override;
+};
 }
