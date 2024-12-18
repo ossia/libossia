0e9f97a7b0bda171b73bcde35513bdcdc99481d5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-12-17 21:36:31 -0500

    ci: find boost 1.86 exactly as websocketpp is incompatible with 1.87+

--- cmake/deps/mqtt.cmake
+++ cmake/deps/mqtt.cmake
@@ -3,6 +3,18 @@ if(OSSIA_USE_SYSTEM_LIBRARIES)
 endif()
 
 if(NOT TARGET Async::MQTT5)
-  add_subdirectory("${OSSIA_3RDPARTY_FOLDER}/async-mqtt5" async-mqtt5)
+    add_library(async_mqtt5 INTERFACE IMPORTED GLOBAL)
+    target_include_directories(async_mqtt5
+      INTERFACE
+        "$<BUILD_INTERFACE:${OSSIA_3RDPARTY_FOLDER}/async-mqtt5/include>"
+    )
+
+    if(TARGET Boost::boost)
+      target_link_libraries(async_mqtt5 INTERFACE $<BUILD_INTERFACE:Boost::boost>)
+    elseif(TARGET boost)
+      target_link_libraries(async_mqtt5 INTERFACE $<BUILD_INTERFACE:boost>)
+    endif()
+
+    add_library(Async::MQTT5 ALIAS async_mqtt5)
 endif()
 
