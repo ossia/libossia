d2372522ce654909e00f98838458ef45a17cc1c7
jcelerier <jcelerier@users.noreply.github.com>
2025-01-16 04:01:34 +0000

    Deploy code docs to GitHub Pages, Github Action id: $GITHUB_ACTION \nCommit: $GITHUB_SHA 0af49f492f4dcf7a6cba1cfbd395d4133ad042f0

---
+++ html/dir_e085be862af11ab585fc219a8f0b1441.js
@@ -0,0 +1,5 @@
+var dir_e085be862af11ab585fc219a8f0b1441 =
+[
+    [ "wiimote_parameter.hpp", "wiimote__parameter_8hpp_source.html", null ],
+    [ "wiimote_protocol.hpp", "wiimote__protocol_8hpp_source.html", null ]
+];
---
+++ html/navtreeindex6.js
@@ -0,0 +1,18 @@
+var NAVTREEINDEX6 =
+{
+"value__vector_8hpp_source.html":[3,0,0,0,1,35],
+"value__with__unit_8hpp_source.html":[3,0,0,0,6,2,19],
+"variant_8hpp_source.html":[3,0,0,0,2,67],
+"vec_8hpp_source.html":[3,0,0,0,6,11,12],
+"websocket_8hpp_source.html":[3,0,0,0,6,10,11],
+"websocket__client_8hpp_source.html":[3,0,0,0,6,10,12],
+"websocket__log__sink_8hpp_source.html":[3,0,0,0,6,1,12],
+"websocket__reply_8hpp_source.html":[3,0,0,0,6,10,13],
+"websocket__server_8hpp_source.html":[3,0,0,0,6,10,14],
+"wiimote__parameter_8hpp_source.html":[3,0,0,0,8,11,0],
+"wiimote__protocol_8hpp_source.html":[3,0,0,0,8,11,1],
+"wrapped__parameter_8hpp_source.html":[3,0,0,0,6,4,4],
+"writers_8hpp_source.html":[3,0,0,0,6,10,15],
+"ws__generic__client__protocol_8hpp_source.html":[3,0,0,6,4,0],
+"zeroconf_8hpp_source.html":[3,0,0,0,6,12,0]
+};
