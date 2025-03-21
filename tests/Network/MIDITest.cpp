807cbe650efe69f80504453568c7bccd9c3d2074
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-03-21 17:36:34 +0100

    ci: work on debugging MIDITest

--- tests/Network/MIDITest.cpp
+++ tests/Network/MIDITest.cpp
@@ -8,48 +8,58 @@
 #include <ossia/detail/config.hpp>
 
 #include <libremidi/libremidi.hpp>
+#include <magic_enum/magic_enum.hpp>
 #if defined(OSSIA_PROTOCOL_MIDI)
 #include <ossia/protocols/midi/midi.hpp>
 
 TEST_CASE("test_midi", "test_midi")
 {
   using namespace ossia::net::midi;
-  try
-  {
     auto ctx = ossia::net::create_network_context();
     for(auto midi_api : libremidi::available_apis())
     {
-      libremidi::observer_configuration conf;
-      libremidi::observer obs{conf, libremidi::observer_configuration_for(midi_api)};
-
-      for(auto port : obs.get_input_ports())
+      try
       {
-        libremidi::input_configuration conf;
-        auto api_conf = libremidi::midi_in_configuration_for(midi_api);
+        std::cerr << "Testing api: " << int(midi_api) << ": "
+                  << libremidi::get_api_display_name(midi_api) << "\n";
+        libremidi::observer_configuration conf;
+        libremidi::observer obs{conf, libremidi::observer_configuration_for(midi_api)};
 
-        auto proto
-            = std::make_unique<midi_protocol>(ctx, port.display_name, conf, api_conf);
-        proto->set_info(midi_info{midi_info::Type::Input, port, false});
+        for(auto port : obs.get_input_ports())
+        {
+          libremidi::input_configuration conf;
+          conf.on_message = [](libremidi::message m) { };
+          auto api_conf = libremidi::midi_in_configuration_for(midi_api);
 
-        midi_device dev("test", std::move(proto));
-        dev.create_full_tree();
+          auto proto
+              = std::make_unique<midi_protocol>(ctx, port.display_name, conf, api_conf);
+          proto->set_info(midi_info{midi_info::Type::Input, port, false});
+
+          midi_device dev("test", std::move(proto));
+          dev.create_full_tree();
+        }
+        for(auto port : obs.get_output_ports())
+        {
+          libremidi::output_configuration conf;
+          auto api_conf = libremidi::midi_out_configuration_for(midi_api);
+
+          auto proto
+              = std::make_unique<midi_protocol>(ctx, port.display_name, conf, api_conf);
+          proto->set_info(midi_info{midi_info::Type::Output, port, false});
+
+          midi_device dev("test", std::move(proto));
+          dev.create_full_tree();
+        }
       }
-      for(auto port : obs.get_output_ports())
+      catch(const std::exception& e)
       {
-        libremidi::input_configuration conf;
-        auto api_conf = libremidi::midi_in_configuration_for(midi_api);
-
-        auto proto
-            = std::make_unique<midi_protocol>(ctx, port.display_name, conf, api_conf);
-        proto->set_info(midi_info{midi_info::Type::Input, port, false});
-
-        midi_device dev("test", std::move(proto));
-        dev.create_full_tree();
+        std::cerr << "Error: " << libremidi::get_api_display_name(midi_api) << ": "
+                  << e.what() << "\n";
+      }
+      catch(...)
+      {
+        std::cerr << "Error: " << libremidi::get_api_display_name(midi_api);
       }
     }
-  }
-  catch(...)
-  {
-  }
 }
 #endif
