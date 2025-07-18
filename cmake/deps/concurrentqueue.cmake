968a9cd0c8f562e29a3b07ae5a02190f0017277f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-07-18 12:55:29 -0400

    3rdparty: add missing folder manually to concurrentqueue cmake target
    Fixes ossia/score#1720

--- cmake/deps/concurrentqueue.cmake
+++ cmake/deps/concurrentqueue.cmake
@@ -1,5 +1,17 @@
 if(OSSIA_USE_SYSTEM_LIBRARIES)
   find_package(concurrentqueue 1.0 CONFIG GLOBAL)
+  if(TARGET concurrentqueue::concurrentqueue)
+    get_target_property(cq_include_dirs concurrentqueue::concurrentqueue  INTERFACE_INCLUDE_DIRECTORIES)
+
+    # concurrentqueue package's cmake files as of 2025-07-18 still do not add the target_include_directories
+    # concurrentqueue/moodycamel, leading to build issues
+    foreach(cq_include_dir "${cq_include_dirs}")
+      if("${cq_include_dir}" MATCHES ".*concurrentqueue.*")
+        target_include_directories(concurrentqueue::concurrentqueue INTERFACE "$<BUILD_INTERFACE:${cq_include_dir}/moodycamel>")
+        break()
+      endif()
+    endforeach()
+  endif()
 endif()
 
 if(NOT TARGET concurrentqueue::concurrentqueue)
